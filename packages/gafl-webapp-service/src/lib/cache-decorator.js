256cbd3caf0efb4aada4faf0a4c707af9ff649b9
graham-willis-druid <graham.willis.ea@gmail.com>
2020-03-12 14:38:26 +0000

    Added session and cache mechanism

--- packages/gafl-webapp-service/src/lib/cache-decorator.js
+++ packages/gafl-webapp-service/src/lib/cache-decorator.js
@@ -3,24 +3,6 @@
  * Decorators to make access to the session cache available as
  * simple setters and getters hiding the session key.
  */
-<<<<<<< HEAD
-const cacheDecorator = sessionCookieName =>
-  function () {
-    const id = () => this.state[sessionCookieName].id
-
-    return {
-      set: async obj => {
-        if (!obj || typeof obj !== 'object') {
-          throw new Error('Expect object')
-        }
-        const cache = await this.server.app.cache.get(id())
-        await this.server.app.cache.set(id(), Object.assign(cache || {}, obj))
-      },
-      get: async () => this.server.app.cache.get(id())
-      // drop: async () => this.server.cache.drop(id())
-    }
-  }
-=======
 import db from 'debug'
 
 const debug = db('cache-decorator')
@@ -31,17 +13,14 @@ const cacheDecorator = (sessionCookieName) => function () {
   return {
     get: async () => this.server.app.cache.get(id()),
     set: async (obj) => {
-      if (!obj || typeof obj !== 'object') {
-        throw new Error('Expect object')
-      }
-      const cache = await this.server.app.cache.get(id())
-      await this.server.app.cache.set(id(), Object.assign(cache || {}, obj))
+      console.debug(`Saving ${JSON.stringify(obj)} to ${id()}`)
+      const existing = await this.server.app.cache.get(id())
+      await this.server.app.cache.set(id(), Object.assign(existing || {}, obj))
     },
     drop: async () => {
       await this.server.cache.drop(id())
     }
   }
 }
->>>>>>> Added session and cache mechanism
 
 export default cacheDecorator
--- packages/gafl-webapp-service/src/lib/session-manager.js
+++ packages/gafl-webapp-service/src/lib/session-manager.js
@@ -1,10 +1,6 @@
 'use strict'
 
-<<<<<<< HEAD
-import uuidv4 from 'uuid/v4.js'
-=======
 import uuid from 'uuid'
->>>>>>> Added session and cache mechanism
 import db from 'debug'
 import moment from 'moment'
 
@@ -13,15 +9,6 @@ const debug = db('session-manager')
  * (1) If the user cache does not exist create it
  * (2) If the cookie does not exist create it
  * @param sessionCookieName
-<<<<<<< HEAD
- * @returns {function(*, *)}
- */
-const sessionManager = sessionCookieName => async (request, h) => {
-  // If the session cookie does not exist create it
-  if (!request.state[sessionCookieName]) {
-    const id = uuidv4()
-    debug(`New session cookie: ${id}`)
-=======
  * @returns {function(*, *): string|((key?: IDBValidKey) => void)}
  */
 const sessionManager = (sessionCookieName) => async (request, h) => {
@@ -29,18 +16,12 @@ const sessionManager = (sessionCookieName) => async (request, h) => {
   if (!request.state[sessionCookieName]) {
     const id = uuid.v4()
     console.debug(`New session cookie: ${id}`)
->>>>>>> Added session and cache mechanism
     h.state(sessionCookieName, { id })
     request.state[sessionCookieName] = { id }
   }
 
-<<<<<<< HEAD
-  if (!(await request.cache().get())) {
-    await request.cache().set({ created: moment.utc() })
-=======
   if (!await request.cache().get()) {
     request.cache().set({ created: moment.utc() })
->>>>>>> Added session and cache mechanism
   }
 
   return h.continue
