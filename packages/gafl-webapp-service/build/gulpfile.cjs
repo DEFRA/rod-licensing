4c994cd80e50c7ed8744f5e25c2161b76ca77902
graham-willis-druid <graham.willis.ea@gmail.com>
2020-03-18 09:25:07 +0000

    Tweaked gulpfile

--- packages/gafl-webapp-service/build/gulpfile.cjs
+++ packages/gafl-webapp-service/build/gulpfile.cjs
@@ -12,38 +12,48 @@ const sourcemaps = require('gulp-sourcemaps')
 const del = require('del')
 const minify = require('gulp-minify')
 const merge = require('gulp-merge')
+const path = require('path')
+
+const base = path.join(__dirname, '..')
 
 const paths = {
-  assets: '../src/assets/',
-  public: '../public/'
+  assets: path.join(base, 'src/assets/'),
+  public: path.join(base, 'public/'),
+  govUk: path.join(base, 'node_modules/govuk-frontend/govuk/')
 }
+
+Object.assign(paths, {
+  govUkAssets: path.join(paths.govUk, '/assets/{images/**/*.*,fonts/**/*.*}')
+})
+
+console.log(`Building gafl-webapp-service. Paths: ${JSON.stringify(paths, null, 4)}`)
 
 const clean = () => {
   return del(paths.public, { force: true })
 }
 
 const copyAssets = () => {
-  return gulp.src('../node_modules/govuk-frontend/govuk/assets/{images/**/*.*,fonts/**/*.*}')
+  return gulp.src(paths.govUkAssets)
     .pipe(gulp.dest(paths.public))
 }
 
 const copyJs = () => {
   return merge(
-    gulp.src('../node_modules/govuk-frontend/govuk/all.js'),
-    gulp.src('../src/assets/javascript/**/*.*')
-  ).pipe(minify({ noSource: true })).pipe(gulp.dest(paths.public + '/javascript'))
+    gulp.src(`${paths.govUk}all.js`),
+    gulp.src(`${paths.assets}javascript/**/*.*`)
+  ).pipe(minify({ noSource: true })).pipe(gulp.dest(`${paths.public}javascript`))
 }
 
 // Build the sass
 const buildSass = () => {
-  return gulp.src(paths.assets + 'sass/*.scss')
+  return gulp.src(`${paths.assets}sass/*.scss`)
     .pipe(sourcemaps.init())
     .pipe(sass({
       outputStyle: 'compressed',
-      includePaths: 'node_modules'
+      includePaths: path.join(base, 'node_modules')
     }).on('error', sass.logError))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest(paths.public + 'stylesheets/'))
+    .pipe(gulp.dest(`${paths.public}stylesheets/`))
 }
 
 // The default Gulp task builds the resources
@@ -54,11 +64,3 @@ gulp.task('default', gulp.series(
   buildSass
 ))
 
-/*
- * The Gulp v4 cli must be installed globally to run the watch
- * npm rm -g gulp
- * npm install -g gulp-cli
- */
-gulp.task('watch', gulp.series(() => {
-  gulp.watch(paths.assets + 'sass/**/*.scss', gulp.series(buildSass))
-}))
