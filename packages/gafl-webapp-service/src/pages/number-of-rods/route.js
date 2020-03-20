232bbdcef793baf933d33efb42d8864a2f53997a
graham-willis-druid <graham.willis.ea@gmail.com>
2020-03-20 10:06:10 +0000

    milestone add basic journey

--- packages/gafl-webapp-service/src/pages/number-of-rods/route.js
+++ packages/gafl-webapp-service/src/pages/number-of-rods/route.js
@@ -2,7 +2,9 @@ import pageRoute from '../../routes/page-route.js'
 import Joi from '@hapi/joi'
 
 const validator = Joi.object({
-  'number-of-rods': Joi.string().valid('2', '3').required()
+  'number-of-rods': Joi.string()
+    .valid('2', '3')
+    .required()
 }).options({ abortEarly: false, allowUnknown: true })
 
 export default pageRoute('number-of-rods', '/buy/number-of-rods', validator, '/controller')
