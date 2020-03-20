c81bbe15561fa64f7b5f9bc0328a6a66f5c725ba
sgd <sgardnerdell.ea@gmail.com>
2020-03-20 16:05:16 +0000

    Fix travis publish deploy issue

--- packages/dynamics-lib/src/client/cache.js
+++ packages/dynamics-lib/src/client/cache.js
@@ -2,7 +2,7 @@ import cacheManager from 'cache-manager'
 import redisStore from 'cache-manager-ioredis'
 const cache = cacheManager.caching({
   store: 'memory',
-  ttl: 60 * 60 * 12,
+  ttl: process.env.DYNAMICS_CACHE_TTL || 60 * 60 * 12,
   ...(process.env.REDIS_HOST && { store: redisStore, host: process.env.REDIS_HOST, port: process.env.REDIS_PORT || 6379 })
 })
 
