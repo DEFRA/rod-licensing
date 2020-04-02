28875ccef15419181626980d04fcd4187a850651
sgd <sgardnerdell-ea@users.noreply.github.com>
2020-04-02 11:04:07 +0100

    Update SQS receiver to handle all 2XX responses (inc 204 No Content) and use debug namespace (#62)
    * Update SQS receiver to handle all 2XX responses (inc 204 No Content) and use debug namespace
    
    * Update infrastructure network configuration, add dynamodb admin tool and update SQS configuration

--- packages/sqs-receiver-service/src/__tests__/process-message.spec.js
+++ packages/sqs-receiver-service/src/__tests__/process-message.spec.js
@@ -3,41 +3,57 @@ import fetch from 'node-fetch'
 
 test('process-message returns payload', async () => {
   fetch.__goodResult()
-  const result = await processMessage({ Attributes: { MessageGroupId: 'messageId' }, ReceiptHandle: '123' }, 'http://0.0.0.0/')
-  expect(result).toEqual({
-    handle: '123',
-    status: 200
-  })
+  const result = await processMessage(
+    { MessageId: 'test1', Attributes: { MessageGroupId: 'messageId' }, ReceiptHandle: '123' },
+    'http://0.0.0.0/'
+  )
+  expect(result).toMatchObject(
+    expect.objectContaining({
+      id: 'test1',
+      handle: '123',
+      status: 200,
+      statusText: 'OK',
+      message: expect.any(String)
+    })
+  )
 })
 
 test('process-message returns payload with no message group', async () => {
   fetch.__goodResult()
-  const result = await processMessage({ Attributes: {}, ReceiptHandle: '123' }, 'http://0.0.0.0/')
-  expect(result).toEqual({
-    handle: '123',
-    status: 200
-  })
+  const result = await processMessage({ MessageId: 'test2', Attributes: {}, ReceiptHandle: '123' }, 'http://0.0.0.0/')
+  expect(result).toMatchObject(
+    expect.objectContaining({
+      id: 'test2',
+      handle: '123',
+      status: 200,
+      statusText: 'OK',
+      message: expect.any(String)
+    })
+  )
 })
 
 test('process-message bad gateway', async () => {
   const consoleError = jest.spyOn(console, 'error').mockImplementation(() => {})
   fetch.__BadGateway()
-  const result = await processMessage({ Attributes: {}, ReceiptHandle: '2342' }, 'http://0.0.0.0/')
-  expect(result).toEqual({
-    error: 'Bad Gateway',
-    handle: '2342',
-    message: 'Bad Gateway',
-    status: 502
-  })
+  const result = await processMessage({ MessageId: 'test3', Attributes: {}, ReceiptHandle: '2342' }, 'http://0.0.0.0/')
+  expect(result).toMatchObject(
+    expect.objectContaining({
+      id: 'test3',
+      handle: '2342',
+      status: 502,
+      statusText: 'Bad Gateway',
+      message: expect.any(String)
+    })
+  )
   expect(consoleError).toHaveBeenCalled()
 })
 
 test('Completes on not found error', async () => {
   const consoleError = jest.spyOn(console, 'error').mockImplementation(() => {})
   fetch.__NotFound()
-  const result = await processMessage({ MessageId: '123', Attributes: {}, ReceiptHandle: '2342' }, 'http://0.0.0.0/')
-  await expect(result).toStrictEqual({
-    id: '123',
+  const result = await processMessage({ MessageId: 'test4', Attributes: {}, ReceiptHandle: '2342' }, 'http://0.0.0.0/')
+  expect(result).toEqual({
+    id: 'test4',
     handle: '2342',
     status: 500,
     message: 'Error'
--- packages/sqs-receiver-service/src/delete-messages.js
+++ packages/sqs-receiver-service/src/delete-messages.js
@@ -6,26 +6,28 @@ const { sqs } = AWS()
 /**
  * This removes any messages from the queue that have been processed without error
  */
-const debug = db('delete-messages')
+const debug = db('sqs:delete-messages')
 
 const deleteMessages = async (url, messageSubscriberResults) => {
   try {
     const params = {
       QueueUrl: url,
-      Entries: messageSubscriberResults.filter(msr => msr.status === 200).map(msr => ({ Id: msr.id, ReceiptHandle: msr.handle }))
+      Entries: messageSubscriberResults
+        .filter(msr => Math.floor(msr.status / 100) === 2)
+        .map(msr => ({ Id: msr.id, ReceiptHandle: msr.handle }))
     }
 
     if (params.Entries.length) {
       const results = await sqs.deleteMessageBatch(params).promise()
 
       if (results.Failed.length) {
-        console.error(`Failed to delete from batch${JSON.stringify(results.Failed)}`)
+        console.error('Failed to delete from batch: %O', results.Failed)
       }
 
       debug({ success: results.Successful, failed: results.Failed })
     }
   } catch (err) {
-    console.error(`Error deleting message: ${JSON.stringify({ url, err })}`)
+    console.error('Error deleting messages for %s: %O', url, err)
   }
 }
 
