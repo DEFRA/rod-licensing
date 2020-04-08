6e22cf542050da21bf75615731e061874066d426
sgd <sgardnerdell-ea@users.noreply.github.com>
2020-04-08 12:54:45 +0100

    Add business-rules-lib package (#77)

---
+++ packages/business-rules-lib/src/index.js
@@ -0,0 +1,7 @@
+import * as contactValidation from './validators/contact.js'
+import * as permissionValidation from './validators/permission.js'
+
+export const validation = {
+  contact: contactValidation,
+  permission: permissionValidation
+}
