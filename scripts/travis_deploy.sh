dad53fb00011216110e05c42ee88b15166ec77be
sgardnerdell <sgardnerdell.ea@gmail.com>
2020-07-06 12:48:33 +0100

    Debug lerna versioning output

--- scripts/travis_deploy.sh
+++ scripts/travis_deploy.sh
@@ -63,8 +63,14 @@ echo "Generating changelog updates for all changes between ${PREVIOUS_VERSION} a
 lerna-changelog --from "${PREVIOUS_VERSION}" --to "${NEW_VERSION}" | cat - CHANGELOG.md > CHANGELOG.new && mv CHANGELOG.new CHANGELOG.md
 git commit -a --amend --no-edit --no-verify
 
+echo "Pushing new release to the remote"
+# TEMP: DEBUG
+git status
+git tag --list --sort=version:refname
+git log
+
 # Push new tag, updated changelog and package metadata to the remote
-git push
+git push origin "${TARGET_BRANCH}:${TARGET_BRANCH}"
 
 # Publish packages to npm
 lerna publish from-git --yes --no-git-reset --pre-dist-tag rc
