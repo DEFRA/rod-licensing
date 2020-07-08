671c9986ab224b84a9d6177b3ed9c03878ba5dbf
graham-willis-druid <graham.willis.ea@gmail.com>
2020-07-08 09:42:50 +0100

    Add ssl support

--- packages/gafl-webapp-service/src/services/payment/__test__/govuk-pay-service.spec.js
+++ packages/gafl-webapp-service/src/services/payment/__test__/govuk-pay-service.spec.js
@@ -4,18 +4,21 @@ import { AGREED } from '../../../uri.js'
 
 describe('The govuk-pay-service', () => {
   it('prepares a correct payment response endpoint for http', async () => {
-    process.env.GOV_PAY_HTTPS_REDIRECT = false
-    expect(preparePayment({ info: { host: '0.0.0.0:3000' } }, mockTransaction).return_url).toEqual('http://0.0.0.0:3000' + AGREED.uri)
+    expect(
+      preparePayment({ info: { host: '0.0.0.0:3000' }, headers: { 'x-forwarded-proto': 'http' } }, mockTransaction).return_url
+    ).toEqual('http://0.0.0.0:3000' + AGREED.uri)
   })
 
   it('prepares a correct payment response endpoint for https', async () => {
-    process.env.GOV_PAY_HTTPS_REDIRECT = true
-    expect(preparePayment({ info: { host: '0.0.0.0:3000' } }, mockTransaction).return_url).toEqual('https://0.0.0.0:3000' + AGREED.uri)
+    expect(
+      preparePayment({ info: { host: '0.0.0.0:3000' }, headers: { 'x-forwarded-proto': 'https' } }, mockTransaction).return_url
+    ).toEqual('https://0.0.0.0:3000' + AGREED.uri)
   })
 
   it('prepares a correct payment creation object', async () => {
-    process.env.GOV_PAY_HTTPS_REDIRECT = true
-    expect(preparePayment({ info: { host: '0.0.0.0:3000' } }, mockTransaction)).toEqual({
+    expect(
+      preparePayment({ info: { host: '0.0.0.0:3000' }, headers: {}, server: { info: { protocol: 'https' } } }, mockTransaction)
+    ).toEqual({
       amount: 5400,
       delayed_capture: false,
       description: 'Salmon 12 month 1 Rod Licence (Senior)',
@@ -35,10 +38,9 @@ describe('The govuk-pay-service', () => {
   })
 
   it('prepares a correct payment creation object - with locality', async () => {
-    process.env.GOV_PAY_HTTPS_REDIRECT = true
     const mockTransaction2 = Object.assign({}, mockTransaction)
     mockTransaction2.permissions[0].licensee.locality = 'Stoke Bishop'
-    expect(preparePayment({ info: { host: '0.0.0.0:3000' } }, mockTransaction2)).toEqual({
+    expect(preparePayment({ info: { host: '0.0.0.0:3000' }, headers: { 'x-forwarded-proto': 'https' } }, mockTransaction2)).toEqual({
       amount: 5400,
       delayed_capture: false,
       description: 'Salmon 12 month 1 Rod Licence (Senior)',
@@ -61,7 +63,7 @@ describe('The govuk-pay-service', () => {
   it('prepares a correct payment creation object where there are multiple licences', async () => {
     const newMockTransaction = Object.assign({}, mockTransaction)
     newMockTransaction.permissions.push(mockTransaction.permissions[0])
-    expect(preparePayment({ info: { host: '0.0.0.0:3000' } }, newMockTransaction)).toEqual({
+    expect(preparePayment({ info: { host: '0.0.0.0:3000' }, headers: { 'x-forwarded-proto': 'https' } }, newMockTransaction)).toEqual({
       amount: 5400,
       delayed_capture: false,
       description: 'Multiple permits',
@@ -71,7 +73,7 @@ describe('The govuk-pay-service', () => {
   })
 
   it('posts the prepared payment to the GOV.PAY api', async () => {
-    const preparedPayment = preparePayment({ info: { host: '0.0.0.0:3000' } }, mockTransaction)
+    const preparedPayment = preparePayment({ info: { host: '0.0.0.0:3000' }, headers: { 'x-forwarded-proto': 'https' } }, mockTransaction)
     console.log(preparedPayment)
   })
 })
