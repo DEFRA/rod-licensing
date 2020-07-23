616bad2d4c558c7ced30443987be68d7e59410dd
sgardnerdell <sgardnerdell.ea@gmail.com>
2020-07-23 14:53:49 +0100

    Add FTP server to Jenkins build scripts

--- scripts/docker/build.sh
+++ scripts/docker/build.sh
@@ -9,4 +9,5 @@ TAG_ARGUMENT="${1:-latest}"
 TAG=${TAG:-${TAG_ARGUMENT}}
 
 echo "Building images for tag ${TAG} using $(docker --version)"
+TAG=${TAG} docker-compose -f docker/infrastructure.yml build
 TAG=${TAG} docker-compose -f docker/services.build.yml build
