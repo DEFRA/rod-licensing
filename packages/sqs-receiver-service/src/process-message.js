2ed439ed45f979d67f81d5c1a9ef9f7971b58e85
sgardnerdell <sgardnerdell.ea@gmail.com>
2020-07-29 09:38:36 +0100

    Revise logging output to simplify tracing in AWS CloudWatch logs

--- packages/sqs-receiver-service/src/process-message.js
+++ packages/sqs-receiver-service/src/process-message.js
@@ -4,6 +4,17 @@ import db from 'debug'
 const debug = db('sqs:process-message')
 
 /**
+ * @typedef {Object} ProcessingResponse
+ * @property {!string} id The MessageId which generated this response
+ * @property {!string} handle The ReceiptHandler of the message
+ * @property {number} status The status-code returned by the subscriber
+ * @property {string} [statusText] The status-text for the status-code
+ * @property {string} requestBody The message that was sent to the subscriber in the body of the request
+ * @property {string} responseBody The message returned in the body of the response from the subscriber
+ * @property {Error} [error] Details of any exception which may have occurred
+ */
+
+/**
  * Posts what ever is in the body of the message to the subscriber. The message group id is added to the
  * subscriber url to allow the sent message to be directed to a specific endpoint
  *
@@ -17,8 +28,9 @@ const processMessage = async (message, subscriber, subscriberTimeoutMs) => {
     ? new URL(`${subscriber}/${message.Attributes.MessageGroupId}`)
     : new URL(subscriber)
 
+  let responseData
   try {
-    debug('Sending message to %s: %O', subscriberURL.toString(), message)
+    debug('Sending message to %s: %o', subscriberURL.toString(), message)
 
     const response = await fetch(subscriberURL, {
       method: 'post',
@@ -27,38 +39,32 @@ const processMessage = async (message, subscriber, subscriberTimeoutMs) => {
       timeout: subscriberTimeoutMs
     })
 
-    /**
-     * @typedef {Object} ProcessingResponse
-     * @property {!string} id The MessageId which generated this response
-     * @property {!string} handle The ReceiptHandler of the message
-     * @property {number} status The status-code returned by the subscriber
-     * @property {string} [statusText] The status-text for the status-code
-     * @property {string} message The message returned in the body of the response from the subscriber
-     */
-    const responseData = {
+    responseData = {
       id: message.MessageId,
       handle: message.ReceiptHandle,
       status: response.status,
       statusText: response.statusText,
-      message: await response.text()
+      requestBody: message.Body,
+      responseBody: await response.text()
     }
-    debug('Subscriber %s successfully processed message %s', subscriberURL.toString(), message.MessageId)
+    debug('Subscriber %s successfully processed message %o', subscriberURL.toString(), message)
 
     if (!response.ok) {
       // If we have an error log it and continue. We will not remove the message
-      console.error('Error from subscriber %s processing message %s: %O', subscriberURL.toString(), message.MessageId, responseData)
+      console.error('Error from subscriber %s processing message: %o', subscriberURL.toString(), responseData)
     }
-    return responseData
   } catch (err) {
-    console.error('Error sending message to subscriber %s: %O', subscriberURL.toString(), err)
-
-    return {
+    responseData = {
       id: message.MessageId,
       handle: message.ReceiptHandle,
       status: 500,
-      message: err.message
+      requestBody: message.Body,
+      responseBody: err.message,
+      error: err
     }
+    console.error('Error sending message to subscriber %s: %o', subscriberURL.toString(), responseData)
   }
+  return responseData
 }
 
 export default processMessage
