aba1106b0713ebaa4ad781a2a75bc1798309e275
sgardnerdell <sgardnerdell.ea@gmail.com>
2020-08-03 12:13:31 +0100

    Workaround for bug in adal-node failing to cache tokens and leaking memory
    - Issue described here: https://github.com/AzureAD/azure-activedirectory-library-for-nodejs/issues/239

--- packages/dynamics-lib/src/client/dynamics-client.js
+++ packages/dynamics-lib/src/client/dynamics-client.js
@@ -1,8 +1,18 @@
 import util from 'util'
 import DynamicsWebApi from 'dynamics-web-api'
 import AdalNode from 'adal-node'
+import Bottleneck from 'bottleneck'
 import db from 'debug'
 const debug = db('dynamics:auth')
+
+/*
+Bottleneck is used to prevent more than one request to retrieve a token from being executed concurrently. This is due to a bug in adal-node which
+adds multiple cache entries if acquireTokenWithClientCredentials is invoked more than once before the first call returns (asynchronously).
+Once this occurs, subsequent calls to the cache always fail as the cache finds more than one candidate to return.  Therefore more entries get added
+to the cache and it keeps growing forever.
+I have raised an issue with Microsoft here: https://github.com/AzureAD/azure-activedirectory-library-for-nodejs/issues/239
+ */
+const limiter = new Bottleneck({ maxConcurrent: 1 })
 
 export function config () {
   if (debug.enabled) {
@@ -15,7 +25,7 @@ export function config () {
 
   const authorityUrl = `${process.env.OAUTH_AUTHORITY_HOST_URL}${process.env.OAUTH_TENANT}`
   const adalContext = new AdalNode.AuthenticationContext(authorityUrl)
-  const acquireTokenWithClientCredentials = util.promisify(adalContext.acquireTokenWithClientCredentials).bind(adalContext)
+  const acquireTokenWithClientCredentials = limiter.wrap(util.promisify(adalContext.acquireTokenWithClientCredentials).bind(adalContext))
 
   return {
     webApiUrl: process.env.DYNAMICS_API_PATH,
