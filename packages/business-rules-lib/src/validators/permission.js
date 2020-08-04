a3f386b357bd19357bcbae4c2c7fd8233a43e2b0
sgardnerdell <sgardnerdell.ea@gmail.com>
2020-08-04 14:04:00 +0100

    Convert to uppercase before validating permission numbers

--- packages/business-rules-lib/src/validators/permission.js
+++ packages/business-rules-lib/src/validators/permission.js
@@ -2,6 +2,7 @@ export const createPermissionNumberValidator = joi =>
   joi
     .string()
     .trim()
+    .uppercase()
     .pattern(/^\d{8}-\d[A-Z]{2}\d[A-Z]{3}-[A-HJ-NP-Z0-9]{6}$/)
     .required()
     .description('The permission reference number')
