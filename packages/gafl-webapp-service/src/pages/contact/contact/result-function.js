6477edd6194d8f91d7a6e72fc658bc7d1a6c226a
Graham Willis <graham.willis.ea@gmail.com>
2020-09-08 09:17:05 +0100

    Remove redundant redirects from contact pages (#341)

--- packages/gafl-webapp-service/src/pages/contact/contact/result-function.js
+++ packages/gafl-webapp-service/src/pages/contact/contact/result-function.js
@@ -1,21 +1,6 @@
-import { HOW_CONTACTED } from '../../../processors/mapping-constants.js'
-import { CommonResults, CONTACT_SUMMARY_SEEN } from '../../../constants.js'
+import { CONTACT_SUMMARY_SEEN, CommonResults } from '../../../constants.js'
 
-/**
- * Returns rod license type troutAndCourse or salmonAndSeaTrout
- * @param request
- * @returns {Promise<string>}
- */
 export default async request => {
   const status = await request.cache().helpers.status.getCurrentPermission()
-  const { licensee } = await request.cache().helpers.transaction.getCurrentPermission()
-  let result = CommonResults.YES
-
-  if (status.fromSummary && status.fromSummary === CONTACT_SUMMARY_SEEN) {
-    result = CommonResults.SUMMARY
-  } else if ([HOW_CONTACTED.letter, HOW_CONTACTED.none].includes(licensee.preferredMethodOfConfirmation)) {
-    result = CommonResults.NO
-  }
-
-  return result
+  return status.fromSummary === CONTACT_SUMMARY_SEEN ? CommonResults.SUMMARY : CommonResults.OK
 }
