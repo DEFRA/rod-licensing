e856b6e698c039e62c69339551a2feb03d90c6ee
graham-willis-druid <graham.willis@environment-agency.gov.uk>
2020-09-14 14:04:21 +0100

    Fix sonar complexity issue

--- packages/gafl-webapp-service/src/pages/licence-details/licence-length/update-transaction.js
+++ packages/gafl-webapp-service/src/pages/licence-details/licence-length/update-transaction.js
@@ -7,20 +7,11 @@ import { SERVICE_LOCAL_TIME } from '@defra-fish/business-rules-lib'
 import { isPhysical } from '../../../processors/licence-type-display.js'
 import { licenceToStart } from '../licence-to-start/update-transaction.js'
 
-export const onLengthChange = permission => {
-  // If the licence start date has be chosen as today, and the licence is changed to a 12 month
-  // then set the start after payment flag
-  if (
-    permission.licenceLength === '12M' &&
-    moment(permission.licenceStartDate, cacheDateFormat)
-      .tz(SERVICE_LOCAL_TIME)
-      .isSame(moment(), 'day')
-  ) {
-    permission.licenceToStart = licenceToStart.AFTER_PAYMENT
-    permission.licenceStartTime = null
-  }
-
-  // If we have set the licence type to physical change the method of contact from 'none' to 'letter' and vice-versa
+/**
+ * If we have set the licence type to physical change the method of contact from 'none' to 'letter' and vice-versa
+ * @param permission
+ */
+const checkContactDetails = permission => {
   if (isPhysical(permission) && permission?.licensee?.preferredMethodOfConfirmation === mappings.HOW_CONTACTED.none) {
     permission.licensee.preferredMethodOfConfirmation = mappings.HOW_CONTACTED.letter
     permission.licensee.preferredMethodOfReminder = mappings.HOW_CONTACTED.letter
@@ -30,9 +21,30 @@ export const onLengthChange = permission => {
     permission.licensee.preferredMethodOfConfirmation = mappings.HOW_CONTACTED.none
     permission.licensee.preferredMethodOfReminder = mappings.HOW_CONTACTED.none
   }
+}
 
-  // If a disabled concession is set and the licence is moved to an 1/8 day then store the concession and restore it
-  // if the concession is set back
+/**
+ *  If the licence start date has be chosen as today, and the licence is changed to a
+ *  12 month then set the start after payment flag
+ * @param permission
+ */
+const checkLicenceToStart = permission => {
+  if (
+    permission.licenceLength === '12M' &&
+    moment(permission.licenceStartDate, cacheDateFormat)
+      .tz(SERVICE_LOCAL_TIME)
+      .isSame(moment(), 'day')
+  ) {
+    permission.licenceToStart = licenceToStart.AFTER_PAYMENT
+    permission.licenceStartTime = null
+  }
+}
+
+/*
+ * If a disabled concession is set and the licence is moved to an 1/8 day then store the concession and restore it
+ * if the concession is set back
+ */
+const checkDisabledConcessions = permission => {
   if (permission.licenceLength !== '12M' && concessionHelper.hasDisabled(permission)) {
     permission.previouslyDisabled = permission.concessions.find(c => c.type === mappings.CONCESSION.DISABLED)
     concessionHelper.removeDisabled(permission)
@@ -44,12 +56,17 @@ export const onLengthChange = permission => {
   }
 
   // For a trout and coarse, setting the licence length to anything other than 12 months sets the 2 rod licence
-  if (permission.licenceLength !== '12M') {
-    if (permission.licenceType === mappings.LICENCE_TYPE['trout-and-coarse']) {
-      permission.numberOfRods = '2'
-    }
+  if (permission.licenceLength !== '12M' && permission.licenceType === mappings.LICENCE_TYPE['trout-and-coarse']) {
+    permission.numberOfRods = '2'
   }
 }
+
+export const onLengthChange = permission => {
+  checkLicenceToStart(permission)
+  checkContactDetails(permission)
+  checkDisabledConcessions(permission)
+}
+
 /**
  * Transfer the validate page object
  * @param request
