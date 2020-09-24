7f2441da7db779b0e0e7fa5eb86366e9d69ce7db
Phil Benson <philbens@gmail.com>
2020-09-24 11:36:13 +0100

    Bugfix: Junior Licences not tracked as sales (#367)
    Ensure that Junior Licences are tracked by ecommerce analytics

--- packages/gafl-webapp-service/src/handlers/__tests__/agreed-handler-ga.spec.js
+++ packages/gafl-webapp-service/src/handlers/__tests__/agreed-handler-ga.spec.js
@@ -2,7 +2,7 @@ import { salesApi } from '@defra-fish/connectors-lib'
 
 import agreedHandler from '../agreed-handler.js'
 import { COMPLETION_STATUS } from '../../constants.js'
-import { getAffiliation } from '../../processors/analytics'
+import { getAffiliation, getTrackingProductDetailsFromTransaction } from '../../processors/analytics'
 import { ADULT_FULL_1_DAY_LICENCE } from '../../__mocks__/mock-journeys.js'
 
 const mockProductDetails = [
@@ -17,8 +17,19 @@ const mockProductDetails = [
   }
 ]
 
-jest.mock('@defra-fish/connectors-lib')
+const mockJuniorProductDetails = [
+  {
+    id: 'Salmon 1 Year Junior Red Licence',
+    name: 'Salmon and sea trout - Junior licence',
+    brand: 'Rod Fishing Licence',
+    category: 'Salmon and sea trout/1 rod/Junior',
+    variant: '12 Month(s)',
+    quantity: 1,
+    price: 0
+  }
+]
 
+jest.mock('@defra-fish/connectors-lib')
 jest.mock('../../processors/payment.js')
 jest.mock('../../services/payment/govuk-pay-service.js', () => ({
   sendPayment: () =>
@@ -73,7 +84,7 @@ describe('Google Analytics for agreed handler', () => {
   })
 
   it('sends purchase event with expected data when payment succeeds', async () => {
-    const request = getMockRequest(false)
+    const request = getMockRequest({ checkout: false })
 
     await agreedHandler(request, getMockResponseToolkit())
 
@@ -85,7 +96,7 @@ describe('Google Analytics for agreed handler', () => {
   })
 
   it.each(['zzz-999', 'xxx-123', 'thj-598'])('provides transaction identifier for purchase: %s', async samplePaymentId => {
-    const request = getMockRequest(false, samplePaymentId)
+    const request = getMockRequest({ checkout: false, payment_id: samplePaymentId })
 
     await agreedHandler(request, getMockResponseToolkit())
 
@@ -94,7 +105,7 @@ describe('Google Analytics for agreed handler', () => {
 
   it.each(['telesales', 'websales', 'door-to-door'])('passes channel to be transformed by affiliation', async sampleChannel => {
     process.env.CHANNEL = sampleChannel
-    const request = getMockRequest(false)
+    const request = getMockRequest({ checkout: false })
 
     await agreedHandler(request, getMockResponseToolkit())
 
@@ -103,7 +114,36 @@ describe('Google Analytics for agreed handler', () => {
 
   it.each(['telesales', 'websales', 'door-to-door'])('provides affiliation for purchase', async sampleChannel => {
     process.env.CHANNEL = sampleChannel
-    const request = getMockRequest(false)
+    const request = getMockRequest({ checkout: false })
+
+    await agreedHandler(request, getMockResponseToolkit())
+    const affiliation = getAffiliation.mock.results[0].value
+
+    expect(request.ga.ecommerce.mock.results[0].value.purchase).toHaveBeenCalledWith(expect.any(Array), expect.any(String), affiliation)
+  })
+
+  it('sends purchase event with expected data when transaction cost is zero', async () => {
+    const request = getMockRequest({ cost: 0 })
+    getTrackingProductDetailsFromTransaction.mockReturnValueOnce(mockJuniorProductDetails)
+
+    await agreedHandler(request, getMockResponseToolkit())
+
+    expect(request.ga.ecommerce.mock.results[0].value.purchase).toHaveBeenCalledWith(mockJuniorProductDetails, expect.any(String), expect.any(String))
+  })
+
+  it('sends session id as payment id when transaction cost is zero', async () => {
+    const sessionId = 'qwe-345-lkj-101'
+    const request = getMockRequest({ cost: 0, sessionId })
+    getTrackingProductDetailsFromTransaction.mockReturnValueOnce(mockJuniorProductDetails)
+
+    await agreedHandler(request, getMockResponseToolkit())
+
+    expect(request.ga.ecommerce.mock.results[0].value.purchase).toHaveBeenCalledWith(expect.any(Array), sessionId, expect.any(String))
+  })
+
+  it('sends purchase event with expected data when transaction cost is zero', async () => {
+    const request = getMockRequest({ cost: 0 })
+    getTrackingProductDetailsFromTransaction.mockReturnValueOnce(mockJuniorProductDetails)
 
     await agreedHandler(request, getMockResponseToolkit())
     const affiliation = getAffiliation.mock.results[0].value
@@ -112,7 +152,7 @@ describe('Google Analytics for agreed handler', () => {
   })
 })
 
-const getMockRequest = (checkout = true, payment_id = 'aaa111') => ({
+const getMockRequest = ({ checkout = true, payment_id = 'aaa111', cost = 1, sessionId = 'aaa111' } = {}) => ({
   cache: jest.fn(() => ({
     helpers: {
       status: {
@@ -149,11 +189,12 @@ const getMockRequest = (checkout = true, payment_id = 'aaa111') => ({
             }
           ],
           id: 'fff-111-eee-222',
-          cost: 1
+          cost
         }),
         set: () => {}
       }
-    }
+    },
+    getId: () => sessionId
   })),
   ga: {
     ecommerce: jest.fn(() => ({
