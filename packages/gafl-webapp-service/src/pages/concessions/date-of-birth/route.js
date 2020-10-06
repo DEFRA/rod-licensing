619f74b306ed1b060f92cc64dc19e0ec224baf46
Graham Willis <graham.willis.ea@gmail.com>
2020-10-06 10:11:23 +0100

    Ensure date validator schemas are created at the time of use (#404)
    * Ensure date validator schemas are created at the time of use
    
    * Add a test for the date validator change
    
    Co-authored-by: graham-willis-druid <graham.willis@environment-agency.gov.uk>

--- packages/gafl-webapp-service/src/pages/concessions/date-of-birth/route.js
+++ packages/gafl-webapp-service/src/pages/concessions/date-of-birth/route.js
@@ -4,13 +4,14 @@ import pageRoute from '../../../routes/page-route.js'
 import { validation } from '@defra-fish/business-rules-lib'
 import { nextPage } from '../../../routes/next-page.js'
 
-const schema = Joi.object({
-  'date-of-birth': validation.contact.createBirthDateValidator(Joi)
-})
-
 const validator = payload => {
   const dateOfBirth = `${payload['date-of-birth-year']}-${payload['date-of-birth-month']}-${payload['date-of-birth-day']}`
-  Joi.assert({ 'date-of-birth': dateOfBirth }, schema)
+  Joi.assert(
+    { 'date-of-birth': dateOfBirth },
+    Joi.object({
+      'date-of-birth': validation.contact.createBirthDateValidator(Joi)
+    })
+  )
 }
 
 export default pageRoute(DATE_OF_BIRTH.page, DATE_OF_BIRTH.uri, validator, nextPage)
