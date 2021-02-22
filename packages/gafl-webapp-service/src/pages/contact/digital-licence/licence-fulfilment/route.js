4b62b95c24cc6d4ab7a512403fbe054b7b31cb10
nabeelamir-defra <nabeel.amir@defra.gov.uk>
2021-02-22 11:04:04 +0000

    removed empty functions

--- packages/gafl-webapp-service/src/pages/contact/digital-licence/licence-fulfilment/route.js
+++ packages/gafl-webapp-service/src/pages/contact/digital-licence/licence-fulfilment/route.js
@@ -2,6 +2,4 @@ import pageRoute from '../../../../routes/page-route.js'
 import { LICENCE_FULFILMENT } from '../../../../uri.js'
 import { nextPage } from '../../../../routes/next-page.js'
 
-const validator = () => {}
-const getData = () => {}
-export default pageRoute(LICENCE_FULFILMENT.page, LICENCE_FULFILMENT.uri, validator, nextPage, getData)
+export default pageRoute(LICENCE_FULFILMENT.page, LICENCE_FULFILMENT.uri, undefined, nextPage, undefined)
