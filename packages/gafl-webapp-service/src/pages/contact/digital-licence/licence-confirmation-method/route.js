5738c983d73899b56deefe3d3fc3b06c6a25d01c
nabeelamir-defra <73947018+nabeelamir-defra@users.noreply.github.com>
2021-03-17 15:53:19 +0000

    Licence confirmation method paper (#615)
    * Licence confirmation method paper
    
    https://eaflood.atlassian.net/browse/IWTF-1963
    
    Licence confirmation method screen if selecting paper licence
    
    * refactored licence-confirmation-method to allow paper options

--- packages/gafl-webapp-service/src/pages/contact/digital-licence/licence-confirmation-method/route.js
+++ packages/gafl-webapp-service/src/pages/contact/digital-licence/licence-confirmation-method/route.js
@@ -1,54 +1,42 @@
 import Joi from 'joi'
 import { validation } from '@defra-fish/business-rules-lib'
 
-import { LICENCE_CONFIRMATION_METHOD, DATE_OF_BIRTH, LICENCE_TO_START, LICENCE_LENGTH } from '../../../../uri.js'
+import { CONTACT, LICENCE_CONFIRMATION_METHOD } from '../../../../uri.js'
 import pageRoute from '../../../../routes/page-route.js'
 import GetDataRedirect from '../../../../handlers/get-data-redirect.js'
 import { nextPage } from '../../../../routes/next-page.js'
 import { HOW_CONTACTED } from '../../../../processors/mapping-constants.js'
+import { isPhysical } from '../../../../processors/licence-type-display.js'
+import { mobilePhoneValidator } from '../../../../processors/contact-validator.js'
 
 const getData = async request => {
   const permission = await request.cache().helpers.transaction.getCurrentPermission()
 
-  // We need to have set the licence length, dob and start date here to determining the contact
-  // messaging
-  if (!permission.licensee.birthDate) {
-    throw new GetDataRedirect(DATE_OF_BIRTH.uri)
+  // page is only permitted for physical licences
+  if (!isPhysical(permission)) {
+    throw new GetDataRedirect(CONTACT.uri)
   }
 
-  if (!permission.licenceStartDate) {
-    throw new GetDataRedirect(LICENCE_TO_START.uri)
-  }
-
-  if (!permission.licenceLength) {
-    throw new GetDataRedirect(LICENCE_LENGTH.uri)
-  }
-
+  const { licensee } = permission
   return {
     licensee: permission.licensee,
-    howContacted: HOW_CONTACTED
+    howContacted: HOW_CONTACTED,
+    postalFulfilment: licensee.postalFulfilment
   }
 }
 
-export const mobilePhoneRegex = /^((\+44)(\s?)|(0))(7\d{3})(\s?)(\d{3})(\s?)(\d{3})$/
-export const mobilePhoneValidator = Joi.string()
-  .trim()
-  .pattern(mobilePhoneRegex)
-  .replace(mobilePhoneRegex, '$2$4$5$7$9')
-  .example('+44 7700 900088')
-
 const validator = Joi.object({
   'licence-confirmation-method': Joi.string()
-    .valid('email', 'text')
+    .valid('email', 'text', 'none')
     .required(),
-  email: Joi.alternatives().conditional('licence-confirmation-method', {
-    is: 'email',
-    then: validation.contact.createEmailValidator(Joi),
-    otherwise: Joi.string().empty('')
-  }),
   text: Joi.alternatives().conditional('licence-confirmation-method', {
     is: 'text',
     then: mobilePhoneValidator,
+    otherwise: Joi.string().empty('')
+  }),
+  email: Joi.alternatives().conditional('licence-confirmation-method', {
+    is: 'email',
+    then: validation.contact.createEmailValidator(Joi),
     otherwise: Joi.string().empty('')
   })
 }).options({ abortEarly: false, allowUnknown: true })
