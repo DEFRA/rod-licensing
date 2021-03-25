d54f2cb784a29cb08ae9d5f1771f20034bacf14d
nabeelamir-defra <nabeel.amir@defra.gov.uk>
2021-03-25 12:59:56 +0000

    refactored summary page

--- packages/gafl-webapp-service/src/pages/summary/contact-summary/route.js
+++ packages/gafl-webapp-service/src/pages/summary/contact-summary/route.js
@@ -6,8 +6,10 @@ import { CONTACT_SUMMARY_SEEN } from '../../../constants.js'
 import { isPhysical } from '../../../processors/licence-type-display.js'
 import { nextPage } from '../../../routes/next-page.js'
 
-import { CONTACT_SUMMARY, LICENCE_SUMMARY, NAME, ADDRESS_ENTRY, ADDRESS_SELECT, 
-  ADDRESS_LOOKUP, CONTACT, NEWSLETTER, LICENCE_FULFILMENT, LICENCE_CONFIRMATION_METHOD } from '../../../uri.js'
+import {
+  CONTACT_SUMMARY, LICENCE_SUMMARY, NAME, ADDRESS_ENTRY, ADDRESS_SELECT,
+  ADDRESS_LOOKUP, CONTACT, NEWSLETTER, LICENCE_FULFILMENT, LICENCE_CONFIRMATION_METHOD
+} from '../../../uri.js'
 
 const getData = async request => {
   const status = await request.cache().helpers.status.getCurrentPermission()
@@ -44,6 +46,7 @@ const getData = async request => {
     reminderMethod: permission.licensee.preferredMethodOfReminder,
     newsLetter: permission.licensee.preferredMethodOfNewsletter !== HOW_CONTACTED.none,
     howContacted: HOW_CONTACTED,
+    summary: getLicenseeDetailsSummaryRows(permission, countryName),
     uri: {
       name: NAME.uri,
       address: ADDRESS_LOOKUP.uri, // Encourage the address lookup on an amendment
@@ -57,3 +60,78 @@ const getData = async request => {
 }
 
 export default pageRoute(CONTACT_SUMMARY.page, CONTACT_SUMMARY.uri, null, nextPage, getData)
+
+const getLicenseeDetailsSummaryRows = (permission, countryName) => {
+  return [
+    getRow('Name', permission.licensee.firstName + ' ' + permission.licensee.lastName, NAME.uri, 'name', 'change-name'),
+    getRow('Address', getAddressText(permission.licensee, countryName), ADDRESS_LOOKUP.uri, 'address', 'change-address'),
+    ...getContactDetails(permission),
+    getRow('Newsletter', permission.licensee.preferredMethodOfNewsletter === HOW_CONTACTED.none ? 'No' : 'Yes', NEWSLETTER.uri, 'newsletter', 'change-newsletter')
+  ]
+}
+
+const getContactDetails = (permission) => {
+  if (isPhysical(permission) && permission.licensee.postalFulfilment === 'Yes') {
+    return [
+      getRow('Licence Confirmation',
+        getContactText(permission.licensee.preferredMethodOfConfirmation, permission.licensee),
+        LICENCE_CONFIRMATION_METHOD.uri, 'licence confirmation option', 'licence-confirmation-option'),
+      getRow('Contact',
+        getContactText(permission.licensee.preferredMethodOfReminder, permission.licensee, 'By post', 'Text messages to '),
+        CONTACT.uri, 'contact', 'change-contact')
+    ]
+  } else if (isPhysical(permission) && permission.licensee.postalFulfilment === 'No') {
+    return [
+      getRow('Licence',
+        getContactText(permission.licensee.preferredMethodOfConfirmation, permission.licensee),
+        LICENCE_CONFIRMATION_METHOD.uri, 'licence confirmation option', 'licence-confirmation-option'),
+      getRow('Contact',
+        getContactText(permission.licensee.preferredMethodOfReminder, permission.licensee, 'By post', 'Text messages to '),
+        CONTACT.uri, 'contact', 'change-contact')
+    ]
+  } else {
+    return [
+      getRow('Licence details',
+        getContactText(permission.licensee.preferredMethodOfReminder, permission.licensee, 'Make a note on confirmation', 'Text messages to '),
+        CONTACT.uri, 'contact', 'change-contact')
+    ]
+  }
+}
+
+const getAddressText = (licensee, countryName) => {
+  return `${licensee.premises}, ${licensee.street}, ${licensee.locality}, ${licensee.town}, ${licensee.postcode} ${countryName.toUpperCase()}`
+}
+
+const getContactText = (contactMethod, licensee, defaultMessage = 'Note of licence', textMessage = 'Text message to ') => {
+  switch (contactMethod) {
+    case HOW_CONTACTED.email:
+      return 'Email to ' + licensee.email
+    case HOW_CONTACTED.text:
+      return textMessage + licensee.mobilePhone
+    default:
+      return defaultMessage
+  }
+}
+
+const getRow = (label, text, href, visuallyHiddenText, id) => {
+  return {
+    key: {
+      text: label
+    },
+    value: {
+      text
+    },
+    ...(href) && {
+      actions: {
+        items: [
+          {
+            href,
+            text: 'Change',
+            visuallyHiddenText: visuallyHiddenText,
+            attributes: { id }
+          }
+        ]
+      }
+    }
+  }
+}
