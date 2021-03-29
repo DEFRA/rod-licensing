7de07328514bd3b8117f00127bc21694d5149fcd
Phil Benson <phil.benson@defra.gov.uk>
2021-03-29 14:49:27 +0100

    Remove UK as an option, reorder other home nations

---
+++ packages/gafl-webapp-service/src/processors/__tests__/refdata-helper.spec.js
@@ -0,0 +1,60 @@
+import { salesApi } from '@defra-fish/connectors-lib'
+import { countries } from '../refdata-helper.js'
+
+jest.mock('@defra-fish/connectors-lib', () => ({
+  salesApi: {
+    countries: {
+      getAll: async () => ({
+        options: {
+          DE: { description: 'DE', label: 'Germany' },
+          NRI: { description: 'GB-NIR', label: 'Northern Ireland' },
+          FR: { description: 'FR', label: 'France' },
+          SCO: { description: 'GB-SCT', label: 'Scotland' },
+          WAL: { description: 'GB-WLS', label: 'Wales' },
+          GB: { description: 'GB', label: 'United Kingdom' },
+          ENG: { description: 'GB-ENG', label: 'England' },
+          IT: { description: 'IT', label: 'Italy' },
+          ES: { description: 'ES', label: 'Spain' }
+        }
+      })
+    }
+  }
+}))
+
+describe('Countries list', () => {
+  it('includes home nations', async () => {
+    const c = await countries.getAll()
+    const homeNations = [
+      expect.objectContaining({ code: 'GB-ENG', name: 'England' }),
+      expect.objectContaining({ code: 'GB-WLS', name: 'Wales' }),
+      expect.objectContaining({ code: 'GB-SCT', name: 'Scotland' }),
+      expect.objectContaining({ code: 'GB-NIR', name: 'Northern Ireland' })
+    ]
+    expect(c).toEqual(expect.arrayContaining(homeNations))
+  })
+
+  it('Excludes UK', async () => {
+    const c = await countries.getAll()
+    expect(c).not.toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ code: 'GB' })
+      ])
+    )
+  })
+
+  it('Lists countries in correct order', async () => {
+    const [first, second, third, fourth] = await countries.getAll()
+    expect(first).toEqual(
+      expect.objectContaining({ code: 'GB-ENG', name: 'England' })
+    )
+    expect(second).toEqual(
+      expect.objectContaining({ code: 'GB-WLS', name: 'Wales' })
+    )
+    expect(third).toEqual(
+      expect.objectContaining({ code: 'GB-SCT', name: 'Scotland' })
+    )
+    expect(fourth).toEqual(
+      expect.objectContaining({ code: 'GB-NIR', name: 'Northern Ireland' })
+    )
+  })
+})
