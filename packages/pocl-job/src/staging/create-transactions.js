3e33063f31cb55a3d31b14b44fb05de4f750f255
Dana Gajewska <danusia.x@outlook.com>
2021-05-06 15:08:05 +0100

    Adds logging to pocl staging

--- packages/pocl-job/src/staging/create-transactions.js
+++ packages/pocl-job/src/staging/create-transactions.js
@@ -70,8 +70,9 @@ const getInitialState = async filename => {
  */
 const createTransactionsInSalesApi = async (filename, state) => {
   if (state.buffer.length) {
+    debug({buffer:state.buffer})
     const createResults = await salesApi.createTransactions(state.buffer.map(item => item.createTransactionPayload))
-
+    debug({createResults})
     const succeeded = []
     const failed = []
     state.buffer.forEach((record, idx) => {
