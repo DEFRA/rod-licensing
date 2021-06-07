b6a884d04892dd17bdb518f3fcedd899f6b65dc2
nabeelamir-defra <nabeel.amir@defra.gov.uk>
2021-06-07 16:21:31 +0100

    added unit test for licence information page

--- packages/gafl-webapp-service/src/pages/order-complete/licence-information/route.js
+++ packages/gafl-webapp-service/src/pages/order-complete/licence-information/route.js
@@ -1,12 +1,28 @@
-import pageRoute from '../../../routes/page-route.js'
+import Boom from '@hapi/boom'
 
+import pageRoute from '../../../routes/page-route.js'
 import { LICENCE_INFORMATION } from '../../../uri.js'
+import { COMPLETION_STATUS } from '../../../constants.js'
 import { nextPage } from '../../../routes/next-page.js'
 import { licenceTypeDisplay } from '../../../processors/licence-type-display.js'
 import { displayStartTime, displayEndTime } from '../../../processors/date-and-time-display.js'
 import * as concessionHelper from '../../../processors/concession-helper.js'
 
 const getData = async request => {
+  const status = await request.cache().helpers.status.get()
+
+  if (!status[COMPLETION_STATUS.agreed]) {
+    throw Boom.forbidden('Attempt to access the licence information handler with no agreed flag set')
+  }
+
+  if (!status[COMPLETION_STATUS.posted]) {
+    throw Boom.forbidden('Attempt to access the licence information handler with no posted flag set')
+  }
+
+  if (!status[COMPLETION_STATUS.finalised]) {
+    throw Boom.forbidden('Attempt to access the licence information handler with no finalised flag set')
+  }
+
   const permission = await request.cache().helpers.transaction.getCurrentPermission()
 
   const startTimeString = displayStartTime(permission)
