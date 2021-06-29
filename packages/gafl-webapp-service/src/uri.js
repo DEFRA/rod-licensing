db3d43b7218a75292df912164517d01025d60399
nabeelamir-defra <73947018+nabeelamir-defra@users.noreply.github.com>
2021-06-29 10:27:32 +0100

    pdf licences downloading with the incorrect file type (#789)
    * pdf licences downloading with the incorrect file type
    
    https://eaflood.atlassian.net/browse/IWTF-2265
    
    pdf licences downloading with the incorrect file type
    
    * corrected extension of pdf licence

--- packages/gafl-webapp-service/src/uri.js
+++ packages/gafl-webapp-service/src/uri.js
@@ -25,7 +25,7 @@ export const PAYMENT_FAILED = { uri: '/buy/payment-failed', page: 'payment-faile
 
 export const TERMS_AND_CONDITIONS = { uri: '/buy/terms-conditions', page: 'terms-and-conditions' }
 export const ORDER_COMPLETE = { uri: '/buy/order-complete', page: 'order-complete' }
-export const ORDER_COMPLETE_PDF = { uri: '/buy/order-complete-pdf' }
+export const ORDER_COMPLETE_PDF = { uri: '/buy/order-complete.pdf' }
 export const ATTRIBUTION = { uri: '/buy/attribution' }
 
 export const OIDC_SIGNIN = { uri: '/oidc/signin' }
