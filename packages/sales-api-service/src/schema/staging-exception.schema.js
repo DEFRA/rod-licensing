86cc08cdfb18192cf98a6c86883cb048a9a4f3da
Dana Gajewska <danusia.x@outlook.com>
2021-07-02 11:42:15 +0100

    Comments out schema 'or' validation

--- packages/sales-api-service/src/schema/staging-exception.schema.js
+++ packages/sales-api-service/src/schema/staging-exception.schema.js
@@ -28,7 +28,7 @@ const schemaObject = {
  * @type {Joi.AnySchema}
  */
 export const createStagingExceptionRequestSchema = Joi.object(schemaObject)
-  .or('stagingException', 'transactionFileException', 'record')
+  // .or('stagingException', 'transactionFileException', 'record')
   .label('create-staging-exception-request')
 
 /**
