06098ad02a7f1473a8b97278bae05c50cff681fc
Dana Gajewska <danusia.x@outlook.com>
2021-07-05 15:53:03 +0100

    Fixes concession mapping to validation error entity, for real

--- packages/sales-api-service/src/services/exceptions/exceptions.service.js
+++ packages/sales-api-service/src/services/exceptions/exceptions.service.js
@@ -74,10 +74,10 @@ export const createTransactionFileException = async transactionFileError => {
 const getJSONString = obj => JSON.stringify(obj, null, 2)
 
 const getConcessions = (concessions) => {
-  const blueBadgeConcession = getJSONString(concessions.find(c => c.type === BLUE_BADGE))
-  const pipConcession = getJSONString(concessions.find(c => c.type === NATIONAL_INSURANCE_NUMBER))
+  const blueBadgeConcession = getJSONString(concessions.find(c => c.proof.type === BLUE_BADGE))
+  const pipConcession = getJSONString(concessions.find(c => c.proof.type === NATIONAL_INSURANCE_NUMBER))
   // all other types of concessions are classified as senior concessions
-  const seniorConcession = getJSONString(concessions.find(c => ![BLUE_BADGE, NATIONAL_INSURANCE_NUMBER].includes(c.type)))
+  const seniorConcession = getJSONString(concessions.find(c => ![BLUE_BADGE, NATIONAL_INSURANCE_NUMBER].includes(c.proof.type)))
 
   return {
     ...blueBadgeConcession && { blueBadgeConcession },
