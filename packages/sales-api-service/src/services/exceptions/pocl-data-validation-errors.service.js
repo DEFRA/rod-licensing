421ff59260a86e4ccc42ba8fd3d9c3c67f330782
Dana Gajewska <danusia.x@outlook.com>
2021-07-26 15:32:10 +0100

    Adds logging and testing filers

--- packages/sales-api-service/src/services/exceptions/pocl-data-validation-errors.service.js
+++ packages/sales-api-service/src/services/exceptions/pocl-data-validation-errors.service.js
@@ -1,13 +1,13 @@
 import { dynamicsClient } from '@defra-fish/dynamics-lib'
 import { PoclDataValidationError } from './temp/pocl-data-validation-error.entity.js'
-const READY_FOR_PROCESSING_ID = 910400001
+const READY_FOR_PROCESSING_ID = 910400000
 
 const getRecords = async () => {
   const filters = [
     `${PoclDataValidationError.definition.mappings.status.field} eq ${READY_FOR_PROCESSING_ID}`,
     `${PoclDataValidationError.definition.defaultFilter}`
   ]
-
+  console.log({ filters })
   // perform a multiple records retrieve operation
   return dynamicsClient.retrieveMultipleRequest(PoclDataValidationError.definition.toRetrieveRequest(filters.join(' and ')))
 }
@@ -20,6 +20,7 @@ const getRecords = async () => {
 export async function getPoclValidationErrors () {
   console.log('about to retrieve validation errors')
   const validationErrors = await getRecords()
+  console.log({ validationErrors })
   const recordsReadyForProcessing = validationErrors.filter(record => {
     console.log({ record, status: record.status })
     return record.status.label === 'Needs Review'
