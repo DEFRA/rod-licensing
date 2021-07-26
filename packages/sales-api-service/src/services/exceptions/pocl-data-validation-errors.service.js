aeff3593c429010282dc6133287ed56bd604f018
Dana Gajewska <danusia.x@outlook.com>
2021-07-26 15:14:21 +0100

    Adds bespoke call to dynamics with filter

--- packages/sales-api-service/src/services/exceptions/pocl-data-validation-errors.service.js
+++ packages/sales-api-service/src/services/exceptions/pocl-data-validation-errors.service.js
@@ -1,5 +1,17 @@
-import { retrieveMultiple } from '@defra-fish/dynamics-lib'
+import { dynamicsClient } from '@defra-fish/dynamics-lib'
 import { PoclDataValidationError } from './temp/pocl-data-validation-error.entity.js'
+const READY_FOR_PROCESSING_ID = 910400001
+
+const getRecords = async () => {
+  const filters = [
+    `${PoclDataValidationError.definition.mappings.status.field} eq ${READY_FOR_PROCESSING_ID}`,
+    `${PoclDataValidationError.definition.defaultFilter}`
+  ]
+  
+  // perform a multiple records retrieve operation
+  return dynamicsClient.retrieveMultipleRequest(PoclDataValidationError.definition.toRetrieveRequest(filters.join(' and ')))
+}
+
 /**
  * Query for POCL data validation errors with 'Ready for Processing' status
  * @param {*} payload
@@ -7,7 +19,8 @@ import { PoclDataValidationError } from './temp/pocl-data-validation-error.entit
  */
 export async function getPoclValidationErrors () {
   console.log('about to retrieve validation errors')
-  const validationErrors = await retrieveMultiple(PoclDataValidationError).cached()
+  const
+  const validationErrors = await getRecords()
   const recordsReadyForProcessing = validationErrors.filter(record => {
     console.log({ record, status: record.status })
     return record.status.label === 'Needs Review'
