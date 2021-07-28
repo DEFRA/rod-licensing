5453181367266f66701e56c3cdddc531e06b95f3
Dana Gajewska <danusia.x@outlook.com>
2021-07-28 10:30:47 +0100

    Adds executeQuery call

--- packages/sales-api-service/src/services/exceptions/pocl-data-validation-errors.service.js
+++ packages/sales-api-service/src/services/exceptions/pocl-data-validation-errors.service.js
@@ -1,24 +1,12 @@
-import { dynamicsClient, findPoclValidationErrors } from '@defra-fish/dynamics-lib'
+import { executeQuery, findPoclValidationErrors } from '@defra-fish/dynamics-lib'
 import { getGlobalOptionSetValue } from '../reference-data.service.js'
 
 const POCL_VALIDATION_ERROR_STATUS_OPTIONSET = 'defra_poclvalidationerrorstatus'
 
-// @IWTF-2175: Remove after testing
-// import { PoclValidationError } from './temp/pocl-data-validation-error.entity.js'
-
-// const getRecords = async () => {
-
-//   const filters = [
-//     `${PoclValidationError.definition.mappings.status.field} eq ${status.id}`,
-//     `${PoclValidationError.definition.defaultFilter}`
-//   ]
-
-//   const { value } = await dynamicsClient.retrieveMultipleRequest(
-//     PoclValidationError.definition.toRetrieveRequest(filters.join(' and '))
-//   )
-//   console.log(`Retrieved ${value.length} records`, value)
-//   return value
-// }
+const getRecords = async () => {
+  const status = await getGlobalOptionSetValue(POCL_VALIDATION_ERROR_STATUS_OPTIONSET, 'Ready for Processing')
+  return executeQuery(findPoclValidationErrors(status))
+}
 
 /**
  * Query for POCL data validation errors with 'Ready for Processing' status
@@ -27,8 +15,7 @@ const POCL_VALIDATION_ERROR_STATUS_OPTIONSET = 'defra_poclvalidationerrorstatus'
  */
 export async function getPoclValidationErrors () {
   console.log('about to retrieve validation errors')
-  const status = await getGlobalOptionSetValue(POCL_VALIDATION_ERROR_STATUS_OPTIONSET, 'Ready for Processing')
-  const records = await findPoclValidationErrors(status)
+  const records = await getRecords()
   console.log(`Retrieved ${records.length} records`, records)
   return records
 }
