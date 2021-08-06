87ae74e2448f00ba768ae911e0b69ed450e205d9
Dana Gajewska <danusia.x@outlook.com>
2021-08-06 10:24:20 +0100

    Increases test coverage. Makes code consistent

--- packages/pocl-job/src/staging/__tests__/pocl-validation-errors.spec.js
+++ packages/pocl-job/src/staging/__tests__/pocl-validation-errors.spec.js
@@ -39,12 +39,41 @@ const getPoclValidationError = () => ({
   status: { label: 'Ready for Processing' }
 })
 
+const getFinalisationError = (status, error) => ({
+  status,
+  error,
+  message: 'The transaction has already been finalised',
+  body: {
+    data: { status: { id: 'FINALISED', messageId: 'message2' } }
+  }
+})
+
 describe('pocl-validation-errors', () => {
   beforeEach(jest.clearAllMocks)
 
   it('retrieves validation errors from the Sales Api', async () => {
     await processPoclValidationErrors()
     expect(salesApi.getPoclValidationErrorsForProcessing).toBeCalled()
+  })
+
+  describe('when no validation errors are returned from Sales Api', () => {
+    let result
+    beforeEach(async () => {
+      salesApi.getPoclValidationErrorsForProcessing.mockResolvedValue(null)
+      result = await processPoclValidationErrors()
+    })
+
+    it('returns undefined', () => {
+      expect(result).toBe(undefined)
+    })
+
+    it('does not attempt to create transactions', () => {
+      expect(salesApi.createTransactions).not.toBeCalled()
+    })
+
+    it('does not finalise transactions which have failed', () => {
+      expect(salesApi.finaliseTransaction).not.toBeCalled()
+    })
   })
 
   describe('processes successfully fixed validation errors', () => {
@@ -61,12 +90,20 @@ describe('pocl-validation-errors', () => {
       expect(validationError).toMatchSnapshot()
     })
 
-    it('finalises transaction the transaction in the Sales Api', () => {
+    it('finalises the transaction in the Sales Api', () => {
       const record = salesApi.finaliseTransaction.mock.calls[0]
       expect(record).toMatchSnapshot()
     })
 
     it('updates POCL validation error record in the Sales Api', () => {
+      const record = salesApi.updatePoclValidationError.mock.calls[0]
+      expect(record).toMatchSnapshot()
+    })
+
+    it('treats 410 Gone errors from the Sales API as a success', async () => {
+      salesApi.finaliseTransaction.mockRejectedValueOnce(getFinalisationError(410, 'Gone'))
+      await processPoclValidationErrors()
+
       const record = salesApi.updatePoclValidationError.mock.calls[0]
       expect(record).toMatchSnapshot()
     })
@@ -89,6 +126,33 @@ describe('pocl-validation-errors', () => {
 
     it('does not finalise transactions which have failed', () => {
       expect(salesApi.finaliseTransaction).not.toBeCalled()
+    })
+
+    it('updates POCL validation error record in the Sales Api', () => {
+      const record = salesApi.updatePoclValidationError.mock.calls[0]
+      expect(record).toMatchSnapshot()
+    })
+  })
+
+  describe('processes records which failed during finalising', () => {
+    let poclValidationError
+    beforeEach(async () => {
+      poclValidationError = getPoclValidationError()
+      poclValidationError.email = 'daniel-ricc@example.couk'
+      salesApi.getPoclValidationErrorsForProcessing.mockResolvedValue([poclValidationError])
+      salesApi.createTransactions.mockResolvedValue([{ statusCode: 201, response: { id: 'test-response-id' } }])
+      salesApi.finaliseTransaction.mockRejectedValueOnce(getFinalisationError(500, 'Internal server error'))
+      await processPoclValidationErrors()
+    })
+
+    it('creates transaction in the Sales Api', () => {
+      const validationError = salesApi.createTransactions.mock.calls[0][0][0]
+      expect(validationError).toMatchSnapshot()
+    })
+
+    it('attempts to finalise transaction in the Sales Api', () => {
+      const record = salesApi.finaliseTransaction.mock.calls[0]
+      expect(record).toMatchSnapshot()
     })
 
     it('updates POCL validation error record in the Sales Api', () => {
