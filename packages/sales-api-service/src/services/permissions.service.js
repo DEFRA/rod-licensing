c330b4f463176d1da4c429105e5316406537d8b5
Phil Benson <phil.benson@defra.gov.uk>
2021-08-11 07:49:18 +0100

    remove unused export

--- packages/sales-api-service/src/services/permissions.service.js
+++ packages/sales-api-service/src/services/permissions.service.js
@@ -12,13 +12,6 @@ const DICTIONARIES = [
   'ABCDEFGHJKLMNPQRSTUVWXYZ1234567890'
 ]
 
-export const logStartDateError = permission => {
-  const { startDate, issueDate } = permission
-  if (moment(startDate).isBefore(issueDate || undefined)) {
-    console.error('permission start date before current time: ', permission)
-  }
-}
-
 /**
  * Generate a new permission number
  *
