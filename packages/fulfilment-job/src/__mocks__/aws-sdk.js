a1365a9d68c7dc44aa4e0726b9c1a1442aa7e8fc
Phil Benson <phil.benson@defra.gov.uk>
2021-08-23 16:32:57 +0100

    correct ref path to aws-mock-helper

--- packages/fulfilment-job/src/__mocks__/aws-sdk.js
+++ packages/fulfilment-job/src/__mocks__/aws-sdk.js
@@ -1,3 +1,3 @@
-import { configureAwsSdkMock } from '../../../connectors-lib/src/__mocks__/aws-mock-helper.js'
+import { configureAwsSdkMock } from '@defra-fish/connectors-lib/src/__mocks__/aws-mock-helper.js'
 const AwsSdk = configureAwsSdkMock(jest.genMockFromModule('aws-sdk'))
 export default AwsSdk
