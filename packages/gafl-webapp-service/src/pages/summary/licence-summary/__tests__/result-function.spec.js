73383a477b41a57cd1609d4a4dd7bab7c44bf001
nabeelamir-defra <nabeel.amir@defra.gov.uk>
2021-08-24 12:13:24 +0100

    cleaned up code

--- packages/gafl-webapp-service/src/pages/summary/licence-summary/__tests__/result-function.spec.js
+++ packages/gafl-webapp-service/src/pages/summary/licence-summary/__tests__/result-function.spec.js
@@ -1,5 +1,5 @@
 import resultFunction from '../result-function'
-import { CONTACT_SUMMARY_SEEN, CommonResults, showDigitalLicencePages } from '../../../../constants.js'
+import { CommonResults, showDigitalLicencePages } from '../../../../constants.js'
 
 describe('licence-summary > result-function', () => {
   const mockStatusCacheGet = jest.fn()
@@ -22,7 +22,7 @@ describe('licence-summary > result-function', () => {
     beforeEach(jest.clearAllMocks)
 
     it('should return the digital licence screen, if licence is in renewal, is 12 months and postal fulfilment is true', async () => {
-      mockStatusCacheGet.mockImplementationOnce(() => ({renewal: true}))
+      mockStatusCacheGet.mockImplementationOnce(() => ({ renewal: true }))
       mockTransactionPageGet.mockImplementationOnce(() => ({
         licenceLength: '12M',
         licensee: {
@@ -46,7 +46,7 @@ describe('licence-summary > result-function', () => {
     })
 
     it('should return summary if from summary', async () => {
-      mockStatusCacheGet.mockImplementationOnce(() => ({fromSummary: true}))
+      mockStatusCacheGet.mockImplementationOnce(() => ({ fromSummary: true }))
       mockTransactionPageGet.mockImplementationOnce(() => ({}))
       const result = await resultFunction(mockRequest)
       expect(result).toBe(CommonResults.OK)
