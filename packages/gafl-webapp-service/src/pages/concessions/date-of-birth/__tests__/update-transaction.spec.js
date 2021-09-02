4bb39dd4ee7202c9a83d8671d8f4b690591bdf7e
Dana Gajewska <danusia.x@outlook.com>
2021-09-02 13:58:25 +0100

    Adds error logging when page cache does not match payload
    IWTF-2349

---
+++ packages/gafl-webapp-service/src/pages/concessions/date-of-birth/__tests__/update-transaction.spec.js
@@ -0,0 +1,94 @@
+import { DATE_OF_BIRTH } from '../../../../uri.js'
+import updateTransaction from '../update-transaction.js'
+import { ageConcessionHelper } from '../../../../processors/concession-helper.js'
+import { onLengthChange } from '../../../licence-details/licence-length/update-transaction.js'
+
+jest.mock('../../../../processors/concession-helper.js', () => ({
+  ageConcessionHelper: jest.fn()
+}))
+
+jest.mock('../../../licence-details/licence-length/update-transaction.js', () => ({
+  onLengthChange: jest.fn()
+}))
+
+const VALID_PAYLOAD = {
+  'date-of-birth-year': '1985',
+  'date-of-birth-month': '1',
+  'date-of-birth-day': '1'
+}
+
+const transactionHelperMock = ({
+  get: jest.fn(),
+  getCurrentPermission: jest.fn(() => ({ licensee: {} })),
+  setCurrentPermission: jest.fn()
+})
+
+describe('updateTransaction', () => {
+  beforeEach(jest.clearAllMocks)
+
+  describe('happy path', () => {
+    let request
+    beforeEach(async () => {
+      request = createRequestMock(VALID_PAYLOAD)
+      await updateTransaction(request)
+    })
+
+    it('gets the payload from the page cache', async () => {
+      expect(request.cache.mock.results[0].value.helpers.page.getCurrentPermission).toBeCalledWith(DATE_OF_BIRTH.page)
+    })
+
+    it('gets the current permission', async () => {
+      expect(transactionHelperMock.getCurrentPermission).toBeCalled()
+    })
+
+    it('calls the age concession helper', async () => {
+      expect(ageConcessionHelper).toBeCalled()
+    })
+
+    it('calls on length change', async () => {
+      expect(onLengthChange).toBeCalled()
+    })
+
+    it('sets the birth date on the current permission', async () => {
+      const [[permission]] = transactionHelperMock.setCurrentPermission.mock.calls
+      expect(permission.licensee.birthDate).toBe('1985-01-01')
+    })
+  })
+
+  describe('unhappy path', () => {
+    let consoleErrorSpy, request
+    beforeEach(async () => {
+      consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation(jest.fn())
+      request = createRequestMock()
+      await updateTransaction(request)
+    })
+
+    it('logs an error if page cache does not match payload', async () => {
+      expect(consoleErrorSpy).toBeCalledWith(
+        'DOB page cache payload does not match current permission payload',
+        { dobPageCache: { payload: VALID_PAYLOAD }, payload: { test: 'payload' } }
+      )
+    })
+  })
+})
+
+const createRequestMock = (payload) => ({
+  cache: jest.fn(() => ({
+    helpers: {
+      page: {
+        get: jest.fn(() => ({
+          permissions: [{
+            [DATE_OF_BIRTH.page]: {
+              payload: VALID_PAYLOAD
+            }
+          }]
+        })),
+        getCurrentPermission: jest.fn(() => ({ payload: payload || { test: 'payload' } }))
+      },
+      status: {
+        get: jest.fn()
+      },
+      transaction: transactionHelperMock
+    }
+  }))
+})
--- packages/gafl-webapp-service/src/pages/concessions/date-of-birth/update-transaction.js
+++ packages/gafl-webapp-service/src/pages/concessions/date-of-birth/update-transaction.js
@@ -3,6 +3,10 @@ import { DATE_OF_BIRTH } from '../../../uri.js'
 import { ageConcessionHelper } from '../../../processors/concession-helper.js'
 import { cacheDateFormat } from '../../../processors/date-and-time-display.js'
 import { onLengthChange } from '../../licence-details/licence-length/update-transaction.js'
+
+const pageCacheMatchesPayload = (pageCache, payload) =>
+  JSON.stringify(pageCache[DATE_OF_BIRTH.page].payload) === JSON.stringify(payload)
+
 /**
  * Transfer the validated page object
  * @param request
@@ -16,6 +20,16 @@ export default async request => {
     month: Number.parseInt(payload['date-of-birth-month']) - 1,
     day: payload['date-of-birth-day']
   }).format(cacheDateFormat)
+
+  const pageCache = await request.cache().helpers.page.get()
+  const dobPageCache = pageCache.permissions.find(permission => !!permission[DATE_OF_BIRTH.page])
+
+  if (!pageCacheMatchesPayload(dobPageCache, payload)) {
+    console.error('DOB page cache payload does not match current permission payload', { dobPageCache: dobPageCache[DATE_OF_BIRTH.page], payload })
+    console.log('page cache', JSON.stringify(await request.cache().helpers.page.get()))
+    console.log('transaction cache', JSON.stringify(await request.cache().helpers.transaction.get()))
+    console.log('status cache', JSON.stringify(await request.cache().helpers.status.get()))
+  }
 
   // Work out the junior or senior concession at the point at which the licence starts
   const permission = await request.cache().helpers.transaction.getCurrentPermission()
