8de514c2607958a5a7511c053c0ef55bcdaa7479
Phil Benson <phil.benson@defra.gov.uk>
2021-10-13 12:23:04 +0100

    rewrote mocks to work with the altered behaviour of isolateModules in jest 27

--- packages/fulfilment-job/src/fulfilment-job.js
+++ packages/fulfilment-job/src/fulfilment-job.js
@@ -6,9 +6,9 @@ fulfilmentJob
   .command('execute')
   .description('Run fulfilment processor')
   .action(processFulfilment)
-
 // Configure help for unrecognised commands
 fulfilmentJob.command('*').action(() => fulfilmentJob.help())
 fulfilmentJob.parse()
 fulfilmentJob.args.length || fulfilmentJob.outputHelp()
+
 export default fulfilmentJob
