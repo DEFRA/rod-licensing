881925e8d50db421adf50924043ed1e43eda9386
nabeelamir-defra <nabeel.amir@defra.gov.uk>
2021-10-22 15:02:58 +0100

    made getFromSummary more explicit

--- packages/gafl-webapp-service/src/pages/summary/licence-summary/route.js
+++ packages/gafl-webapp-service/src/pages/summary/licence-summary/route.js
@@ -83,6 +83,14 @@ export const getData = async request => {
   }
 }
 
-export const getFromSummary = status => status.renewal ? LICENCE_SUMMARY_SEEN : status.fromSummary || LICENCE_SUMMARY_SEEN
+const getFromSummary = status => {
+  if (status.renewal) {
+    return LICENCE_SUMMARY_SEEN
+  }
+  if (status.fromSummary) {
+    return status.fromSummary
+  }
+  return LICENCE_SUMMARY_SEEN
+}
 
 export default pageRoute(LICENCE_SUMMARY.page, LICENCE_SUMMARY.uri, null, nextPage, getData)
