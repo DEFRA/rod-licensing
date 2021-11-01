661eda88709d30163b6d66247f26206d8ccdb185
nabeelamir-defra <nabeel.amir@defra.gov.uk>
2021-11-01 09:37:50 +0000

    pr comment - remove integration test and added unit test

--- packages/gafl-webapp-service/src/pages/licence-details/licence-for/route.js
+++ packages/gafl-webapp-service/src/pages/licence-details/licence-for/route.js
@@ -4,7 +4,7 @@ import { LICENCE_FOR } from '../../../uri.js'
 import pageRoute from '../../../routes/page-route.js'
 import { nextPage } from '../../../routes/next-page.js'
 
-const validator = Joi.object({
+export const validator = Joi.object({
   'licence-for': Joi.string()
     .valid('you', 'someone-else')
     .required()
