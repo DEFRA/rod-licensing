a2b3f8189ab70d24f94e48b61fe7308c54b98469
nabeelamir-defra <nabeel.amir@defra.gov.uk>
2021-11-04 14:28:50 +0000

    added comma to unit tests

--- packages/gafl-webapp-service/src/pages/contact/name/__tests__/route.spec.js
+++ packages/gafl-webapp-service/src/pages/contact/name/__tests__/route.spec.js
@@ -14,26 +14,26 @@ describe('name > route', () => {
   }
 
   describe('getData', () => {
-    it('should return isLicenceForYou as true if isLicenceForYou is true on the status cache', async () => {
+    it('should return isLicenceForYou as true, if isLicenceForYou is true on the status cache', async () => {
       mockStatusCacheGet.mockImplementationOnce(() => ({ isLicenceForYou: true }))
       const result = await getData(mockRequest)
       expect(result.isLicenceForYou).toBeTruthy()
     })
 
-    it('should return pronoun as your if isLicenceForYou is true on the status cache', async () => {
+    it('should return pronoun as your, if isLicenceForYou is true on the status cache', async () => {
       mockStatusCacheGet.mockImplementationOnce(() => ({ isLicenceForYou: true }))
       const result = await getData(mockRequest)
       console.log(result)
       expect(result.pronoun).toBe('your')
     })
 
-    it('should return isLicenceForYou as false if isLicenceForYou is false on the status cache', async () => {
+    it('should return isLicenceForYou as false, if isLicenceForYou is false on the status cache', async () => {
       mockStatusCacheGet.mockImplementationOnce(() => ({ isLicenceForYou: false }))
       const result = await getData(mockRequest)
       expect(result.isLicenceForYou).toBeFalsy()
     })
 
-    it('should return isLicenceForYou as their if isLicenceForYou is false on the status cache', async () => {
+    it('should return isLicenceForYou as their, if isLicenceForYou is false on the status cache', async () => {
       mockStatusCacheGet.mockImplementationOnce(() => ({ isLicenceForYou: false }))
       const result = await getData(mockRequest)
       expect(result.pronoun).toBe('their')
