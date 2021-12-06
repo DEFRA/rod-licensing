3d02d1aa3aee0e036ff284d25d0234ba00408d79
Phil Benson <phil.benson@defra.gov.uk>
2021-12-06 10:29:45 +0000

    Added tests for correct handler being attached to

--- packages/gafl-webapp-service/src/pages/licence-details/licence-for/route.js
+++ packages/gafl-webapp-service/src/pages/licence-details/licence-for/route.js
@@ -2,7 +2,6 @@ import Joi from 'joi'
 import { LICENCE_FOR } from '../../../uri.js'
 import pageRoute from '../../../routes/page-route.js'
 import { nextPage } from '../../../routes/next-page.js'
-import { initialiseSession } from '../../../handlers/new-session-handler.js'
 
 export const validator = Joi.object({
   'licence-for': Joi.string()
@@ -10,14 +9,19 @@ export const validator = Joi.object({
     .required()
 }).options({ abortEarly: false, allowUnknown: true })
 
-const licenceForKey = 'licence-for'
 const route = pageRoute(LICENCE_FOR.page, LICENCE_FOR.uri, validator, nextPage)
-const handler = route[1].handler
-route[1].handler = async (request, h) => {
-  const permission = await request.cache().helpers.page.getCurrentPermission()
-  const licenceForHasChanged = permission[licenceForKey].payload[licenceForKey] !== request.payload[licenceForKey]
+const postRoute = route.find(r => r.method === 'POST')
+const { handler } = postRoute
+postRoute.handler = async (request, h) => {
+  const { currentPermissionIdx } = await request.cache().helpers.status.get()
+  const pageCache = Object.assign({}, await request.cache().helpers.page.get())
+  const permission = await request.cache().helpers.page.getCurrentPermission(LICENCE_FOR.page)
+  const licenceForHasChanged = (permission !== undefined) && permission.payload[LICENCE_FOR.page] !== request.payload[LICENCE_FOR.page]
   if (licenceForHasChanged) {
-    initialiseSession(request)
+    pageCache.permissions[currentPermissionIdx] = {
+      [LICENCE_FOR.page]: permission
+    }
+    await request.cache().helpers.page.set(pageCache)
   }
   return handler(request, h)
 }
