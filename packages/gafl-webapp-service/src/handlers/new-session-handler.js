4358e9c304ca6595bef77652e8ac189cd42992bd
Phil Benson <phil.benson@defra.gov.uk>
2021-12-06 11:26:32 +0000

    Revert changes to new session handler

--- packages/gafl-webapp-service/src/handlers/new-session-handler.js
+++ packages/gafl-webapp-service/src/handlers/new-session-handler.js
@@ -1,14 +1,10 @@
 import { CONTROLLER } from '../uri.js'
 import { initialiseAnalyticsSessionData } from '../processors/analytics.js'
 
-export const initialiseSession = async request => {
+export default async (request, h) => {
   // The user may have an existing session, in which case we need to examine this for attribution and/or clientId
   const existingCacheStatus = await request.cache().helpers.status.get()
   await request.cache().initialize()
   await initialiseAnalyticsSessionData(request, existingCacheStatus)
-}
-
-export default async (request, h) => {
-  await initialiseSession(request)
   return h.redirect(CONTROLLER.uri)
 }
