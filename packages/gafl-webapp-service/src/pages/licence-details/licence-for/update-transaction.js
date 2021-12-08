4f770c610afe519a343cab31cdc3c769057a2f5c
Phil Benson <phil.benson@defra.gov.uk>
2021-12-08 14:58:46 +0000

    Revert changes in update transaction

--- packages/gafl-webapp-service/src/pages/licence-details/licence-for/update-transaction.js
+++ packages/gafl-webapp-service/src/pages/licence-details/licence-for/update-transaction.js
@@ -1,25 +1,7 @@
 import { LICENCE_FOR } from '../../../uri.js'
 
-const setIsLicenceForYou = async request => {
-  const { payload } = await request.cache().helpers.page.getCurrentPermission(LICENCE_FOR.page)
-  const isLicenceForYou = (payload[LICENCE_FOR.page] === 'you')
-  await request.cache().helpers.status.setCurrentPermission({ isLicenceForYou })
-}
-
-const clearPageCacheIfLicenceForHasChanged = async request => {
-  const permission = await request.cache().helpers.page.getCurrentPermission(LICENCE_FOR.page)
-  const { currentPermissionIdx } = await request.cache().helpers.status.get()
-  const pageCache = Object.assign({}, await request.cache().helpers.page.get())
-  const licenceForHasChanged = (permission.payload[LICENCE_FOR.page] !== request.payload[LICENCE_FOR.page])
-  if (licenceForHasChanged) {
-    pageCache.permissions[currentPermissionIdx] = {
-      [LICENCE_FOR.page]: permission
-    }
-    await request.cache().helpers.page.set(pageCache)
-  }
-}
-
 export default async request => {
-  await setIsLicenceForYou(request)
-  await clearPageCacheIfLicenceForHasChanged(request)
+  const { payload } = await request.cache().helpers.page.getCurrentPermission(LICENCE_FOR.page)
+  const isLicenceForYou = payload['licence-for'] === 'you'
+  await request.cache().helpers.status.setCurrentPermission({ isLicenceForYou })
 }
