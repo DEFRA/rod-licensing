793d3318954ca46aff9dca9dc449c6fe66549b26
nabeelamir-defra <nabeel.amir@defra.gov.uk>
2021-12-15 14:41:37 +0000

    pr comment - added additional unit tests

--- packages/gafl-webapp-service/src/pages/contact/contact/__tests__/result-function.spec.js
+++ packages/gafl-webapp-service/src/pages/contact/contact/__tests__/result-function.spec.js
@@ -28,5 +28,29 @@ describe('contact > result-function', () => {
       const result = await resultFunction(mockRequest)
       expect(result).toBe(CommonResults.SUMMARY)
     })
+
+    it('should return ok if fromSummary is licence-summary', async () => {
+      mockStatusCacheGet.mockImplementationOnce(() => ({ fromSummary: 'licence-summary' }))
+      const result = await resultFunction(mockRequest)
+      expect(result).toBe(CommonResults.OK)
+    })
+
+    it('should return summary if fromSummary is contact-summary', async () => {
+      mockStatusCacheGet.mockImplementationOnce(() => ({ fromSummary: 'contact-summary' }))
+      const result = await resultFunction(mockRequest)
+      expect(result).toBe(CommonResults.SUMMARY)
+    })
+
+    it('should return ok if renewal is false', async () => {
+      mockStatusCacheGet.mockImplementationOnce(() => ({ renewal: false }))
+      const result = await resultFunction(mockRequest)
+      expect(result).toBe(CommonResults.OK)
+    })
+
+    it('should return summary if renewal is true', async () => {
+      mockStatusCacheGet.mockImplementationOnce(() => ({ renewal: true }))
+      const result = await resultFunction(mockRequest)
+      expect(result).toBe(CommonResults.SUMMARY)
+    })
   })
 })
