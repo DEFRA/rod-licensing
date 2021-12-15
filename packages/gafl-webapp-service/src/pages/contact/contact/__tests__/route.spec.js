7d5b03d87fff1f3b79134c04fd596fd76cd65ee9
Phil Benson <phil.benson@defra.gov.uk>
2021-12-15 16:27:05 +0000

    broadened tests

--- packages/gafl-webapp-service/src/pages/contact/contact/__tests__/route.spec.js
+++ packages/gafl-webapp-service/src/pages/contact/contact/__tests__/route.spec.js
@@ -1,6 +1,9 @@
 import { getData, validator } from '../route'
 import pageRoute from '../../../../routes/page-route.js'
 import { nextPage } from '../../../../routes/next-page.js'
+import { isPhysical } from '../../../../processors/licence-type-display.js'
+import { hasJunior } from '../../../../processors/concession-helper.js'
+import { HOW_CONTACTED } from '../../../../processors/mapping-constants.js'
 
 jest.mock('../../../../routes/next-page.js', () => ({
   nextPage: jest.fn()
@@ -11,23 +14,41 @@ jest.mock('../../../../uri.js', () => ({
   CONTACT: {
     page: 'mock-contact-page',
     uri: '/mock/contact/page/uri'
+  },
+  DATE_OF_BIRTH: {
+    uri: '/mock/date/of/birth/uri'
   }
+}))
+jest.mock('../../../../processors/licence-type-display.js', () => ({
+  isPhysical: jest.fn(() => true)
+}))
+jest.mock('../../../../processors/concession-helper.js', () => ({
+  hasJunior: jest.fn(() => false)
+}))
+jest.mock('../../../../processors/mapping-constants.js', () => ({
+  smoke_signal: 'smoke_signal',
+  telegraph: 'telegraph',
+  chinese_whispers: 'high knees whispa',
+  semaphore: 'semaphore'
 }))
 
 describe('name > route', () => {
-  const mockStatusCacheGet = jest.fn()
+  const mockStatusCacheGet = jest.fn(() => ({
+    isLicenceForYou: true
+  }))
+  const mockTransactionCacheGet = jest.fn(async () => ({
+    licensee: {
+      birthDate: 'birthDate'
+    },
+    licenceLength: 'licenceLength',
+    licenceStartDate: 'licenceStartDate'
+  }))
 
   const mockRequest = {
     cache: () => ({
       helpers: {
         transaction: {
-          getCurrentPermission: async () => ({
-            licensee: {
-              birthDate: 'birthDate'
-            },
-            licenceLength: 'licenceLength',
-            licenceStartDate: 'licenceStartDate'
-          })
+          getCurrentPermission: mockTransactionCacheGet
         },
         status: {
           getCurrentPermission: mockStatusCacheGet
@@ -38,15 +59,49 @@ describe('name > route', () => {
 
   describe('getData', () => {
     it('should return isLicenceForYou as true, if isLicenceForYou is true on the status cache', async () => {
-      mockStatusCacheGet.mockImplementationOnce(() => ({ isLicenceForYou: true }))
+      mockStatusCacheGet.mockReturnValueOnce({ isLicenceForYou: true })
       const result = await getData(mockRequest)
       expect(result.isLicenceForYou).toBeTruthy()
     })
 
     it('should return isLicenceForYou as false, if isLicenceForYou is false on the status cache', async () => {
-      mockStatusCacheGet.mockImplementationOnce(() => ({ isLicenceForYou: false }))
+      mockStatusCacheGet.mockReturnValueOnce({ isLicenceForYou: false })
       const result = await getData(mockRequest)
       expect(result.isLicenceForYou).toBeFalsy()
+    })
+
+    it('should return licensee from transaction permission', async () => {
+      const licensee = { birthDate: 'birthDate' }
+      mockTransactionCacheGet.mockReturnValueOnce({
+        licensee: licensee,
+        licenceLength: 'licenceLength',
+        licenceStartDate: 'licenceStartDate'
+      })
+      const result = await getData(mockRequest)
+      expect(result.licensee).toBe(licensee)
+    })
+
+    it.each([
+      [true],
+      [false]
+    ])('isPhysical should match output of isPhysical function', async isPhysicalVal => {
+      isPhysical.mockReturnValueOnce(isPhysicalVal)
+      const result = await getData(mockRequest)
+      expect(result.isPhysical).toEqual(isPhysicalVal)
+    })
+
+    it.each([
+      [true],
+      [false]
+    ])('isJunior should match output of hasJunior function', async hasJuniorVal => {
+      hasJunior.mockReturnValueOnce(hasJuniorVal)
+      const result = await getData(mockRequest)
+      expect(result.isJunior).toEqual(hasJuniorVal)
+    })
+
+    it('how contacted returns enum', async () => {
+      const result = await getData(mockRequest)
+      expect(result.howContacted).toEqual(HOW_CONTACTED)
     })
   })
 
