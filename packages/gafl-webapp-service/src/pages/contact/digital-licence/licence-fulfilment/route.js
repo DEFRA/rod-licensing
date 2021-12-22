2cd1076dba608bbf4c332428175078ac46577b17
nabeelamir-defra <nabeel.amir@defra.gov.uk>
2021-12-22 11:49:22 +0000

    made title dynamic on licence fulfilment screen, depending on isLicenceForYou flag

--- packages/gafl-webapp-service/src/pages/contact/digital-licence/licence-fulfilment/route.js
+++ packages/gafl-webapp-service/src/pages/contact/digital-licence/licence-fulfilment/route.js
@@ -6,13 +6,16 @@ import { nextPage } from '../../../../routes/next-page.js'
 import { isPhysical } from '../../../../processors/licence-type-display.js'
 import GetDataRedirect from '../../../../handlers/get-data-redirect.js'
 
-const isPhysicalPermission = async request => {
+export const getData = async request => {
   const permission = await request.cache().helpers.transaction.getCurrentPermission()
+  const { isLicenceForYou } = await request.cache().helpers.status.getCurrentPermission()
 
   // page is only permitted for physical licences
   if (!isPhysical(permission)) {
     throw new GetDataRedirect(CONTACT.uri)
   }
+
+  return { isLicenceForYou }
 }
 
 const validator = Joi.object({
@@ -21,4 +24,4 @@ const validator = Joi.object({
     .required()
 }).options({ abortEarly: false, allowUnknown: true })
 
-export default pageRoute(LICENCE_FULFILMENT.page, LICENCE_FULFILMENT.uri, validator, nextPage, isPhysicalPermission)
+export default pageRoute(LICENCE_FULFILMENT.page, LICENCE_FULFILMENT.uri, validator, nextPage, getData)
