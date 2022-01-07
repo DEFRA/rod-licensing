1d53780cc226a26df57be8642f5c8a9514a12b25
nabeelamir-defra <nabeel.amir@defra.gov.uk>
2022-01-07 11:01:36 +0000

    fixed unit tests

--- packages/gafl-webapp-service/src/pages/licence-details/licence-for/__tests__/update-transaction.spec.js
+++ packages/gafl-webapp-service/src/pages/licence-details/licence-for/__tests__/update-transaction.spec.js
@@ -5,6 +5,8 @@ describe('licence-for > update-transaction', () => {
 
   const mockPageCacheGet = jest.fn()
   const mockStatusCacheSet = jest.fn()
+  const mockTransactionCacheSet = jest.fn()
+  const mockTransactionCacheGet = jest.fn()
 
   const mockRequest = {
     cache: () => ({
@@ -14,6 +16,10 @@ describe('licence-for > update-transaction', () => {
         },
         status: {
           setCurrentPermission: mockStatusCacheSet
+        },
+        transaction: {
+          setCurrentPermission: mockTransactionCacheSet,
+          getCurrentPermission: mockTransactionCacheGet
         }
       }
     })
@@ -22,14 +28,20 @@ describe('licence-for > update-transaction', () => {
   describe('default', () => {
     it('should set isLicenceForYou to true if user has selected you', async () => {
       mockPageCacheGet.mockImplementationOnce(() => ({ payload: { 'licence-for': 'you' } }))
+      mockTransactionCacheGet.mockImplementationOnce(() => ({}))
+
       await updateTransaction(mockRequest)
       expect(mockStatusCacheSet).toHaveBeenCalledWith({ isLicenceForYou: true })
+      expect(mockTransactionCacheSet).toHaveBeenCalledWith({ isLicenceForYou: true })
     })
 
     it('should set isLicenceForYou to false if user has selected someone-else', async () => {
       mockPageCacheGet.mockImplementationOnce(() => ({ payload: { 'licence-for': 'someone-else' } }))
+      mockTransactionCacheGet.mockImplementationOnce(() => ({}))
+
       await updateTransaction(mockRequest)
       expect(mockStatusCacheSet).toHaveBeenCalledWith({ isLicenceForYou: false })
+      expect(mockTransactionCacheSet).toHaveBeenCalledWith({ isLicenceForYou: false })
     })
   })
 })
