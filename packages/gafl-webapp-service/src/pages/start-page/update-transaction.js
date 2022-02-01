7f29d0a5eb41c54249bf64158d58066e497815d9
michael steinacher <steinacherm@hotmail.co.uk>
2022-02-01 16:05:54 +0000

    Removed previously created 'start-page' folder as this has been updated to buy-or-renew. Alos updated tests in session manager now buy-or-renew is the start page

--- packages/gafl-webapp-service/src/pages/start-page/update-transaction.js
+++
@@ -1,7 +0,0 @@
-import { START_PAGE } from '../../../uri.js'
-
-export default async request => {
-  const { payload } = await request.cache().helpers.page.getCurrentPermission(START_PAGE.page)
-  const buyNewLicence = payload['start-page'] === 'buy-licence'
-  await request.cache().helpers.status.setCurrentPermission({ buyNewLicence })
-}
--- packages/gafl-webapp-service/src/session-cache/__tests__/session-manager.spec.js
+++ packages/gafl-webapp-service/src/session-cache/__tests__/session-manager.spec.js
@@ -3,7 +3,7 @@ import { isStaticResource, useSessionCookie, includesRegex } from '../session-ma
 import { licenseTypes } from '../../pages/licence-details/licence-type/route.js'
 import {
   CONTROLLER,
-  LICENCE_FOR,
+  BUY_OR_RENEW,
   LICENCE_TYPE,
   ORDER_COMPLETE,
   LICENCE_DETAILS,
@@ -69,7 +69,7 @@ describe('The user', () => {
     await injectWithoutSessionCookie('POST', LICENCE_TYPE.uri, { 'licence-type': licenseTypes.troutAndCoarse2Rod })
     const response = await injectWithCookies('GET', CONTROLLER.uri)
     expect(response.statusCode).toBe(302)
-    expect(response.headers.location).toBe(LICENCE_FOR.uri)
+    expect(response.headers.location).toBe(BUY_OR_RENEW.uri)
   })
 
   /*
@@ -81,7 +81,7 @@ describe('The user', () => {
     await injectWithoutSessionCookie('POST', LICENCE_TYPE.uri, {})
     const response = await injectWithCookies('GET', CONTROLLER.uri)
     expect(response.statusCode).toBe(302)
-    expect(response.headers.location).toBe(LICENCE_FOR.uri)
+    expect(response.headers.location).toBe(BUY_OR_RENEW.uri)
   })
 
   /*
