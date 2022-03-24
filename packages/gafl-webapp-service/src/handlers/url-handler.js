72fb082907befe3c355bc817ccf19b4418bc7888
ScottDormand96 <sdormand@hotmail.com>
2022-03-24 15:00:35 +0000

    update url handlder

--- packages/gafl-webapp-service/src/handlers/url-handler.js
+++ packages/gafl-webapp-service/src/handlers/url-handler.js
@@ -1,4 +1,4 @@
-import { QUERYSTRING_LICENCE_KEY } from '../constants.js'
+import { QUERYSTRING_LICENCE_KEY, UTM } from '../constants.js'
 import { initialiseAnalyticsSessionData } from '../processors/analytics.js'
 import { IDENTIFY, ATTRIBUTION } from '../uri.js'
 
@@ -14,7 +14,7 @@ export default async (request, h) => {
     const refNumber = request.query[QUERYSTRING_LICENCE_KEY]
     const sixDigit = /^[A-Za-z0-9]{6}$/.test(refNumber)
     if (sixDigit) {
-      return h.redirect(`${ATTRIBUTION.uri}/${refNumber}`)
+      return h.redirect(`${ATTRIBUTION.uri}/${UTM.CAMPAIGN}=renewals&${UTM.SOURCE}=aen_invitation&reference=${refNumber}`)
     }
   }
   return h.redirect(IDENTIFY.uri)
