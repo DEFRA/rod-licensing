2347f9139f7c696242df06b8c295bbd848b5883a
ScottDormand96 <sdormand@hotmail.com>
2022-03-28 10:02:29 +0100

    update url-handler

--- packages/gafl-webapp-service/src/handlers/url-handler.js
+++ packages/gafl-webapp-service/src/handlers/url-handler.js
@@ -10,11 +10,11 @@ import { IDENTIFY, ATTRIBUTION } from '../uri.js'
  */
 export default async (request, h) => {
   await initialiseAnalyticsSessionData(request)
-  if (request.query[QUERYSTRING_LICENCE_KEY]) {
-    const refNumber = request.query[QUERYSTRING_LICENCE_KEY]
+  if (request.params[QUERYSTRING_LICENCE_KEY]) {
+    const refNumber = request.params[QUERYSTRING_LICENCE_KEY]
     const sixDigit = /^[A-Za-z0-9]{6}$/.test(refNumber)
     if (sixDigit) {
-      return h.redirect(`${ATTRIBUTION.uri}/${UTM.CAMPAIGN}=renewals&${UTM.SOURCE}=aen_invitation&reference=${refNumber}`)
+      return h.redirect(`${ATTRIBUTION.uri}?${UTM.CAMPAIGN}=renewals&${UTM.SOURCE}=aen_invitation&reference=${refNumber}`)
     }
   }
   return h.redirect(IDENTIFY.uri)
