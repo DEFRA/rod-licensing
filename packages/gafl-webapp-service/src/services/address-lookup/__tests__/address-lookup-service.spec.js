c8259ca1dc438dcfd793f049ea80fb317d24bb91
michael steinacher <steinacherm@hotmail.co.uk>
2022-03-29 13:45:10 +0100

    Amended test for address lookup to cover a variety of addresses

--- packages/gafl-webapp-service/src/services/address-lookup/__tests__/address-lookup-service.spec.js
+++ packages/gafl-webapp-service/src/services/address-lookup/__tests__/address-lookup-service.spec.js
@@ -17,32 +17,24 @@ describe('address-lookup-service', () => {
       expect(results).toEqual([])
     })
 
-    it('if data is returned from the API, it maps the data correctly', async () => {
+    it.each([
+      ['1 HOWECROFT COURT, EASTMEAD LANE, BRISTOL, BS9 1HJ', 'BS9 1HJ', '1 howecroft court, eastmead lane, bristol, BS9 1HJ', 'BS9 1HJ'],
+      ['9 ORBIT STREET, ADAMSDOWN, CARDIfF, CF24 0JX', 'CF24 0JX', '9 orbit street, adamsdown, cardiff, CF24 0JX', 'CF24 0JX'],
+      ['45 TINTERN CLOSE, EASTBOURNE, BN22 0UF', 'BN22 0UF', '45 tintern close, eastbourne, BN22 0UF', 'BN22 0UF']
+    ])('if data is returned from the API, it maps the data correctly in lower case, other than postcode', async (address, postcode, expectedAddress, expectedPostcode) => {
       fetch.mockResolvedValue({
         json: () => ({
           results: [{
-            address: '1 HOWECROFT COURT, EASTMEAD LANE, BRISTOL, BS9 1HJ',
-            premises: '1',
-            street_address: 'HOWECROFT COURT',
-            locality: 'EASTMEAD LANE',
-            city: 'BRISTOL',
-            postcode: 'BS9 1HJ'
+            address: address,
+            postcode: postcode
           }]
         })
       })
       const results = await als()
-      expect(results).toStrictEqual([
-        {
-          id: 0,
-          address: '1 howecroft court, eastmead lane, bristol, BS9 1HJ',
-          premises: '1',
-          street: 'HOWECROFT COURT',
-          locality: 'EASTMEAD LANE',
-          town: 'BRISTOL',
-          postcode: 'BS9 1HJ',
-          country: undefined
-        }
-      ])
+      expect(results[0]).toEqual(expect.objectContaining({
+        address: expectedAddress,
+        postcode: expectedPostcode
+      }))
     })
   })
 })
