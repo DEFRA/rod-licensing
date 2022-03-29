e9cdc9458012cedabc0e3d47466d48ec356f4ff5
nabeelamir-defra <nabeel.amir@defra.gov.uk>
2022-03-29 12:56:03 +0100

    pr comment - update dependency import

--- packages/fulfilment-job/src/__mocks__/aws-sdk.js
+++ packages/fulfilment-job/src/__mocks__/aws-sdk.js
@@ -1,3 +1,3 @@
-import { configureAwsSdkMock } from '../../../connectors-lib/src/__mocks__/aws-mock-helper.js'
+import { configureAwsSdkMock } from '@defra-fish/connectors-lib/src/__mocks__/aws-mock-helper.js'
 const AwsSdk = configureAwsSdkMock()
 export default AwsSdk
