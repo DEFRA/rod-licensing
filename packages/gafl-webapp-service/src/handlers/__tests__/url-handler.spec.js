b4183a8011752d331ad65f52ea377b7e846bf4d6
ScottDormand96 <sdormand@hotmail.com>
2022-03-30 09:16:01 +0100

    update handlder and tests

--- packages/gafl-webapp-service/src/handlers/__tests__/url-handler.spec.js
+++ packages/gafl-webapp-service/src/handlers/__tests__/url-handler.spec.js
@@ -1,6 +1,6 @@
 import { UTM, QUERYSTRING_LICENCE_KEY } from '../../constants'
 import urlHandler from '../url-handler'
-import { ATTRIBUTION, IDENTIFY } from '../../uri'
+import { IDENTIFY } from '../../uri'
 
 jest.mock('../../constants', () => ({
   UTM: {
@@ -23,46 +23,40 @@ describe('The url handler', () => {
   })
 
   it('returns identify if no licence key', async () => {
-    const query = {
+    const params = {
       [UTM.CAMPAIGN]: 'renewals',
       [QUERYSTRING_LICENCE_KEY]: null
     }
     const responseToolkit = generateResponseToolkitMock()
-    await urlHandler(generateRequestMock(query), responseToolkit)
+    await urlHandler(generateRequestMock(params), responseToolkit)
     expect(responseToolkit.redirect).toHaveBeenCalledWith(IDENTIFY.uri)
   })
 
-  it.each([
-    ['B2F11U'],
-    ['AH56F6'],
-    ['GH330P']
-  ])('6 digit reference number exists and returns ATTRIBUTION', async licenceKey => {
-    const query = {
+  it.each([['B2F11U'], ['AH56F6'], ['GH330P']])('6 digit reference number exists and returns ATTRIBUTION', async licenceKey => {
+    const params = {
       [UTM.CAMPAIGN]: 'renewals',
       [QUERYSTRING_LICENCE_KEY]: licenceKey
     }
     const responseToolkit = generateResponseToolkitMock()
-    await urlHandler(generateRequestMock(query), responseToolkit)
-    const regExMatch = new RegExp(`^${ATTRIBUTION.uri}/${licenceKey}$`)
+    await urlHandler(generateRequestMock(params), responseToolkit)
+    const regExMatch = new RegExp(`^/attribution-url\\?utmcampaign\\=renewals&utmsource\\=aen_invitation&reference\\=${licenceKey}$`)
     expect(responseToolkit.redirect).toHaveBeenCalledWith(expect.stringMatching(regExMatch))
   })
 
-  it.each([
-    ['B2F11UH5D'],
-    ['AH56'],
-    ['GH330PPTD']
-  ])('reference number is not 6 digits and returns back to IDENTIFY', async licenceKey => {
-    const query = {
+  it.each([['B2F11UH5D'], ['AH56'], ['GH330PPTD']])('reference number is not 6 digits and returns back to IDENTIFY', async licenceKey => {
+    const params = {
       [UTM.CAMPAIGN]: 'renewals',
+      [UTM.SOURCE]: 'aen_invitation',
       [QUERYSTRING_LICENCE_KEY]: licenceKey
     }
     const responseToolkit = generateResponseToolkitMock()
-    await urlHandler(generateRequestMock(query), responseToolkit)
+    await urlHandler(generateRequestMock(params), responseToolkit)
     expect(responseToolkit.redirect).toHaveBeenCalledWith(IDENTIFY.uri)
   })
 
-  const generateRequestMock = (query, status = {}) => ({
-    query,
+  const generateRequestMock = (params, query, status = {}) => ({
+    params,
+    query: { _ga: null },
     cache: jest.fn(() => ({
       helpers: {
         status: {
