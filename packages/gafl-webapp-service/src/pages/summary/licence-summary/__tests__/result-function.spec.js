e4b2b538d49d7a654a2b4af2564af88f1041decd
ScottDormand96 <sdormand@hotmail.com>
2022-04-01 13:49:11 +0100

    updated tests

--- packages/gafl-webapp-service/src/pages/summary/licence-summary/__tests__/result-function.spec.js
+++ packages/gafl-webapp-service/src/pages/summary/licence-summary/__tests__/result-function.spec.js
@@ -60,10 +60,16 @@ describe('licence-summary > result-function', () => {
       expect(result).toBe(CommonResults.OK)
     })
 
-    it('should return summary if from summary', async () => {
-      mockStatusCacheGet.mockImplementationOnce(() => ({ fromSummary: true }))
-      mockTransactionPageGet.mockImplementationOnce(() => ({}))
-      mockTransactionCacheGet.mockImplementationOnce(() => ({ permissions: { length: 0 } }))
+    it('should return summary if fromSummary is true', async () => {
+      mockStatusCacheGet.mockImplementationOnce(() => ({ fromSummary: 'contact-summary', CONTACT_SUMMARY_SEEN: 'contact-summary' }))
+      mockTransactionCacheGet.mockImplementationOnce(() => ({ permissions: { length: 0, isLicenceForYou: true } }))
+      const result = await resultFunction(mockRequest)
+      expect(result).toBe(CommonResults.SUMMARY)
+    })
+
+    it('should return ok if fromSummary is false', async () => {
+      mockStatusCacheGet.mockImplementationOnce(() => ({ fromSummary: 'not-contact-summary', CONTACT_SUMMARY_SEEN: 'contact-summary' }))
+      mockTransactionCacheGet.mockImplementationOnce(() => ({ permissions: { length: 0, isLicenceForYou: true } }))
       const result = await resultFunction(mockRequest)
       expect(result).toBe(CommonResults.OK)
     })
