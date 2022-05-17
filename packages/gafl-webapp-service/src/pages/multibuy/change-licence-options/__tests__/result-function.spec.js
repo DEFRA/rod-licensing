3b6475ae51b50effaa39924b3c531daf9fa5a7a0
michael steinacher <steinacherm@hotmail.co.uk>
2022-05-17 14:15:13 +0100

    Added additional tests to result function spec to ensure 100% coverage

--- packages/gafl-webapp-service/src/pages/multibuy/change-licence-options/__tests__/result-function.spec.js
+++ packages/gafl-webapp-service/src/pages/multibuy/change-licence-options/__tests__/result-function.spec.js
@@ -1,5 +1,6 @@
 import resultFunction from '../result-function'
-import { CommonResults } from '../../../../constants.js'
+import { CommonResults, MultibuyForYou, ShowDigitalLicencePages } from '../../../../constants.js'
+import { isMultibuyForYou } from '../../../../handlers/multibuy-for-you-handler.js'
 
 jest.mock('../../../../handlers/multibuy-for-you-handler.js', () => ({
   isMultibuyForYou: jest.fn()
@@ -25,6 +26,43 @@ describe('change-licence-options > result-function', () => {
   describe('default', () => {
     beforeEach(jest.clearAllMocks)
 
+    it('should return the digital licence screen, if licence is in renewal, is 12 months and showDigitalLicencePages is true', async () => {
+      mockStatusCacheGet.mockImplementationOnce(() => ({ renewal: true, showDigitalLicencePages: true }))
+      mockTransactionPageGet.mockImplementationOnce(() => ({
+        licenceLength: '12M',
+        licensee: {
+          postalFulfilment: true
+        }
+      }))
+      const result = await resultFunction(mockRequest)
+      expect(result).toBe(ShowDigitalLicencePages.YES)
+    })
+
+    it('should return the summary screen, if licence is in renewal, is 12 months and showDigitalLicencePages is false', async () => {
+      mockStatusCacheGet.mockImplementationOnce(() => ({ renewal: true, showDigitalLicencePages: false }))
+      mockTransactionPageGet.mockImplementationOnce(() => ({
+        licenceLength: '12M',
+        licensee: {
+          postalFulfilment: false
+        }
+      }))
+      const result = await resultFunction(mockRequest)
+      expect(result).toBe(CommonResults.SUMMARY)
+    })
+
+    it('should return ok, if licence is 12 months and showDigitalLicencePages is true', async () => {
+      mockStatusCacheGet.mockImplementationOnce(() => ({ showDigitalLicencePages: true }))
+      mockTransactionPageGet.mockImplementationOnce(() => ({
+        licenceLength: '12M',
+        licensee: {
+          postalFulfilment: true
+        }
+      }))
+      isMultibuyForYou.mockImplementationOnce(() => false)
+      const result = await resultFunction(mockRequest)
+      expect(result).toBe(CommonResults.OK)
+    })
+
     it('should return amend if fromChangeOptions is seen', async () => {
       mockStatusCacheGet.mockImplementationOnce(() => ({ fromChangeLicenceOptions: 'seen' }))
       const result = await resultFunction(mockRequest)
@@ -35,6 +73,20 @@ describe('change-licence-options > result-function', () => {
       mockStatusCacheGet.mockImplementationOnce(() => ({ fromChangeLicenceOptions: 'not-options' }))
       const result = await resultFunction(mockRequest)
       expect(result).toBe(CommonResults.OK)
+    })
+
+    it('should return isMultibuyForYou when is true', async () => {
+      mockStatusCacheGet.mockImplementationOnce(() => ({ renewal: false }))
+      isMultibuyForYou.mockImplementationOnce(() => true)
+      const result = await resultFunction(mockRequest)
+      expect(result).toBe(MultibuyForYou.YES)
+    })
+
+    it('should not return isMultibuyForYou when is false', async () => {
+      mockStatusCacheGet.mockImplementationOnce(() => ({ renewal: false }))
+      isMultibuyForYou.mockImplementationOnce(() => false)
+      const result = await resultFunction(mockRequest)
+      expect(result).not.toBe(MultibuyForYou.YES)
     })
   })
 })
