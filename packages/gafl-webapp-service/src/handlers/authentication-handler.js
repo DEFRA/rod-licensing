72ea7e6bc87c5f263c21f004477369d004c0d5d3
nabeelamir-defra <73947018+nabeelamir-defra@users.noreply.github.com>
2022-05-19 15:45:11 +0100

    Short licence error page update - easy renewals (#1296)
    * Short licence error page update - easy renewals
    
    https://eaflood.atlassian.net/browse/IWTF-2906
    
    Short licence error page update - easy renewals
    
    * modified error message when attempting to renew a 1 or 8 day licence
    
    * pr comment - moved nunjucks code to route.js

--- packages/gafl-webapp-service/src/handlers/authentication-handler.js
+++ packages/gafl-webapp-service/src/handlers/authentication-handler.js
@@ -1,5 +1,6 @@
 import { setUpCacheFromAuthenticationResult, setUpPayloads } from '../processors/renewals-write-cache.js'
 import { IDENTIFY, CONTROLLER, RENEWAL_INACTIVE } from '../uri.js'
+import { RENEWAL_ERROR_REASON } from '../constants.js'
 import { validation, RENEW_BEFORE_DAYS, RENEW_AFTER_DAYS, SERVICE_LOCAL_TIME } from '@defra-fish/business-rules-lib'
 import Joi from 'joi'
 import { salesApi } from '@defra-fish/connectors-lib'
@@ -47,21 +48,16 @@ export default async (request, h) => {
     return h.redirect(IDENTIFY.uri)
   } else {
     // Test for 12 month licence
-    const daysDiff = moment(authenticationResult.permission.endDate).diff(
-      moment()
-        .tz(SERVICE_LOCAL_TIME)
-        .startOf('day'),
-      'days'
-    )
+    const daysDiff = moment(authenticationResult.permission.endDate).diff(moment().tz(SERVICE_LOCAL_TIME).startOf('day'), 'days')
     if (
       authenticationResult.permission.permit.durationDesignator.description === 'M' &&
       authenticationResult.permission.permit.durationMagnitude === 12
     ) {
       // Test for active renewal
       if (daysDiff > RENEW_BEFORE_DAYS) {
-        return linkInactive('not-due')
+        return linkInactive(RENEWAL_ERROR_REASON.NOT_DUE)
       } else if (daysDiff < -RENEW_AFTER_DAYS) {
-        return linkInactive('expired')
+        return linkInactive(RENEWAL_ERROR_REASON.EXPIRED)
       } else {
         await setUpCacheFromAuthenticationResult(request, authenticationResult)
         await setUpPayloads(request)
@@ -69,7 +65,7 @@ export default async (request, h) => {
         return h.redirect(CONTROLLER.uri)
       }
     } else {
-      return linkInactive('not-annual')
+      return linkInactive(RENEWAL_ERROR_REASON.NOT_ANNUAL)
     }
   }
 }
---
+++ packages/gafl-webapp-service/src/pages/renewals/renewal-inactive/__tests__/route.spec.js
@@ -0,0 +1,113 @@
+import { getData, getTitleAndBodyMessage } from '../route'
+import { NEW_TRANSACTION } from '../../../../uri.js'
+import { RENEWAL_ERROR_REASON } from '../../../../constants'
+
+import englishTranslations from '../../../../locales/en.json'
+import welshTranslations from '../../../../locales/cy.json'
+
+describe('renewal-inactive > route', () => {
+  const mockStatusCacheGet = jest.fn()
+
+  const mockRequest = {
+    cache: () => ({
+      helpers: {
+        status: {
+          getCurrentPermission: mockStatusCacheGet
+        }
+      }
+    }),
+    i18n: {
+      getCatalog: jest.fn(() => englishTranslations)
+    }
+  }
+  describe('getData', () => {
+    it('should return the reason the renewal is inactive', async () => {
+      mockStatusCacheGet.mockImplementationOnce(() => ({ authentication: { reason: RENEWAL_ERROR_REASON.NOT_DUE } }))
+      const result = await getData(mockRequest)
+      expect(result.reason).toBe(RENEWAL_ERROR_REASON.NOT_DUE)
+    })
+
+    it('should return the new transation uri', async () => {
+      mockStatusCacheGet.mockImplementationOnce(() => ({ authentication: {} }))
+      const result = await getData(mockRequest)
+      expect(result.uri.new).toBe(NEW_TRANSACTION.uri)
+    })
+
+    it('should return the body message', async () => {
+      mockStatusCacheGet.mockImplementationOnce(() => ({
+        authentication: { reason: RENEWAL_ERROR_REASON.NOT_DUE },
+        referenceNumber: 'ABC123'
+      }))
+      const result = await getData(mockRequest)
+      expect(result.bodyMessage).toBe('The licence ending in ABC123 does not expire until 19 May 2022')
+    })
+  })
+
+  describe('getTitleAndBodyMessage', () => {
+    it.each([
+      [
+        'The licence ending in ABC123 does not expire until 13 December 2020',
+        RENEWAL_ERROR_REASON.NOT_DUE,
+        'ABC123',
+        '13 December 2020',
+        englishTranslations
+      ],
+      [
+        'The licence ending in ABC123 has expired on 13 December 2020 and can no longer be renewed',
+        RENEWAL_ERROR_REASON.EXPIRED,
+        'ABC123',
+        '13 December 2020',
+        englishTranslations
+      ],
+      [
+        'The licence ending in ABC123 is not a 12 month licence and cannot be renewed.',
+        RENEWAL_ERROR_REASON.NOT_ANNUAL,
+        'ABC123',
+        '13 December 2020',
+        englishTranslations
+      ],
+      [
+        'The licence ending in ABC123 does not expire until 13 December 2020',
+        RENEWAL_ERROR_REASON.NOT_DUE,
+        'ABC123',
+        '13 December 2020',
+        welshTranslations
+      ],
+      [
+        'The licence ending in ABC123 has expired on 13 December 2020 and can no longer be renewed',
+        RENEWAL_ERROR_REASON.EXPIRED,
+        'ABC123',
+        '13 December 2020',
+        welshTranslations
+      ],
+      [
+        'The licence ending in ABC123 is not a 12 month licence and cannot be renewed.',
+        RENEWAL_ERROR_REASON.NOT_ANNUAL,
+        'ABC123',
+        '13 December 2020',
+        welshTranslations
+      ]
+    ])('should return and object with bodyMessage as %s if the reason is %s', (expected, reason, referenceNumber, validTo, translation) => {
+      const result = getTitleAndBodyMessage(translation, reason, referenceNumber, validTo)
+      expect(result.bodyMessage).toBe(expected)
+    })
+
+    it.each([
+      ['You are renewing this licence too early', RENEWAL_ERROR_REASON.NOT_DUE, englishTranslations],
+      ['The licence renewal has expired', RENEWAL_ERROR_REASON.EXPIRED, englishTranslations],
+      ['You cannot renew an 8 day or 1 day licence', RENEWAL_ERROR_REASON.NOT_ANNUAL, englishTranslations],
+      ['You are renewing this licence too early', RENEWAL_ERROR_REASON.NOT_DUE, welshTranslations],
+      ['The licence renewal has expired', RENEWAL_ERROR_REASON.EXPIRED, welshTranslations],
+      ['You cannot renew an 8 day or 1 day licence', RENEWAL_ERROR_REASON.NOT_ANNUAL, welshTranslations]
+    ])('should return and object with title as %s if the reason is %s', (expected, reason, translation) => {
+      const result = getTitleAndBodyMessage(translation, reason, 'ABC123', '13 December 2020')
+      expect(result.title).toBe(expected)
+    })
+
+    it('should return an object with bodyMessage and title as empty if no reason is provided', () => {
+      const result = getTitleAndBodyMessage(englishTranslations, undefined, 'ABC123', '13 December 2020')
+      expect(result.bodyMessage).toBe('')
+      expect(result.title).toBe('')
+    })
+  })
+})
