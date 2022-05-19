fb443cbd3e300c6d3312e741b0d9d97955f4cc8a
nabeelamir-defra <nabeel.amir@defra.gov.uk>
2022-05-19 11:21:39 +0100

    pr comment - moved nunjucks code to route.js

--- packages/gafl-webapp-service/src/pages/renewals/renewal-inactive/__tests__/route.spec.js
+++ packages/gafl-webapp-service/src/pages/renewals/renewal-inactive/__tests__/route.spec.js
@@ -1,5 +1,9 @@
-import { getData } from '../route'
+import { getData, getBodyMessage } from '../route'
 import { NEW_TRANSACTION } from '../../../../uri.js'
+import { RENEWAL_ERROR_REASON } from '../../../../constants'
+
+import englishTranslations from '../../../../locales/en.json'
+import welshTranslations from '../../../../locales/cy.json'
 
 describe('renewal-inactive > route', () => {
   const mockStatusCacheGet = jest.fn()
@@ -11,31 +15,81 @@ describe('renewal-inactive > route', () => {
           getCurrentPermission: mockStatusCacheGet
         }
       }
-    })
+    }),
+    i18n: {
+      getCatalog: jest.fn(() => englishTranslations)
+    }
   }
   describe('getData', () => {
-    it('should return the reference number', async () => {
-      mockStatusCacheGet.mockImplementationOnce(() => ({ referenceNumber: '00000000-2WC3FDR-CD379B', authentication: {} }))
-      const result = await getData(mockRequest)
-      expect(result.referenceNumber).toBe('00000000-2WC3FDR-CD379B')
-    })
-
     it('should return the reason the renewal is inactive', async () => {
-      mockStatusCacheGet.mockImplementationOnce(() => ({ authentication: { reason: 'not-due' } }))
+      mockStatusCacheGet.mockImplementationOnce(() => ({ authentication: { reason: RENEWAL_ERROR_REASON.NOT_DUE } }))
       const result = await getData(mockRequest)
-      expect(result.reason).toBe('not-due')
-    })
-
-    it('should return the date the renewal is valid to', async () => {
-      mockStatusCacheGet.mockImplementationOnce(() => ({ authentication: { endDate: '2020-12-13T23:59:59Z' } }))
-      const result = await getData(mockRequest)
-      expect(result.validTo).toBe('13 December 2020')
+      expect(result.reason).toBe(RENEWAL_ERROR_REASON.NOT_DUE)
     })
 
     it('should return the new transation uri', async () => {
       mockStatusCacheGet.mockImplementationOnce(() => ({ authentication: {} }))
       const result = await getData(mockRequest)
       expect(result.uri.new).toBe(NEW_TRANSACTION.uri)
+    })
+
+    it('should return the body message', async () => {
+      mockStatusCacheGet.mockImplementationOnce(() => ({
+        authentication: { reason: RENEWAL_ERROR_REASON.NOT_DUE },
+        referenceNumber: 'ABC123'
+      }))
+      const result = await getData(mockRequest)
+      expect(result.bodyMessage).toBe('The licence ending in ABC123 does not expire until 19 May 2022')
+    })
+  })
+
+  describe('getBodyMessage', () => {
+    it.each([
+      [
+        'The licence ending in ABC123 does not expire until 13 December 2020',
+        RENEWAL_ERROR_REASON.NOT_DUE,
+        'ABC123',
+        '13 December 2020',
+        englishTranslations
+      ],
+      [
+        'The licence ending in ABC123 has expired on 13 December 2020 and can no longer be renewed',
+        RENEWAL_ERROR_REASON.EXPIRED,
+        'ABC123',
+        '13 December 2020',
+        englishTranslations
+      ],
+      [
+        'The licence ending in ABC123 is not a 12 month licence and cannot be renewed.',
+        RENEWAL_ERROR_REASON.NOT_ANNUAL,
+        'ABC123',
+        '13 December 2020',
+        englishTranslations
+      ],
+      [
+        'The licence ending in ABC123 does not expire until 13 December 2020',
+        RENEWAL_ERROR_REASON.NOT_DUE,
+        'ABC123',
+        '13 December 2020',
+        welshTranslations
+      ],
+      [
+        'The licence ending in ABC123 has expired on 13 December 2020 and can no longer be renewed',
+        RENEWAL_ERROR_REASON.EXPIRED,
+        'ABC123',
+        '13 December 2020',
+        welshTranslations
+      ],
+      [
+        'The licence ending in ABC123 is not a 12 month licence and cannot be renewed.',
+        RENEWAL_ERROR_REASON.NOT_ANNUAL,
+        'ABC123',
+        '13 December 2020',
+        welshTranslations
+      ]
+    ])('should return the message %s if the reason is %s', (expected, reason, referenceNumber, validTo, translation) => {
+      const result = getBodyMessage(translation, reason, referenceNumber, validTo)
+      expect(result).toBe(expected)
     })
   })
 })
--- packages/gafl-webapp-service/src/pages/renewals/renewal-inactive/route.js
+++ packages/gafl-webapp-service/src/pages/renewals/renewal-inactive/route.js
@@ -3,13 +3,29 @@ import pageRoute from '../../../routes/page-route.js'
 import moment from 'moment'
 import { dateDisplayFormat } from '../../../processors/date-and-time-display.js'
 import { nextPage } from '../../../routes/next-page.js'
+import { RENEWAL_ERROR_REASON } from '../../../constants.js'
+
+export const getBodyMessage = (mssgs, reason, referenceNumber, validTo) => {
+  switch (reason) {
+    case RENEWAL_ERROR_REASON.NOT_DUE:
+      return `${mssgs.renewal_inactive_not_due_1}${referenceNumber}${mssgs.renewal_inactive_not_due_2}${validTo}`
+    case RENEWAL_ERROR_REASON.EXPIRED:
+      return `${mssgs.renewal_inactive_not_due_1}${referenceNumber}${mssgs.renewal_inactive_has_expired_1}${validTo}${mssgs.renewal_inactive_has_expired_2}`
+    case RENEWAL_ERROR_REASON.NOT_ANNUAL:
+      return `${mssgs.renewal_inactive_not_due_1}${referenceNumber}${mssgs.renewal_inactive_not_annual_1}`
+  }
+}
 
 export const getData = async request => {
   const { referenceNumber, authentication } = await request.cache().helpers.status.getCurrentPermission()
+  const mssgs = request.i18n.getCatalog()
+  const validTo = moment(authentication.endDate).format(dateDisplayFormat)
+  const reason = authentication.reason
+  const bodyMessage = getBodyMessage(mssgs, reason, referenceNumber, validTo)
   return {
-    referenceNumber,
     reason: authentication.reason,
-    validTo: moment(authentication.endDate).format(dateDisplayFormat),
+    reasonCodes: RENEWAL_ERROR_REASON,
+    bodyMessage,
     uri: {
       new: NEW_TRANSACTION.uri
     }
