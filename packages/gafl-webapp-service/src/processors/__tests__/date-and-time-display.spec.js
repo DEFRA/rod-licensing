da9a9f1b68a1f4f7d12c508bfeed014404cac3d3
michael steinacher <steinacherm@hotmail.co.uk>
2022-06-16 11:48:28 +0100

    Updated route file to include locale translations for 'midday' and 'first minute of the day'. Also updated tests used to include the new param to parse the locale messages.

--- packages/gafl-webapp-service/src/processors/__tests__/date-and-time-display.spec.js
+++ packages/gafl-webapp-service/src/processors/__tests__/date-and-time-display.spec.js
@@ -2,46 +2,69 @@ import { displayStartTime, displayEndTime, displayExpiryDate } from '../date-and
 
 describe('displayStartTime', () => {
   it('displays the date in the correct format where no start time is set', () => {
-    const startTime = displayStartTime({ licenceStartDate: '2021-01-01' })
+    const mssgs = getSampleRequest().i18n.getCatalog()
+    const startTime = displayStartTime({ licenceStartDate: '2021-01-01' }, mssgs)
     expect(startTime).toEqual('0.00am (first minute of the day) on 1 January 2021')
   })
   it('displays the date in the correct format where the start time is midnight', () => {
-    const startTime = displayStartTime({ licenceStartDate: '2021-01-01', licenceStartTime: 0 })
+    const mssgs = getSampleRequest().i18n.getCatalog()
+    const startTime = displayStartTime({ licenceStartDate: '2021-01-01', licenceStartTime: 0 }, mssgs)
     expect(startTime).toEqual('0.00am (first minute of the day) on 1 January 2021')
   })
   it('displays the date in the correct format where the start time is 12pm', () => {
-    const startTime = displayStartTime({ licenceStartDate: '2021-01-01', licenceStartTime: '12' })
+    const mssgs = getSampleRequest().i18n.getCatalog()
+    const startTime = displayStartTime({ licenceStartDate: '2021-01-01', licenceStartTime: '12' }, mssgs)
     expect(startTime).toEqual('12:00pm (midday) on 1 January 2021')
   })
   it('displays the date in the correct format where the start time is 3pm (UTC)', () => {
-    const startTime = displayStartTime({ licenceStartDate: '2021-01-01', licenceStartTime: '15' })
+    const startTime = displayStartTime({ licenceStartDate: '2021-01-01', licenceStartTime: '15' }, {})
     expect(startTime).toEqual('3:00pm on 1 January 2021')
   })
   it('displays the date in the correct format where the start time is 3pm (BST)', () => {
-    const startTime = displayStartTime({ licenceStartDate: '2021-07-01', licenceStartTime: '15' })
+    const startTime = displayStartTime({ licenceStartDate: '2021-07-01', licenceStartTime: '15' }, {})
     expect(startTime).toEqual('3:00pm on 1 July 2021')
   })
 
   it('displays the date in the correct format where the API start time is midnight', () => {
     // Tests that the API start time is used if the date/time has just rolled over (purchase completed at 11.59.59pm)
-    const startTime = displayStartTime({ startDate: '2021-01-01T00:00:00.000Z', licenceStartDate: '2020-12-31' })
+    const mssgs = getSampleRequest().i18n.getCatalog()
+    const startTime = displayStartTime({ startDate: '2021-01-01T00:00:00.000Z', licenceStartDate: '2020-12-31' }, mssgs)
     expect(startTime).toEqual('0.00am (first minute of the day) on 1 January 2021')
   })
   it('displays the date in the correct where the API start time is 1 minute past midnight', () => {
-    const startTime = displayStartTime({ startDate: '2020-01-01T00:01:00.000Z', licenceStartDate: '2020-01-01' })
+    const startTime = displayStartTime({ startDate: '2020-01-01T00:01:00.000Z', licenceStartDate: '2020-01-01' }, {})
     expect(startTime).toEqual('12:01am on 1 January 2020')
   })
   it('displays the date in the correct format where the API start time is 12pm', () => {
-    const startTime = displayStartTime({ startDate: '2021-01-01T12:00:00.000Z', licenceStartDate: '2020-01-01', licenceStartTime: '12' })
+    const mssgs = getSampleRequest().i18n.getCatalog()
+    const startTime = displayStartTime(
+      { startDate: '2021-01-01T12:00:00.000Z', licenceStartDate: '2020-01-01', licenceStartTime: '12' },
+      mssgs
+    )
     expect(startTime).toEqual('12:00pm (midday) on 1 January 2021')
   })
   it('displays the date in the correct format where the API start time is 3pm (UTC)', () => {
-    const startTime = displayStartTime({ startDate: '2021-01-01T15:00:00.000Z', licenceStartDate: '2021-01-01', licenceStartTime: '15' })
+    const startTime = displayStartTime(
+      { startDate: '2021-01-01T15:00:00.000Z', licenceStartDate: '2021-01-01', licenceStartTime: '15' },
+      {}
+    )
     expect(startTime).toEqual('3:00pm on 1 January 2021')
   })
   it('displays the date in the correct format where the API start time is 3pm (BST)', () => {
-    const startTime = displayStartTime({ startDate: '2021-07-01T14:00:00.000Z', licenceStartDate: '2021-07-01', licenceStartTime: '15' })
+    const startTime = displayStartTime(
+      { startDate: '2021-07-01T14:00:00.000Z', licenceStartDate: '2021-07-01', licenceStartTime: '15' },
+      {}
+    )
     expect(startTime).toEqual('3:00pm on 1 July 2021')
+  })
+
+  const getSampleRequest = () => ({
+    i18n: {
+      getCatalog: () => ({
+        licence_start_time_am_text_0: '0.00am (first minute of the day)',
+        licence_start_time_am_text_12: '12:00pm (midday)'
+      })
+    }
   })
 })
 
