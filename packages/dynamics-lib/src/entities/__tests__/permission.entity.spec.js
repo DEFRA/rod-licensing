24fd886f334c4454482613f449febff24f27a3b8
Phil Benson <phil.benson@defra.gov.uk>
2022-07-11 17:01:12 +0100

    Added more tests for permission entity

--- packages/dynamics-lib/src/entities/__tests__/permission.entity.spec.js
+++ packages/dynamics-lib/src/entities/__tests__/permission.entity.spec.js
@@ -1,9 +1,84 @@
 import { Permission, Contact, Permit, PoclFile, Transaction, retrieveGlobalOptionSets } from '../../index.js'
 
-let optionSetData
+const mockIds = [
+  '$45812ea6-991f-41b3-8760-03542f9cc130',
+  '$45812ea6-991f-41b3-8760-03542f9cc131',
+  '$45812ea6-991f-41b3-8760-03542f9cc132',
+  '$45812ea6-991f-41b3-8760-03542f9cc133',
+  '$45812ea6-991f-41b3-8760-03542f9cc134',
+  '$45812ea6-991f-41b3-8760-03542f9cc135',
+  '$45812ea6-991f-41b3-8760-03542f9cc136',
+  '$45812ea6-991f-41b3-8760-03542f9cc137',
+  '$45812ea6-991f-41b3-8760-03542f9cc138',
+  '$45812ea6-991f-41b3-8760-03542f9cc139'
+]
+jest.mock('uuid', () => ({
+  v4: () => {
+    if (mockIds.length) {
+      return mockIds.pop()
+    }
+    throw new Error('You need to add some ids to mockIds')
+  }
+}))
+
 describe('permission entity', () => {
-  beforeAll(async () => {
-    optionSetData = await retrieveGlobalOptionSets().cached()
+  const getOptionSetData = () => ({
+    defra_datasource: {
+      name: 'defra_datasource',
+      options: {
+        99999999: {
+          id: '99999999',
+          label: 'Door to door sales',
+          description: 'Door to door sales'
+        },
+        99999998: {
+          id: '99999998',
+          label: 'Direct',
+          description: 'Pushy direct marketing'
+        }
+      }
+    },
+    defra_islicenceforyou: {
+      name: 'defra_islicenceforyou',
+      options: {
+        0: {
+          id: '0',
+          label: 'Maybe Not',
+          description: 'Perhaps not...'
+        },
+        1: {
+          id: '1',
+          label: 'Maybe',
+          description: 'Definitely maybe'
+        }
+      }
+    },
+    defra_permittype: {
+      name: 'defra_permittype',
+      options: {
+        999999123: {
+          id: 999999123,
+          label: 'Licence for fishing',
+          description: 'Licence for fishing'
+        }
+      }
+    },
+    defra_permitsubtype: {
+      name: 'defra_permitsubtype',
+      options: {
+        999999124: {
+          id: 999999124,
+          label: 'Old Wellies',
+          description: 'W'
+        },
+        999999125: {
+          id: 999999124,
+          label: 'Shopping trollies',
+          description: 'S'
+        }
+      }
+    }
+
   })
 
   describe('maps from dynamics', () => {
@@ -17,41 +92,37 @@ describe('permission entity', () => {
           defra_startdate: '2019-12-14T00:00:00Z',
           defra_enddate: '2020-12-13T23:59:59Z',
           defra_stagingid: '71ad9a25-2a03-406b-a0e3-f4ff37799374',
-          defra_datasource: 910400003,
+          defra_datasource: 99999999,
           defra_licenceforyou: 1,
           defra_ismultibuy: true
         },
-        optionSetData
+        getOptionSetData()
       )
-
-    const getExpectedFields = () => ({
-      id: '347a9083-361e-ea11-a810-000d3a25c5d6',
-      referenceNumber: '00000000-2WC3FDR-CD379B',
-      issueDate: '2019-12-13T09:00:00Z',
-      startDate: '2019-12-14T00:00:00Z',
-      endDate: '2020-12-13T23:59:59Z',
-      stagingId: '71ad9a25-2a03-406b-a0e3-f4ff37799374',
-      dataSource: expect.objectContaining({ id: 910400003, label: 'Web Sales', description: 'Web Sales' }),
-      isLicenceForYou: expect.objectContaining({ id: 1, label: 'Yes', description: 'Yes' }),
-      isMultiBuy: true
-    })
 
     it('is a Permission instance', () => {
       expect(getPermission()).toBeInstanceOf(Permission)
     })
 
-    it('matches etag and expected fields', () => {
-      expect(getPermission()).toMatchObject(
-        expect.objectContaining({
-          etag: 'W/"186695153"',
-          ...getExpectedFields()
-        })
+    it.each([
+      getPermission(),
+      Permission.fromResponse(
+        {
+          '@odata.etag': 'W/"645930475"',
+          defra_permissionid: '8jue9j89-309d-lk23-j892-0fger0f9009d',
+          defra_name: '56787656-3TC3FFR-HJ789F',
+          defra_issuedate: '2021-06-30T22:23:15Z',
+          defra_startdate: '2021-06-30T22:53:15Z',
+          defra_enddate: '2022-06-30T22:53:15Z',
+          defra_stagingid: '7f9d97df-b547-4c52-9bc8-0b98a7fb06f8',
+          defra_datasource: 910400003,
+          defra_licenceforyou: 0,
+          defra_ismultibuy: false
+        },
+        getOptionSetData()
       )
-    })
-
-    it('matches JSON representation', () => {
+    ])('matches JSON representation', () => {
       const json = JSON.parse(getPermission().toString())
-      expect(json).toMatchObject(expect.objectContaining(getExpectedFields()))
+      expect(json).toMatchSnapshot()
     })
   })
 
@@ -74,7 +145,7 @@ describe('permission entity', () => {
         defra_advertisedprice_base: 6.0,
         defra_itemid: '42289'
       },
-      optionSetData
+      getOptionSetData()
     )
     // Mimic a new contact to test binding
     const contact = new Contact()
@@ -88,8 +159,8 @@ describe('permission entity', () => {
     permission.startDate = '2019-12-14T00:00:00Z'
     permission.endDate = '2020-12-13T23:59:59Z'
     permission.stagingId = '71ad9a25-2a03-406b-a0e3-f4ff37799374'
-    permission.dataSource = optionSetData.defra_datasource.options['910400003']
-    permission.isLicenceForYou = optionSetData.defra_islicenceforyou.options['1']
+    permission.dataSource = { id: '99999999', label: 'Door to Door', description: 'Door to door sales' }
+    permission.isLicenceForYou = { id: '1', label: 'Might be', description: 'Definitely maybe' }
     permission.isMultiBuy = true
 
     permission.bindToEntity(Permission.definition.relationships.licensee, contact)
@@ -99,21 +170,6 @@ describe('permission entity', () => {
 
     const dynamicsEntity = permission.toRequestBody()
 
-    expect(dynamicsEntity).toMatchObject(
-      expect.objectContaining({
-        defra_name: '00000000-2WC3FDR-CD379B',
-        defra_issuedate: '2019-12-13T09:00:00Z',
-        defra_startdate: '2019-12-14T00:00:00Z',
-        defra_enddate: '2020-12-13T23:59:59Z',
-        defra_stagingid: '71ad9a25-2a03-406b-a0e3-f4ff37799374',
-        defra_datasource: 910400003,
-        defra_licenceforyou: 1,
-        defra_ismultibuy: true,
-        'defra_PermitId@odata.bind': `/${Permit.definition.dynamicsCollection}(${permit.id})`,
-        'defra_ContactId@odata.bind': `$${contact.uniqueContentId}`,
-        'defra_Transaction@odata.bind': `$${transaction.uniqueContentId}`,
-        'defra_POCLFileId@odata.bind': `$${poclFile.uniqueContentId}`
-      })
-    )
+    expect(dynamicsEntity).toMatchSnapshot()
   })
 })
