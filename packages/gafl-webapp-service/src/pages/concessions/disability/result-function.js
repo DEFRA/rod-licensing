50211dbe2d9c8c7b359fdc9f950a3bd32061bb6f
michael steinacher <steinacherm@hotmail.co.uk>
2022-07-15 16:02:11 +0100

    Removed result function for disability page as it is passed by the back link handler instead

--- packages/gafl-webapp-service/src/pages/concessions/disability/result-function.js
+++
@@ -1,6 +0,0 @@
-import commonResultHandler from '../../../handlers/multibuy-amend-handler.js'
-
-export default async request => {
-  const routeDirection = commonResultHandler(request)
-  return routeDirection
-}
