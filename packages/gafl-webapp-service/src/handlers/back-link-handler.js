1047d210f2ea8fc6447906211323d459c08eec8c
michael steinacher <steinacherm@hotmail.co.uk>
2022-07-15 16:17:29 +0100

    Refactored back link handler

--- packages/gafl-webapp-service/src/handlers/back-link-handler.js
+++ packages/gafl-webapp-service/src/handlers/back-link-handler.js
@@ -1,17 +1,17 @@
 import { CHANGE_LICENCE_OPTIONS, LICENCE_SUMMARY, CONTACT_SUMMARY } from '../uri.js'
 import { LICENCE_SUMMARY_SEEN } from '../constants.js'
 
-export default async (status, defaultUrl) => {
+export default async (status, defaultUri) => {
   const summarySeen = status.fromSummary
   const changeLicenceOptionsSeen = status.fromLicenceOptions
-  if (summarySeen || changeLicenceOptionsSeen) {
-    if (changeLicenceOptionsSeen) {
-      return CHANGE_LICENCE_OPTIONS.uri
-    } else if (summarySeen === LICENCE_SUMMARY_SEEN) {
-      return LICENCE_SUMMARY.uri
-    }
+  if (changeLicenceOptionsSeen) {
+    return CHANGE_LICENCE_OPTIONS.uri
+  }
+  if (summarySeen === LICENCE_SUMMARY_SEEN) {
+    return LICENCE_SUMMARY.uri
+  }
+  if (summarySeen) {
     return CONTACT_SUMMARY.uri
   }
-
-  return defaultUrl
+  return defaultUri
 }
