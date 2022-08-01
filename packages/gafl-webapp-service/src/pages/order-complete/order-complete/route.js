11bb9cff5adf85eccdc494c3a9f1ff58f721c543
Michael Steinacher <49474728+MickStein@users.noreply.github.com>
2022-08-01 09:12:27 +0100

    feature/iwtf 2995 Easy renewal, renewal start date welsh translation (#1403)
    * Easy renewal, renewal start date welsh translation
    
    https://eaflood.atlassian.net/browse/IWTF-2995
    
    Add translations to easy renewal start date page.

--- packages/gafl-webapp-service/src/pages/order-complete/order-complete/route.js
+++ packages/gafl-webapp-service/src/pages/order-complete/order-complete/route.js
@@ -29,7 +29,7 @@ const getData = async request => {
   await request.cache().helpers.status.set({ [COMPLETION_STATUS.completed]: true })
   await request.cache().helpers.status.setCurrentPermission({ currentPage: ORDER_COMPLETE.page })
 
-  const startTimeStringTitle = displayStartTime(permission, request.i18n.getCatalog())
+  const startTimeStringTitle = displayStartTime(request, permission)
   return {
     permission,
     startTimeStringTitle,
