13ed780284dd3a71fdfb89932cb0962de88f934d
Phil Benson <phil.benson@defra.gov.uk>
2022-08-12 15:33:59 +0100

    Fix lint

--- packages/gafl-webapp-service/src/handlers/multibuy-for-you-handler.js
+++ packages/gafl-webapp-service/src/handlers/multibuy-for-you-handler.js
@@ -2,5 +2,5 @@ export const isMultibuyForYou = async request => {
   const transaction = await request.cache().helpers.transaction.get()
   const { isLicenceForYou } = await request.cache().helpers.transaction.getCurrentPermission()
 
-  return (transaction.permissions.length > 1 && isLicenceForYou)
+  return transaction.permissions.length > 1 && isLicenceForYou
 }
