9618da780204a744fd25f96450875174978c828a
Phil Benson <phil.benson@defra.gov.uk>
2022-08-12 15:33:06 +0100

    Collapse if...then into single statement

--- packages/gafl-webapp-service/src/handlers/multibuy-for-you-handler.js
+++ packages/gafl-webapp-service/src/handlers/multibuy-for-you-handler.js
@@ -2,8 +2,5 @@ export const isMultibuyForYou = async request => {
   const transaction = await request.cache().helpers.transaction.get()
   const { isLicenceForYou } = await request.cache().helpers.transaction.getCurrentPermission()
 
-  if (transaction.permissions.length > 1 && isLicenceForYou) {
-    return true
-  }
-  return false
+  return (transaction.permissions.length > 1 && isLicenceForYou)
 }
