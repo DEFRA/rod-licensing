e6f317391bdedf90b9a6e20a0aaf8c171b492004
Phil Benson <phil.benson@defra.gov.uk>
2022-09-20 15:22:37 +0100

    Actioning analytics banner throws an error (#1481)
    Actioning analytics banner throws an error
    
    https://eaflood.atlassian.net/browse/IWTF-3085
    
    Whether accept or reject is pressed on the analytics banner, an error is thrown in pre-prod
    
    Refactored to use url rather than private _url property in request, also _only_ use referer header for redirect check rather than host, as the latter is always the current page host, not the referrer. Also refactored tests, including changing mock host as it was including the protocol which doesn't occur in the wild

--- packages/gafl-webapp-service/src/handlers/__tests__/analytics-handler.spec.js
+++ packages/gafl-webapp-service/src/handlers/__tests__/analytics-handler.spec.js
@@ -20,66 +20,49 @@ describe('The analytics handler', () => {
   })
 
   it.each([
-    ['https://localhost:3000', 'https://localhost:3000/buy/name', 'https://localhost:3000', '/buy/name'],
-    ['https://localhost:1234', 'https://localhost:1234/example/test/redirect', 'https://localhost:1234', '/example/test/redirect'],
+    ['https://localhost:3000', 'https://localhost:3000/buy/name', 'localhost:3000', '/buy/name'],
+    ['https://localhost:1234', 'https://localhost:1234/example/test/redirect', 'localhost:1234', '/example/test/redirect'],
     [
       'https://testserver-example-fish',
       'https://testserver-example-fish/buy/renew/identify',
-      'https://testserver-example-fish',
+      'testserver-example-fish',
       '/buy/renew/identify'
     ]
   ])('redirects to correct page if the HTTP_REFERER host matches the host of the current page', async (origin, referer, host, redirect) => {
-    const payload = {
-      analyticsResponse: 'accept'
-    }
-    const headers = {
-      origin: origin,
-      referer: referer,
-      host: host
-    }
+    const payload = { analyticsResponse: 'accept' }
+    const headers = { origin, referer }
     const request = generateRequestMock(payload, 'analytics', headers, host)
     const responseToolkit = generateResponseToolkitMock()
+
     await analyticsHandler(request, responseToolkit)
+
     expect(responseToolkit.redirect).toHaveBeenCalledWith(redirect)
   })
 
   it.each([
-    ['https://localhost:3000', 'https://localhost:3000/buy/name', 'https://localhost:3000', 'https://localhost:3047'],
-    ['https://localhost:1234', 'https://localhost:1234/example/test/redirect', 'https://localhost:1234', 'https://notsamehost:1234'],
+    ['https://localhost:3000', 'https://localhost:3000/buy/name', 'localhost:3047'],
+    ['https://localhost:1234', 'https://localhost:1234/example/test/redirect', 'notsamehost:1234'],
+    ['https://testserver-example-fish', 'https://testserver-example-fish/buy/renew/identify', 'hdfhdskfhs-ghj-vgjh'],
     [
-      'https://testserver-example-fish',
-      'https://testserver-example-fish/buy/renew/identify',
-      'https://testserver-example-fish',
-      'https://hdfhdskfhs-ghj-vgjh'
+      'https://get-a-fishing-licence.gov.uk/buy/licence-for',
+      'https://some.shady.host/not/where/we/should/go',
+      'get-a-fishing-licence.gov.uk'
     ]
-  ])(
-    'redirects to /buy if the HTTP_REFERER host does not match the host of the current page',
-    async (origin, referer, headersHost, host) => {
-      const payload = {
-        analyticsResponse: 'accept'
-      }
-      const headers = {
-        origin: origin,
-        referer: referer,
-        host: headersHost
-      }
-      const request = generateRequestMock(payload, 'analytics', headers, host)
-      const responseToolkit = generateResponseToolkitMock()
-      await analyticsHandler(request, responseToolkit)
-      expect(responseToolkit.redirect).toHaveBeenCalledWith('/buy')
-    }
-  )
+  ])('redirects to /buy if the HTTP_REFERER host does not match the host of the current page', async (origin, referer, host) => {
+    const payload = { analyticsResponse: 'accept' }
+    const headers = { origin, referer }
+    const request = generateRequestMock(payload, 'analytics', headers, host)
+    const responseToolkit = generateResponseToolkitMock()
+
+    await analyticsHandler(request, responseToolkit)
+
+    expect(responseToolkit.redirect).toHaveBeenCalledWith('/buy')
+  })
 
   it('calls addLanguageCodeToUri with request and /buy', async () => {
-    const headers = {
-      origin: 'https://localhost:3000',
-      referer: 'https://localhost:3000/buy'
-    }
-
+    const headers = { origin: 'https://localhost:3000', referer: 'https://localhost:3000/buy' }
     const redirect = '/buy'
-    const payload = {
-      analyticsResponse: 'accept'
-    }
+    const payload = { analyticsResponse: 'accept' }
     const responseToolkit = generateResponseToolkitMock()
     const request = generateRequestMock(payload, 'analytics', headers)
 
@@ -89,20 +72,18 @@ describe('The analytics handler', () => {
   })
 
   it('selected not true and response is accept sets selected to true and acceptTracking to true', async () => {
-    const payload = {
-      analyticsResponse: 'accept'
-    }
+    const payload = { analyticsResponse: 'accept' }
+    const request = generateRequestMock(payload, 'analytics')
 
-    const request = generateRequestMock(payload, 'analytics', 'origin', 'referer')
     await analyticsHandler(request, generateResponseToolkitMock())
+
     expect(mockAnalyticsSet).toHaveBeenCalledWith(expect.objectContaining({ [ANALYTICS.selected]: true, [ANALYTICS.acceptTracking]: true }))
   })
 
   it('selected not true and response is reject sets selected to true and acceptTracking to false', async () => {
-    const payload = {
-      analyticsResponse: 'reject'
-    }
-    const request = generateRequestMock(payload, 'analytics', 'origin', 'referer')
+    const payload = { analyticsResponse: 'reject' }
+    const request = generateRequestMock(payload, 'analytics')
+
     await analyticsHandler(request, generateResponseToolkitMock())
 
     expect(mockAnalyticsSet).toHaveBeenCalledWith(
@@ -111,11 +92,9 @@ describe('The analytics handler', () => {
   })
 
   it('selected is true sets seenMessage to true', async () => {
-    const analytics = {
-      [ANALYTICS.selected]: true
-    }
+    const analytics = { [ANALYTICS.selected]: true }
+    const request = generateRequestMock('payload', analytics)
 
-    const request = generateRequestMock('payload', analytics, 'origin', 'referer')
     await analyticsHandler(request, generateResponseToolkitMock())
 
     expect(mockAnalyticsSet).toHaveBeenCalledWith(expect.objectContaining({ [ANALYTICS.seenMessage]: true }))
@@ -123,9 +102,15 @@ describe('The analytics handler', () => {
 
   const mockAnalyticsSet = jest.fn()
 
-  const generateRequestMock = (payload, analytics, headers, host = {}) => ({
+  const generateRequestMock = (
+    payload,
+    analytics,
+    headers = { origin: 'http://host/current/page', referer: 'http://host/previous/page' },
+    host = 'host'
+  ) => ({
     payload,
     url: {
+      host,
       search: ''
     },
     cache: jest.fn(() => ({
@@ -136,10 +121,7 @@ describe('The analytics handler', () => {
         }
       }
     })),
-    headers,
-    _url: {
-      host
-    }
+    headers
   })
 
   const generateResponseToolkitMock = () => ({
