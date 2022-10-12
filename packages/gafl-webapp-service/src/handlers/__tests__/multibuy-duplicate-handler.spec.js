277f2c2b29dc46b7d129e5bdd88654bd0de0b37f
ScottDormand96 <sdormand@hotmail.com>
2022-10-12 14:00:04 +0100

    added further no duplicate tests

--- packages/gafl-webapp-service/src/handlers/__tests__/multibuy-duplicate-handler.spec.js
+++ packages/gafl-webapp-service/src/handlers/__tests__/multibuy-duplicate-handler.spec.js
@@ -12,29 +12,51 @@ describe('The multiple check for duplicates handler', () => {
     start: '2023-01-01',
     price: 70
   })
+  // different licenceHolder
   const getLicenceTwo = () => ({
     licenceHolder: 'Angler 2',
-    type: 'Coarse 3 rod',
+    type: 'Salmon and sea trout',
     length: '12M',
-    start: '2023-01-21',
-    price: 50
+    start: '2023-01-01',
+    price: 70
   })
+  // different type
   const getLicenceThree = () => ({
-    licenceHolder: 'Angler 3',
+    licenceHolder: 'Angler 1',
     type: 'Coarse 2 rod',
     length: '12M',
-    start: '2023-02-12',
-    price: 50
+    start: '2023-01-01',
+    price: 70
   })
+  // different length
+  const getLicenceFour = () => ({
+    licenceHolder: 'Angler 1',
+    type: 'Salmon and sea trout',
+    length: '3M',
+    start: '2023-01-01',
+    price: 70
+  })
+  // different start
+  const getLicenceFive = () => ({
+    licenceHolder: 'Angler 1',
+    type: 'Salmon and sea trout',
+    length: '3M',
+    start: '2023-02-01',
+    price: 70
+  })
+
   it('identifies when array has duplicate licences (same licenceHolder, type, length and start)', async () => {
     const licenses = [getLicenceOne(), getLicenceOne(), getLicenceTwo()]
     const result = await hasDuplicates(licenses)
     expect(result).toBeTruthy()
   })
 
-  it('identifies when array has no duplicate licences', async () => {
-    const licenses = [getLicenceOne(), getLicenceTwo(), getLicenceThree()]
-    const result = await hasDuplicates(licenses)
-    expect(result).toBeFalsy()
-  })
+  it.each([[getLicenceTwo()], [getLicenceThree()], [getLicenceFour()], [getLicenceFive()]])(
+    'identifies when array has no duplicate licences',
+    async licence => {
+      const licenses = [getLicenceOne(), licence]
+      const result = await hasDuplicates(licenses)
+      expect(result).toBeFalsy()
+    }
+  )
 })
