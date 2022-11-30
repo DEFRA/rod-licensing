f4f6cc5bcf0c1990b02a940978190b39f9bdcee4
michael steinacher <steinacherm@hotmail.co.uk>
2022-11-30 10:49:31 +0000

    Updated backLinkHandler to also return CHANGE_CONTACT_DETAILS uri

--- packages/gafl-webapp-service/src/handlers/__tests__/back-link-handler.spec.js
+++ packages/gafl-webapp-service/src/handlers/__tests__/back-link-handler.spec.js
@@ -1,12 +1,28 @@
 import backLinkHandler from '../back-link-handler'
-import { CHANGE_LICENCE_OPTIONS, LICENCE_SUMMARY, CONTACT_SUMMARY } from '../../uri.js'
-import { LICENCE_SUMMARY_SEEN, CONTACT_SUMMARY_SEEN, CHANGE_LICENCE_OPTIONS_SEEN } from '../../constants.js'
+import { CHANGE_LICENCE_OPTIONS, LICENCE_SUMMARY, CONTACT_SUMMARY, CHANGE_CONTACT_DETAILS } from '../../uri.js'
+import { LICENCE_SUMMARY_SEEN, CONTACT_SUMMARY_SEEN, CHANGE_LICENCE_OPTIONS_SEEN, CHANGE_CONTACT_DETAILS_SEEN } from '../../constants.js'
 
 describe('back-link-handler', () => {
   describe('default', () => {
     it('should return defaultUrl if request does not have cache function', async () => {
       const result = await backLinkHandler({}, 'aDefaultUrl')
       expect(result).toBe('aDefaultUrl')
+    })
+
+    it('should return CHANGE_LICENCE_OPTIONS uri if changeLicenceOptionsSeen is true', async () => {
+      const status = {
+        fromLicenceOptions: CHANGE_LICENCE_OPTIONS_SEEN.SEEN
+      }
+      const result = await backLinkHandler(status, 'aPage')
+      expect(result).toBe(CHANGE_LICENCE_OPTIONS.uri)
+    })
+
+    it('should return CHANGE_CONTACT_DETAILS uri if changeContactDetailsSeen is true', async () => {
+      const status = {
+        fromContactDetails: CHANGE_CONTACT_DETAILS_SEEN.SEEN
+      }
+      const result = await backLinkHandler(status, 'aPage')
+      expect(result).toBe(CHANGE_CONTACT_DETAILS.uri)
     })
 
     it('should return CHANGE_LICENCE_OPTIONS uri if changeLicenceOptionsSeen is true', async () => {
--- packages/gafl-webapp-service/src/handlers/back-link-handler.js
+++ packages/gafl-webapp-service/src/handlers/back-link-handler.js
@@ -1,11 +1,15 @@
-import { CHANGE_LICENCE_OPTIONS, LICENCE_SUMMARY, CONTACT_SUMMARY } from '../uri.js'
+import { CHANGE_LICENCE_OPTIONS, LICENCE_SUMMARY, CONTACT_SUMMARY, CHANGE_CONTACT_DETAILS } from '../uri.js'
 import { LICENCE_SUMMARY_SEEN } from '../constants.js'
 
 export default async (status, defaultUri) => {
   const summarySeen = status.fromSummary
   const changeLicenceOptionsSeen = status.fromLicenceOptions
+  const fromContactDetailsSeen = status.fromContactDetails
   if (changeLicenceOptionsSeen) {
     return CHANGE_LICENCE_OPTIONS.uri
+  }
+  if (fromContactDetailsSeen) {
+    return CHANGE_CONTACT_DETAILS.uri
   }
   if (summarySeen === LICENCE_SUMMARY_SEEN) {
     return LICENCE_SUMMARY.uri
