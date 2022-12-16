0168e5956517d9507c4a73abcbbabebe00405757
Scott Dormand <61588381+ScottDormand96@users.noreply.github.com>
2022-12-16 14:10:13 +0000

    Multibuy - Only display relevant terms and conditions (#1548)
    * Multibuy - Only display relevant terms and conditions
    
    https://eaflood.atlassian.net/browse/IWTF-3004
    
    Only display terms and conditions for licences that have been selected
    
    * change route
    
    * added tests
    
    * change njk files and add test
    
    * byelaws wording
    
    * change frontend to pass in data .
    
    * move outside describe block
    
    * pass type to getMockPermission instead of return value of licenceTypeDisplay
    
    * change to use .some and refactor tests
    
    * set rods as string
    
    * remove logs and refactor tests
    
    * remove let from setting up data
    
    * added getSamplePermissions

--- packages/gafl-webapp-service/src/pages/terms-and-conditions/__tests__/route.spec.js
+++ packages/gafl-webapp-service/src/pages/terms-and-conditions/__tests__/route.spec.js
@@ -2,18 +2,49 @@ import { getData, validator } from '../route'
 import pageRoute from '../../../routes/page-route.js'
 import { nextPage } from '../../../routes/next-page.js'
 import { LICENCE_SUMMARY, CONTACT_SUMMARY } from '../../../uri.js'
+import { licenceTypeDisplay } from '../../../processors/licence-type-display.js'
+import { LICENCE_TYPE } from '../../../processors/mapping-constants.js'
+
+jest.mock('../../../processors/mapping-constants.js', () => ({
+  LICENCE_TYPE: {
+    'trout-and-coarse': 'Test trout and coarse',
+    'salmon-and-sea-trout': 'Test Salmon'
+  }
+}))
 
 jest.mock('../../../routes/page-route.js')
 
-const getMockPermission = price => ({
+jest.mock('../../../processors/licence-type-display.js', () => ({
+  licenceTypeDisplay: jest.fn()
+}))
+
+const getMockPermission = (price, type, rods) => ({
   licensee: {
     firstName: 'Turanga',
     lastName: 'Leela'
   },
-  licenceType: 'trout-and-coarse',
-  numberOfRods: '2',
+  licenceType: type,
+  numberOfRods: rods,
   licenceLength: '8D',
   permit: { cost: price }
+})
+
+const catalog = Symbol('mock catalog')
+
+const generateMockRequest = (transactionGet = {}, statusGet = { [LICENCE_SUMMARY.page]: true, [CONTACT_SUMMARY.page]: true }) => ({
+  cache: () => ({
+    helpers: {
+      transaction: {
+        get: async () => transactionGet
+      },
+      status: {
+        getCurrentPermission: async () => statusGet
+      }
+    }
+  }),
+  i18n: {
+    getCatalog: () => catalog
+  }
 })
 
 describe('terms-and-conditions > route', () => {
@@ -24,28 +55,15 @@ describe('terms-and-conditions > route', () => {
   })
 
   describe('getData', () => {
-    const generateMockRequest = (statusGet = {}, transactionGet = {}) => ({
-      cache: () => ({
-        helpers: {
-          transaction: {
-            get: async () => transactionGet
-          },
-          status: {
-            getCurrentPermission: async () => statusGet
-          }
-        }
-      })
-    })
-
     beforeEach(() => jest.clearAllMocks())
 
     it('LICENCE_SUMMARY redirect', async () => {
-      const func = () => getData(generateMockRequest({ [LICENCE_SUMMARY.page]: false }))
+      const func = () => getData(generateMockRequest({}, { [LICENCE_SUMMARY.page]: false }))
       await expect(func).rejects.toThrowRedirectTo(LICENCE_SUMMARY.uri)
     })
 
     it('CONTACT_SUMMARY redirect', async () => {
-      const func = () => getData(generateMockRequest({ [LICENCE_SUMMARY.page]: true, [CONTACT_SUMMARY.page]: false }))
+      const func = () => getData(generateMockRequest({}, { [LICENCE_SUMMARY.page]: true, [CONTACT_SUMMARY.page]: false }))
       await expect(func).rejects.toThrowRedirectTo(CONTACT_SUMMARY.uri)
     })
 
@@ -57,11 +75,62 @@ describe('terms-and-conditions > route', () => {
     ])('returns whether payment is required', async (price, anotherPrice, paymentRequired) => {
       const data = await getData(
         generateMockRequest(
-          { [LICENCE_SUMMARY.page]: true, [CONTACT_SUMMARY.page]: true },
-          { permissions: [getMockPermission(price), getMockPermission(anotherPrice)] }
+          { permissions: [getMockPermission(price), getMockPermission(anotherPrice)] },
+          { [LICENCE_SUMMARY.page]: true, [CONTACT_SUMMARY.page]: true }
         )
       )
       expect(data.paymentRequired).toBe(paymentRequired)
+    })
+
+    const troutAndCoarse2Rods = '2 rods, Trout and coarse'
+    const troutAndCoarse3Rods = '3 rods, Trout and coarse'
+    const salmonAndSeaTrout = 'Salmon'
+
+    it.each([[troutAndCoarse2Rods], [troutAndCoarse3Rods], [salmonAndSeaTrout]])(
+      'licenceTypeDisplay is called with permission and catalog',
+      async type => {
+        const permission = getMockPermission(10, type)
+        await getData(
+          generateMockRequest({
+            permissions: [permission]
+          })
+        )
+
+        expect(licenceTypeDisplay).toHaveBeenCalledWith(permission, catalog)
+      }
+    )
+
+    const getSamplePermissions = (type, rods) => {
+      return [getMockPermission(1, type, rods), getMockPermission(1, type, rods), getMockPermission(0, 'type', '4')]
+    }
+
+    const mockGetData = async (type, rods) => {
+      return await getData(
+        generateMockRequest({
+          permissions: getSamplePermissions(type, rods)
+        })
+      )
+    }
+
+    describe.each([
+      [LICENCE_TYPE['trout-and-coarse'], '2', true, false, false],
+      [LICENCE_TYPE['trout-and-coarse'], '3', false, true, false],
+      [LICENCE_TYPE['salmon-and-sea-trout'], '1', false, false, true]
+    ])('testing whether correct type is set to display', (type, rods, troutTwoFlag, troutThreeFlag, salmonFlag) => {
+      it('returns whether to display Trout and coarse, up to 2 rods conditions', async () => {
+        const result = await mockGetData(type, rods)
+        expect(result.troutAndCoarse2Rods).toBe(troutTwoFlag)
+      })
+
+      it('returns whether to display Trout and coarse, up to 3 rods conditions', async () => {
+        const result = await mockGetData(type, rods)
+        expect(result.troutAndCoarse3Rods).toBe(troutThreeFlag)
+      })
+
+      it('returns whether to display Salmon and sea trout conditions', async () => {
+        const result = await mockGetData(type, rods)
+        expect(result.salmonAndSeaTrout).toBe(salmonFlag)
+      })
     })
   })
 })
--- packages/gafl-webapp-service/src/pages/terms-and-conditions/route.js
+++ packages/gafl-webapp-service/src/pages/terms-and-conditions/route.js
@@ -2,12 +2,14 @@ import pageRoute from '../../routes/page-route.js'
 import Joi from 'joi'
 import { TERMS_AND_CONDITIONS, CONTACT_SUMMARY, LICENCE_SUMMARY } from '../../uri.js'
 import { nextPage } from '../../routes/next-page.js'
-
+import { licenceTypeDisplay } from '../../processors/licence-type-display.js'
+import { LICENCE_TYPE } from '../../processors/mapping-constants.js'
 import GetDataRedirect from '../../handlers/get-data-redirect.js'
 
 export const getData = async request => {
   const transaction = await request.cache().helpers.transaction.get()
   const status = await request.cache().helpers.status.getCurrentPermission()
+  const mssgs = request.i18n.getCatalog()
 
   if (!status[LICENCE_SUMMARY.page]) {
     throw new GetDataRedirect(LICENCE_SUMMARY.uri)
@@ -18,12 +20,20 @@ export const getData = async request => {
   }
 
   const licences = transaction.permissions.map(permission => ({
+    type: licenceTypeDisplay(permission, mssgs),
     price: permission.permit.cost
   }))
 
   return {
-    paymentRequired: priceCalculation(licences)
+    paymentRequired: priceCalculation(licences),
+    troutAndCoarse2Rods: checkLicenceType(transaction, LICENCE_TYPE['trout-and-coarse'], '2'),
+    troutAndCoarse3Rods: checkLicenceType(transaction, LICENCE_TYPE['trout-and-coarse'], '3'),
+    salmonAndSeaTrout: checkLicenceType(transaction, LICENCE_TYPE['salmon-and-sea-trout'], '1')
   }
+}
+
+const checkLicenceType = (transaction, type, rods) => {
+  return transaction.permissions.some(p => p.licenceType === type && p.numberOfRods === rods)
 }
 
 const priceCalculation = licences => {
