6296d8c1ee2253d3af426f6192d753055f1cf55d
irisfaraway <iris.faraway@gmail.com>
2022-12-23 15:35:39 +0000

    Refactor new session handler and add tests

--- packages/gafl-webapp-service/src/handlers/new-session-handler.js
+++ packages/gafl-webapp-service/src/handlers/new-session-handler.js
@@ -1,11 +1,10 @@
 import { CONTROLLER } from '../uri.js'
 import { initialiseAnalyticsSessionData } from '../processors/analytics.js'
-import { addLanguageCodeToUri } from '../processors/uri-helper.js'
 
 export default async (request, h) => {
   // The user may have an existing session, in which case we need to examine this for attribution and/or clientId
   const existingCacheStatus = await request.cache().helpers.status.get()
   await request.cache().initialize()
   await initialiseAnalyticsSessionData(request, existingCacheStatus)
-  return h.redirect(addLanguageCodeToUri(request, CONTROLLER.uri))
+  return h.redirectWithLanguageCode(request, CONTROLLER.uri)
 }
