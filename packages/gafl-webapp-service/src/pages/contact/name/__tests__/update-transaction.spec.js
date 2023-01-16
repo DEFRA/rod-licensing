ad4e7781a2110548617fbdb0b13f363664266c01
michael steinacher <steinacherm@hotmail.co.uk>
2023-01-16 16:20:35 +0000

    Refactored tests to make mocks more flexible and make tests simpler.

--- packages/gafl-webapp-service/src/pages/contact/name/__tests__/update-transaction.spec.js
+++ packages/gafl-webapp-service/src/pages/contact/name/__tests__/update-transaction.spec.js
@@ -1,11 +1,15 @@
 import { NAME } from '../../../../uri.js'
 import updateTransaction from '../update-transaction.js'
 
-const createRequestMock = (pageData, licenseeData, setCurrentPermission = jest.fn()) => ({
+const createRequestMock = ({
+  getPageData = () => ({ payload: {} }),
+  licenseeData = { licensee: {} },
+  setCurrentPermission = () => {}
+}) => ({
   cache: () => ({
     helpers: {
       page: {
-        getCurrentPermission: pageData
+        getCurrentPermission: getPageData
       },
       transaction: {
         getCurrentPermission: () => licenseeData,
@@ -18,30 +22,26 @@ const createRequestMock = (pageData, licenseeData, setCurrentPermission = jest.f
 describe('update-transaction', () => {
   beforeEach(jest.clearAllMocks)
   it('gets the payload from the name page cache', async () => {
-    const samplePayload = { payload: { 'first-name': 'firstName', 'last-name': 'lastName' } }
-    const licenseeData = { licensee: { firstName: 'Anikan', lastName: 'Skywalker' } }
-    const getCurrentPermission = jest.fn(() => samplePayload)
-    const mockRequest = createRequestMock(getCurrentPermission, licenseeData)
-    await updateTransaction(mockRequest)
-    expect(getCurrentPermission).toHaveBeenCalledWith(NAME.page)
+    const getPageData = jest.fn(() => ({ payload: {} }))
+    await updateTransaction(createRequestMock({ getPageData }))
+    expect(getPageData).toHaveBeenCalledWith(NAME.page)
   })
 
   it('sets current transaction permission to be value of transaction getCurrentPermission', async () => {
-    const licenseeData = { licensee: {} }
+    const licenseeData = { licensee: { firstName: undefined, lastName: undefined } }
     const setCurrentPermission = jest.fn()
-    const getCurrentPermission = () => ({ payload: { firstName: '', lastName: '' } })
-    const mockRequest = createRequestMock(getCurrentPermission, licenseeData, setCurrentPermission)
+    const mockRequest = createRequestMock({ licenseeData, setCurrentPermission })
     await updateTransaction(mockRequest)
     expect(setCurrentPermission).toHaveBeenCalledWith(licenseeData)
   })
 
-  it('sets sample permission first-name and last-name values to the payload values', async () => {
+  it('sets current transaction permission first-name and last-name values to the payload values', async () => {
     const firstName = Symbol('Homer')
     const lastName = Symbol('Simpson')
     const samplePayload = { payload: { 'first-name': firstName, 'last-name': lastName } }
     const licenseeData = { licensee: { firstName: 'Anikan', lastName: 'Skywalker' } }
-    const getCurrentPermission = () => samplePayload
-    const mockRequest = createRequestMock(getCurrentPermission, licenseeData)
+    const getPageData = () => samplePayload
+    const mockRequest = createRequestMock({ getPageData, licenseeData })
     await updateTransaction(mockRequest)
     expect(licenseeData).toEqual(
       expect.objectContaining({
