3da992fd8d45902451dde3a00ba0b4785f811d4c
Phil Benson <phil.benson@defra.gov.uk>
2023-01-17 16:51:40 +0000

    Refactored tests for isMinor / isJunior / isSenior, added new senior age starting after 1st April 2023

--- packages/business-rules-lib/src/util/ages.js
+++ packages/business-rules-lib/src/util/ages.js
@@ -1,9 +1,14 @@
+import moment from 'moment'
+
 /** The maximum age at which an angler is considered to be a minor (free licence) */
 export const MINOR_MAX_AGE = 12
 /** The maximum age at which an angler is entitled to a junior concession */
 export const JUNIOR_MAX_AGE = 16
 /** The minimum age at which an angler becomes entitled to a senior concession */
 export const SENIOR_MIN_AGE = 65
+export const NEW_SENIOR_MIN_AGE = 66
+export const SENIOR_AGE_CHANGE_DATE = '2023-04-01'
+const changeoverMoment = moment(SENIOR_AGE_CHANGE_DATE)
 
 /**
  * Determine if the provided age is classified as a minor
@@ -22,6 +27,10 @@ export const isJunior = age => age > MINOR_MAX_AGE && age <= JUNIOR_MAX_AGE
 /**
  * Determine if the provided age is classified as a senior
  * @param {number} age The age to be tested
+ * @param {string} permissionStartDate in format YYYY-MM-DD
  * @returns {boolean} true if the given age should be classified as a senior
  */
-export const isSenior = age => age >= SENIOR_MIN_AGE
+export const isSenior = (age, permissionStartDate) => {
+  const permissionStartsAfterChangeover = changeoverMoment.isSameOrBefore(permissionStartDate)
+  return permissionStartsAfterChangeover ? age >= NEW_SENIOR_MIN_AGE : age >= SENIOR_MIN_AGE
+}
