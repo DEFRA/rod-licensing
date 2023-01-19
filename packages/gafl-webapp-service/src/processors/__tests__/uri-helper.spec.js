0996290247ba47e0f9dd58c5735d7ab9d8f79756
Iris Faraway <iris.faraway@gmail.com>
2023-01-19 16:08:01 +0000

    Stop language parameters from being duplicated in URLs (#1603)
    https://eaflood.atlassian.net/browse/IWTF-3111
    
    Testing revealed that the links to accept and reject cookies were failing when the language was set to Welsh. This turned out to be because multiple `lang` parameters were being added - for example, `/buy?lang=cy&lang=cy`
    
    We don't want this to happen, so this PR changes the addLanguageCodeToUri function to strip any existing `lang=` params from the provided URL so we can make sure we only add the relevant ones.

--- packages/gafl-webapp-service/src/processors/__tests__/uri-helper.spec.js
+++ packages/gafl-webapp-service/src/processors/__tests__/uri-helper.spec.js
@@ -45,4 +45,22 @@ describe('URI Helpers: addLanguageCodeToURI', () => {
       expect(result).toEqual(decoratedUrl)
     })
   })
+
+  describe.each([
+    ['https://my-url.com/path?data=true&lang=cy', 'https://my-url.com/path?data=true&lang=cy'],
+    ['https://my-url.com/path?lang=cy', 'https://my-url.com/path?lang=cy'],
+    ['https://my-url.com/path?data=true&lang=en', 'https://my-url.com/path?data=true&lang=cy'],
+    ['https://my-url.com/path?lang=en', 'https://my-url.com/path?lang=cy']
+  ])('', (suppliedPath, expectedPath) => {
+    it('if the supplied url already has a language parameter on it, it does not add a duplicate or conflicting parameter', () => {
+      const mockRequest = {
+        path: suppliedPath,
+        url: {
+          search: '?lang=cy'
+        }
+      }
+      const result = addLanguageCodeToUri(mockRequest, suppliedPath)
+      expect(result).toEqual(expectedPath)
+    })
+  })
 })
