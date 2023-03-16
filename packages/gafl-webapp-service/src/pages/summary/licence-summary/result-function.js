0a435d2e36a4294710646012d562de4b8113e961
irisfaraway <iris.faraway@gmail.com>
2023-03-16 15:13:12 +0000

    Use new isPhysical on licence summary page

--- packages/gafl-webapp-service/src/pages/summary/licence-summary/result-function.js
+++ packages/gafl-webapp-service/src/pages/summary/licence-summary/result-function.js
@@ -1,12 +1,12 @@
 import { CONTACT_SUMMARY_SEEN, CommonResults, ShowDigitalLicencePages } from '../../../constants.js'
-import { isPhysicalOld } from '../../../processors/licence-type-display.js'
+import { isPhysical } from '../../../processors/licence-type-display.js'
 
 export default async request => {
   const status = await request.cache().helpers.status.getCurrentPermission()
   const permission = await request.cache().helpers.transaction.getCurrentPermission()
 
   if (permission.isRenewal) {
-    if (isPhysicalOld(permission) && status.showDigitalLicencePages) {
+    if (isPhysical(permission) && status.showDigitalLicencePages) {
       return ShowDigitalLicencePages.YES
     } else {
       return CommonResults.SUMMARY
