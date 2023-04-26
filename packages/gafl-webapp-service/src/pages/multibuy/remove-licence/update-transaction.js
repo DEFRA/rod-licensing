5b9dd7af120796750f787857d58ba902d96e1128
ScottDormand96 <sdormand@hotmail.com>
2023-04-26 08:31:20 +0100

    remove all of permission including status, address lookup and page

--- packages/gafl-webapp-service/src/pages/multibuy/remove-licence/update-transaction.js
+++ packages/gafl-webapp-service/src/pages/multibuy/remove-licence/update-transaction.js
@@ -1,17 +1,24 @@
 export default async request => {
-  const permission = await request.cache().helpers.transaction.getCurrentPermission()
   const transaction = await request.cache().helpers.transaction.get()
+  const page = await request.cache().helpers.page.get()
+  const status = await request.cache().helpers.status.get()
+  const addressLookup = await request.cache().helpers.addressLookup.get()
 
-  transaction.permissions = transaction.permissions.filter(
-    item =>
-      item.licensee.firstName !== permission.licensee.firstName ||
-      item.licensee.lastName !== permission.licensee.lastName ||
-      item.isLicenceForYou !== permission.isLicenceForYou ||
-      item.licenceType !== permission.licenceType ||
-      item.licenceLength !== permission.licenceLength ||
-      item.licenceToStart !== permission.licenceToStart ||
-      item.permit.cost !== permission.permit.cost
-  )
+  const transactionPermission = await request.cache().helpers.transaction.getCurrentPermission()
+  const pagePermission = await request.cache().helpers.page.getCurrentPermission()
+  const statusPermission = await request.cache().helpers.status.getCurrentPermission()
+  const addressPermission = await request.cache().helpers.addressLookup.getCurrentPermission()
+
+  transaction.permissions.splice(transactionPermission, 1)
+  page.permissions.splice(pagePermission, 1)
+  status.permissions.splice(statusPermission, 1)
+  addressLookup.permissions.splice(addressPermission, 1)
 
   await request.cache().helpers.transaction.set(transaction)
+  await request.cache().helpers.page.set(page)
+  await request.cache().helpers.status.set(status)
+  await request.cache().helpers.addressLookup.set(addressLookup)
+
+  const lastStatusPermission = status.permissions[status.permissions.length - 1]
+  await request.cache().helpers.status.setCurrentPermission(lastStatusPermission)
 }
