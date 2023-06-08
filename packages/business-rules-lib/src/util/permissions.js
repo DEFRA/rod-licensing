cce462a19dad6579f62a93de54981187e5ac406f
ScottDormand96 <sdormand@hotmail.com>
2023-06-08 13:50:44 +0100

    add tests to test result when has value of created date

--- packages/business-rules-lib/src/util/permissions.js
+++ packages/business-rules-lib/src/util/permissions.js
@@ -1,5 +1,9 @@
 export const getPermissionCost = (permission, createdDate) => {
+  console.log('createdDate', createdDate)
+  console.log('startDate', permission.startDate)
+  console.log('newDate', new Date().toISOString())
   const permissionStartDate = createdDate || permission.startDate || new Date().toISOString()
+  console.log('permissionStartDate', permissionStartDate)
   if (Date.parse(permissionStartDate) >= Date.parse(permission.permit.newCostStartDate)) {
     return permission.permit.newCost
   }
