0e11c96b624259bc520e4e95100ed3c8f2a63f60
Scott Dormand <61588381+ScottDormand96@users.noreply.github.com>
2023-06-16 09:38:38 +0100

    Allow to delete duplicate licence (#1736)
    * Allow to delete duplicate licence
    
    https://eaflood.atlassian.net/browse/IWTF-3416
    
    Error when duplicate licences in basket
    
    * remove one duplicate if duplciates exists, added tests, refactor tests so transactionPermissions dont have value
    
    * mock hasDuplicates in test
    
    * mock value hasDuplicates as false

--- packages/gafl-webapp-service/src/pages/multibuy/remove-licence/__tests__/update-transaction.spec.js
+++ packages/gafl-webapp-service/src/pages/multibuy/remove-licence/__tests__/update-transaction.spec.js
@@ -1,5 +1,10 @@
 import { ADD_LICENCE, REMOVE_LICENCE } from '../../../../uri.js'
+import { hasDuplicates } from '../../../../processors/multibuy-processor.js'
 import updateTransaction from '../update-transaction.js'
+
+jest.mock('../../../../processors/multibuy-processor.js', () => ({
+  hasDuplicates: jest.fn(() => false)
+}))
 
 const createMockRequest = ({
   addressLookupPermissions = [],
@@ -61,8 +66,12 @@ describe('update transaction', () => {
     expect(newAddressLookupItems.permissions[1]).toStrictEqual(addressLookup3)
   })
 
-  it('transaction is removed', async () => {
-    const transactionPermissions = [{ hash: 'abc-999' }, { hash: 'aaa-111' }, { hash: 'zzz-010' }]
+  it('transaction is removed - no duplicates', async () => {
+    const transactionPermissions = [
+      { hash: 'abc-999', length: '12M' },
+      { hash: 'aaa-111', length: '1D' },
+      { hash: 'zzz-010', length: '8D' }
+    ]
     const [permission1, permission2, permission3] = transactionPermissions
     const setTransaction = jest.fn()
     const mockRequest = createMockRequest({
@@ -77,6 +86,36 @@ describe('update transaction', () => {
     expect(newTransactionPermissions.permissions.length).toBe(2)
     expect(newTransactionPermissions.permissions[0]).toStrictEqual(permission1)
     expect(newTransactionPermissions.permissions[1]).toStrictEqual(permission3)
+  })
+
+  const getTransactionPermission = length => ({
+    licenceHolder: 'Test Person',
+    start: '2021-01-01',
+    type: 'salmon-and-sea',
+    length,
+    hash: 'abc-999'
+  })
+
+  describe('duplicate licences', () => {
+    it.each`
+      desc                                                                                                           | transactionPermissions                                                                                                                                                 | result
+      ${'two identical permissions'}                                                                                 | ${[getTransactionPermission('12M'), getTransactionPermission('12M')]}                                                                                                  | ${[getTransactionPermission('12M')]}
+      ${'several permissions, all different apart from two identical permissions that arent consecutively placed'}   | ${[getTransactionPermission('12M'), getTransactionPermission('1D'), getTransactionPermission('12M')]}                                                                  | ${[getTransactionPermission('1D'), getTransactionPermission('12M')]}
+      ${'several permissions, all different apart from three identical permissions that arent consecutively placed'} | ${[getTransactionPermission('12M'), getTransactionPermission('1D'), getTransactionPermission('12M'), getTransactionPermission('8D'), getTransactionPermission('12M')]} | ${[getTransactionPermission('1D'), getTransactionPermission('12M'), getTransactionPermission('8D'), getTransactionPermission('12M')]}
+    `('only remove one duplicate when $desc', async ({ transactionPermissions, result }) => {
+      const setTransaction = jest.fn()
+      const mockRequest = createMockRequest({
+        transactionPermissions,
+        setTransaction,
+        currentTransactionPermission: getTransactionPermission('12M')
+      })
+
+      hasDuplicates.mockReturnValue(true)
+      await updateTransaction(mockRequest)
+
+      const newTransactionPermissions = setTransaction.mock.calls[0][0]
+      expect(newTransactionPermissions.permissions).toStrictEqual(result)
+    })
   })
 
   it('page is removed', async () => {
@@ -122,7 +161,11 @@ describe('update transaction', () => {
   })
 
   it('status current permission is set to the latest permission', async () => {
-    const transactionPermissions = [{ hash: 'abc-999' }, { hash: 'aaa-111' }, { hash: 'zzz-010' }]
+    const transactionPermissions = [
+      { hash: 'abc-999', length: '12M' },
+      { hash: 'aaa-111', length: '1D' },
+      { hash: 'zzz-010', length: '8D' }
+    ]
     const statusPermissions = [
       { [ADD_LICENCE.page]: true },
       { [REMOVE_LICENCE.page]: true, [ADD_LICENCE.page]: true },
@@ -143,7 +186,11 @@ describe('update transaction', () => {
   })
 
   it('status sets current permission Idx to the length of permissions in status', async () => {
-    const transactionPermissions = [{ hash: 'abc-999' }, { hash: 'aaa-111' }, { hash: 'zzz-010' }]
+    const transactionPermissions = [
+      { hash: 'abc-999', length: '12M' },
+      { hash: 'aaa-111', length: '1D' },
+      { hash: 'zzz-010', length: '8D' }
+    ]
     const statusPermissions = [
       { [ADD_LICENCE.page]: true },
       { [REMOVE_LICENCE.page]: true, [ADD_LICENCE.page]: true },
@@ -162,13 +209,23 @@ describe('update transaction', () => {
     expect(currentPermissionIdx).toBe(statusPermissions.length - 1)
   })
 
+  it('hasDuplicates to be called with transaction.permissions', async () => {
+    const transactionPermissions = [{ hash: 'abc-999', length: '12M' }]
+    const setTransaction = jest.fn()
+    const mockRequest = createMockRequest({
+      transactionPermissions,
+      setTransaction
+    })
+
+    await updateTransaction(mockRequest)
+    expect(hasDuplicates).toHaveBeenCalledWith(transactionPermissions)
+  })
+
   describe('all licences removed', () => {
     it('current permission of the status is not updated to the latest current permission', async () => {
-      const currentTransactionPermission = { hash: 'abc-123' }
-      const transactionPermissions = [currentTransactionPermission]
+      const transactionPermissions = []
       const setCurrentStatus = jest.fn()
       const mockRequest = createMockRequest({
-        currentTransactionPermission,
         transactionPermissions,
         setCurrentStatus
       })
@@ -179,13 +236,11 @@ describe('update transaction', () => {
     })
 
     it('current permission of the status is not updated with updated currentPermissionIdx', async () => {
-      const currentTransactionPermission = { hash: 'abc-123' }
-      const transactionPermissions = [currentTransactionPermission]
+      const transactionPermissions = []
       const statusPermissions = [{ [REMOVE_LICENCE.page]: true, [ADD_LICENCE.page]: true }]
       const setStatus = jest.fn()
 
       const mockRequest = createMockRequest({
-        currentTransactionPermission,
         transactionPermissions,
         statusPermissions,
         setStatus
