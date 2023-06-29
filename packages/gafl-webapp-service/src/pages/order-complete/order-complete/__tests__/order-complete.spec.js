5ddfdfeb067ab87f727f9267b8dc7f089a2269e8
Phil Benson <phil.benson@defra.gov.uk>
2023-06-29 16:09:43 +0100

    Change order confirmation page for multibuy (#1739)
    * Change order confirmation page for multibuy
    
    https://eaflood.atlassian.net/browse/IWTF-3480
    
    Implement content changes to order confirmation screen for multibuy
    journey
    
    * Refactor to remove logic from template in favour of putting it in the getData function
    * Reworked licence panel
    * Implemented multibuy-compatible order complete page
    * Updated link to annual fisheries report

--- packages/gafl-webapp-service/src/pages/order-complete/order-complete/__tests__/order-complete.spec.js
+++ packages/gafl-webapp-service/src/pages/order-complete/order-complete/__tests__/order-complete.spec.js
@@ -1,4 +1,4 @@
-import { LICENCE_DETAILS, NEW_TRANSACTION, ORDER_COMPLETE } from '../../../../uri.js'
+import { LICENCE_DETAILS, ORDER_COMPLETE } from '../../../../uri.js'
 import { addLanguageCodeToUri } from '../../../../processors/uri-helper.js'
 import { getData } from '../route.js'
 import { COMPLETION_STATUS, FEEDBACK_URI_DEFAULT } from '../../../../constants.js'
@@ -9,7 +9,8 @@ const getMockRequest = ({
   statusGetOverrides = {},
   statusSetCurrentPermission = () => {},
   statusSet = () => {},
-  transaction = getMockTransaction()
+  transaction = getMockTransaction(),
+  catalog = getMockCatalog()
 } = {}) => ({
   cache: () => ({
     helpers: {
@@ -32,19 +33,119 @@ const getMockRequest = ({
     search: ''
   },
   i18n: {
-    getCatalog: () => 'messages'
+    getCatalog: () => catalog
   }
 })
 
-const getMockTransaction = (permissions = [], cost = 0) => ({
+const getMockTransaction = ({ permissions = [], cost = 0 } = {}) => ({
   permissions,
   cost
+})
+
+const getMockCatalog = (overrides = {}) => ({
+  order_complete_title_application: 'order_complete_title_application',
+  order_complete_title_payment: 'order_complete_title_payment',
+  order_complete_panel_text_free_prefix: 'order_complete_panel_text_free_prefix',
+  order_complete_panel_text_prefix: 'order_complete_panel_text_prefix',
+  order_complete_panel_text_join: 'order_complete_panel_text_join',
+  order_complete_panel_text_single_licence: 'order_complete_panel_text_single_licence',
+  order_complete_panel_text_multiple_licences: 'order_complete_panel_text_multiple_licences',
+  order_complete_view_details_postal: 'order_complete_view_details_postal',
+  order_complete_view_details_digital: 'order_complete_view_details_digital',
+  pound: '#',
+  ...overrides
+})
+
+const getPostalPermission = () => ({
+  licensee: {
+    postalFulfilment: true,
+    preferredMethodOfConfirmation: 'Prefer not to be contacted'
+  }
+})
+
+const getDigitalPermission = () => ({
+  licensee: {
+    postalFulfilment: false
+  }
 })
 
 jest.mock('@defra-fish/connectors-lib')
 
 describe('The order completion handler', () => {
   beforeEach(jest.clearAllMocks)
+
+  it.each([
+    ['digital', [getDigitalPermission(), getDigitalPermission()]],
+    ['digital and postal', [getDigitalPermission(), getPostalPermission()]]
+  ])('When %s licences are purchased, viewDetailsParagraphs should have digital paragraph', async (_d, permissions) => {
+    const viewDetailsDigital = Symbol('order_complete_view_details_digital')
+    const request = getMockRequest({
+      transaction: getMockTransaction({ permissions }),
+      catalog: getMockCatalog({
+        order_complete_view_details_digital: viewDetailsDigital
+      })
+    })
+    const data = await getData(request)
+    expect(data.viewDetailsParagraphs[0]).toBe(viewDetailsDigital)
+  })
+
+  it.each([
+    ['postal', [getPostalPermission(), getPostalPermission()]],
+    ['digital and postal', [getDigitalPermission(), getPostalPermission()]]
+  ])('When %s licences are purchased, viewDetailsParagraphs should have postal paragraph', async (_d, permissions) => {
+    const viewDetailsPostal = Symbol('order_complete_view_details_postal')
+    const request = getMockRequest({
+      transaction: getMockTransaction({ permissions }),
+      catalog: getMockCatalog({
+        order_complete_view_details_postal: viewDetailsPostal
+      })
+    })
+    const data = await getData(request)
+    expect(data.viewDetailsParagraphs[data.viewDetailsParagraphs.length - 1]).toBe(viewDetailsPostal)
+  })
+
+  it.each([
+    ['digital', 'order_complete_when_fishing_digital', [getDigitalPermission(), getDigitalPermission()]],
+    ['digital and postal', 'order_complete_when_fishing_mixed', [getDigitalPermission(), getPostalPermission()]]
+  ])('When %s licences are purchased, whenFishingParagraph is set to %s', async (_d, label, permissions) => {
+    const whenFishingLabel = Symbol(label)
+    const request = getMockRequest({
+      transaction: getMockTransaction({ permissions }),
+      catalog: getMockCatalog({
+        [label]: whenFishingLabel
+      })
+    })
+    const data = await getData(request)
+    expect(data.whenFishingParagraph).toBe(whenFishingLabel)
+  })
+
+  it('When postal licences are purchased, whenFishingParagraph is set to expected value', async () => {
+    const orderCompleteWhenFishingPostal = 'when fishing postal text'
+    const orderCompleteWhenFishingPostalLink = 'when fishing postal link'
+    const request = getMockRequest({
+      transaction: getMockTransaction({ permissions: [getPostalPermission(), getPostalPermission()] }),
+      catalog: getMockCatalog({
+        order_complete_when_fishing_postal: orderCompleteWhenFishingPostal,
+        order_complete_when_fishing_postal_link: orderCompleteWhenFishingPostalLink
+      })
+    })
+    const data = await getData(request)
+    expect(data.whenFishingParagraph).toMatch(
+      new RegExp(`${orderCompleteWhenFishingPostal}<a href="(.*)">${orderCompleteWhenFishingPostalLink}</a>`)
+    )
+  })
+
+  it('When postal licences are purchased, whenFishingParagraph contains link to LICENCE_DETAILS', async () => {
+    const request = getMockRequest({
+      transaction: getMockTransaction({ permissions: [getPostalPermission(), getPostalPermission()] }),
+      catalog: getMockCatalog({
+        order_complete_when_fishing_postal: 'when fishing postal text',
+        order_complete_when_fishing_postal_link: 'when fishing postal link'
+      })
+    })
+    const data = await getData(request)
+    expect(data.whenFishingParagraph).toMatch(new RegExp(`.*<a href="${LICENCE_DETAILS.uri}">(.*)</a>`))
+  })
 
   it.each([COMPLETION_STATUS.agreed, COMPLETION_STATUS.posted, COMPLETION_STATUS.finalised])(
     'throws Boom.forbidden error when %s is not set',
@@ -80,20 +181,19 @@ describe('The order completion handler', () => {
     )
   })
 
-  it.each([[LICENCE_DETAILS.uri], [NEW_TRANSACTION.uri]])('addLanguageCodeToUri is called with request and %s', async uri => {
+  it(`addLanguageCodeToUri is called with request and ${LICENCE_DETAILS.uri}`, async () => {
     const request = getMockRequest()
-
     await getData(request)
-    expect(addLanguageCodeToUri).toHaveBeenCalledWith(request, uri)
+    expect(addLanguageCodeToUri).toHaveBeenCalledWith(request, LICENCE_DETAILS.uri)
   })
 
-  it.each(['new', 'licenceDetails'])('data outputs addLanguageCodeToUri decorated value for %s uri', async uriName => {
-    const decoratedUri = Symbol(uriName)
+  it('data outputs addLanguageCodeToUri decorated value for licence details uri', async () => {
+    const decoratedUri = Symbol('licenceDetails')
     addLanguageCodeToUri.mockReturnValue(decoratedUri)
     const request = getMockRequest()
 
     const data = await getData(request)
-    expect(data.uri[uriName]).toEqual(decoratedUri)
+    expect(data.uri.licenceDetails).toEqual(decoratedUri)
   })
 
   it.each(['http://trustpilot.com', 'http://give-us-a-stinker'])('feedback link set to FEEDBACK_URI env var (%s)', async feedbackUri => {
@@ -112,23 +212,11 @@ describe('The order completion handler', () => {
     expect(data.uri.feedback).toBe(FEEDBACK_URI_DEFAULT)
   })
 
-  it.each([
-    [1, ['foo']],
-    [2, ['foo', 'bar']]
-  ])('checks the numberOfLicences correctly when the number of licences is %s', async (count, permissions) => {
-    const transaction = getMockTransaction(permissions)
-    const request = getMockRequest({ transaction })
-
-    const data = await getData(request)
-    expect(data.numberOfLicences).toEqual(count)
-  })
-
-  it('returns the correct totalCost', async () => {
-    const transaction = getMockTransaction([], 100)
-    const request = getMockRequest({ transaction })
-
-    const data = await getData(request)
-    expect(data.totalCost).toEqual(100)
+  it('sets byelaws, catch return and annual report urls', async () => {
+    const {
+      uri: { byelaws, salmonAndSeaTrout, annualReport }
+    } = await getData(getMockRequest())
+    expect({ byelaws, salmonAndSeaTrout, annualReport }).toMatchSnapshot()
   })
 
   it.each([
@@ -138,12 +226,50 @@ describe('The order completion handler', () => {
   ])('returns the correct displayCatchReturnInfo when %s', async (_desc, expected, licenceTypes) => {
     const permissions = []
     for (const licenceType of licenceTypes) {
-      permissions.push({ licenceType })
+      permissions.push({ ...getDigitalPermission(), licenceType })
     }
-    const transaction = getMockTransaction(permissions)
+    const transaction = getMockTransaction({ permissions })
     const request = getMockRequest({ transaction })
 
     const data = await getData(request)
     expect(data.displayCatchReturnInfo).toEqual(expected)
+  })
+
+  it.each([
+    ['order_complete_title_application', 0, 'zero cost title'],
+    ['order_complete_title_payment', 9.99, 'paid for title']
+  ])('sets page title to %s if cost is %d', async (titleKey, cost, expectedTitle) => {
+    const request = getMockRequest({
+      transaction: getMockTransaction({ cost }),
+      catalog: getMockCatalog({
+        [titleKey]: expectedTitle
+      })
+    })
+    const data = await getData(request)
+    expect(data.title).toBe(expectedTitle)
+  })
+
+  it.each([
+    ['You\'ve shelled out <span class="govuk-!-font-weight-bold">#9.99</span> and that got you 2 angling permits', 9.99, 2],
+    ['You\'ve shelled out <span class="govuk-!-font-weight-bold">#23.46</span> and that got you 5 angling permits', 23.46, 5],
+    ['You\'ve shelled out <span class="govuk-!-font-weight-bold">#98.74</span> and that got you 9 angling permits', 98.74, 9],
+    ['You\'ve shelled out <span class="govuk-!-font-weight-bold">#12.50</span> and that got you 2 angling permits', 12.5, 2],
+    ['You\'ve shelled out <span class="govuk-!-font-weight-bold">#23.00</span> and that got you 5 angling permits', 23, 5],
+    ['You\'ve shelled out <span class="govuk-!-font-weight-bold">#59.80</span> and that got you 1 angling permit', 59.8, 1],
+    ["You've paid nowt for 3 angling permits", 0, 3],
+    ["You've paid nowt for 1 angling permit", 0, 1]
+  ])("sets licencePanelText to '%s' when price is %d and number of licences is %i", async (expectedText, cost, numberOfLicences) => {
+    const request = getMockRequest({
+      transaction: getMockTransaction({ permissions: Array(numberOfLicences).fill(getDigitalPermission()), cost }),
+      catalog: getMockCatalog({
+        order_complete_panel_text_free_prefix: "You've paid nowt for ",
+        order_complete_panel_text_prefix: "You've shelled out ",
+        order_complete_panel_text_join: ' and that got you ',
+        order_complete_panel_text_single_licence: ' angling permit',
+        order_complete_panel_text_multiple_licences: ' angling permits'
+      })
+    })
+    const data = await getData(request)
+    expect(data.licencePanelHTML).toBe(expectedText)
   })
 })
--- packages/gafl-webapp-service/src/pages/order-complete/order-complete/route.js
+++ packages/gafl-webapp-service/src/pages/order-complete/order-complete/route.js
@@ -1,8 +1,7 @@
 import pageRoute from '../../../routes/page-route.js'
-
 import Boom from '@hapi/boom'
 import { COMPLETION_STATUS, FEEDBACK_URI_DEFAULT } from '../../../constants.js'
-import { ORDER_COMPLETE, NEW_TRANSACTION, LICENCE_DETAILS } from '../../../uri.js'
+import { ORDER_COMPLETE, LICENCE_DETAILS, LOCAL_BYELAWS, CATCH_RETURN, ANNUAL_REPORT } from '../../../uri.js'
 import * as mappings from '../../../processors/mapping-constants.js'
 import { nextPage } from '../../../routes/next-page.js'
 import { addLanguageCodeToUri } from '../../../processors/uri-helper.js'
@@ -11,9 +10,41 @@ const checkForSalmonPermits = transaction => {
   return transaction.permissions.some(p => p.licenceType === mappings.LICENCE_TYPE['salmon-and-sea-trout'])
 }
 
+const getLicencePanelHTML = (price, numberOfLicences, mssgs) => {
+  const suffix = numberOfLicences === 1 ? mssgs.order_complete_panel_text_single_licence : mssgs.order_complete_panel_text_multiple_licences
+  if (price > 0) {
+    return `${mssgs.order_complete_panel_text_prefix}<span class="govuk-!-font-weight-bold">${mssgs.pound}${price.toFixed(2)}</span>${
+      mssgs.order_complete_panel_text_join
+    }${numberOfLicences}${suffix}`
+  }
+  return `${mssgs.order_complete_panel_text_free_prefix}${numberOfLicences}${suffix}`
+}
+
+const getViewDetailParagraphs = (licenceFulfilmentDigital, licenceFulfilmentPostal, mssgs) => {
+  const paragraphs = []
+  if (licenceFulfilmentDigital) {
+    paragraphs.push(mssgs.order_complete_view_details_digital)
+  }
+  if (licenceFulfilmentPostal) {
+    paragraphs.push(mssgs.order_complete_view_details_postal)
+  }
+  return paragraphs
+}
+
+const getWhenFishingParagraph = (licenceFulfilmentDigital, licenceFulfilmentPostal, mssgs) => {
+  if (licenceFulfilmentDigital && licenceFulfilmentPostal) {
+    return mssgs.order_complete_when_fishing_mixed
+  }
+  if (licenceFulfilmentDigital) {
+    return mssgs.order_complete_when_fishing_digital
+  }
+  return `${mssgs.order_complete_when_fishing_postal}<a href="${LICENCE_DETAILS.uri}">${mssgs.order_complete_when_fishing_postal_link}</a>`
+}
+
 export const getData = async request => {
   const status = await request.cache().helpers.status.get()
   const transaction = await request.cache().helpers.transaction.get()
+  const mssgs = request.i18n.getCatalog()
 
   // If the agreed flag is not set to true then throw an exception
   if (!status[COMPLETION_STATUS.agreed]) {
@@ -32,16 +63,27 @@ export const getData = async request => {
 
   await request.cache().helpers.status.set({ [COMPLETION_STATUS.completed]: true })
   await request.cache().helpers.status.setCurrentPermission({ currentPage: ORDER_COMPLETE.page })
+  const title = transaction.cost > 0 ? mssgs.order_complete_title_payment : mssgs.order_complete_title_application
+  const numberOfLicences = transaction.permissions.length
+  const licenceFulfilmentPostal = transaction.permissions.some(
+    ({ licensee }) => licensee.postalFulfilment && licensee.preferredMethodOfConfirmation === 'Prefer not to be contacted'
+  )
+  const licenceFulfilmentDigital = transaction.permissions.some(({ licensee }) => !licensee.postalFulfilment)
 
   return {
-    howContacted: mappings.HOW_CONTACTED,
-    totalCost: transaction.cost,
-    numberOfLicences: transaction.permissions.length,
     displayCatchReturnInfo: checkForSalmonPermits(transaction),
+    title,
+    titleHTML: transaction.cost > 0 ? `${mssgs.pound}${transaction.cost}<br />${title}` : title,
+    licencePanelHTML: getLicencePanelHTML(transaction.cost, numberOfLicences, mssgs),
+    viewDetailsParagraphs: getViewDetailParagraphs(licenceFulfilmentDigital, licenceFulfilmentPostal, mssgs),
+    whenFishingParagraph: getWhenFishingParagraph(licenceFulfilmentDigital, licenceFulfilmentPostal, mssgs),
+
     uri: {
       feedback: process.env.FEEDBACK_URI || FEEDBACK_URI_DEFAULT,
       licenceDetails: addLanguageCodeToUri(request, LICENCE_DETAILS.uri),
-      new: addLanguageCodeToUri(request, NEW_TRANSACTION.uri)
+      byelaws: LOCAL_BYELAWS.uri,
+      salmonAndSeaTrout: CATCH_RETURN.uri,
+      annualReport: ANNUAL_REPORT.uri
     }
   }
 }
