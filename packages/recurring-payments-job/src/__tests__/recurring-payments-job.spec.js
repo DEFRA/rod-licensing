840ffeb4ec387c38b49fc6bd2d3fcc841595aa50
ScottDormand96 <sdormand@hotmail.com>
2023-08-24 10:11:30 +0100

    setup docker so picks up commands run tomprove running

--- packages/recurring-payments-job/src/__tests__/recurring-payments-job.spec.js
+++ packages/recurring-payments-job/src/__tests__/recurring-payments-job.spec.js
@@ -1,7 +1,52 @@
-import recurringPaymentsJob from '../recurring-payments-job.js'
+import { execute } from '../recurring-payments-processor.js'
+import commander from 'commander'
 
-it('console log outputs recurring payments job is running', async () => {
-  const consoleLogSpy = jest.spyOn(console, 'log').mockImplementation(jest.fn())
-  recurringPaymentsJob()
-  expect(consoleLogSpy).toHaveBeenCalledWith('rp job running')
+jest.mock('commander', () => {
+  if (!global.commander) {
+    global.commander = jest.requireActual('commander')
+    global.commander.args = ['test']
+    global.commander.parse = jest.fn()
+    global.commander.outputHelp = jest.fn()
+    global.commander.help = jest.fn()
+  }
+  return global.commander
+})
+
+jest.mock('../recurring-payments-processor.js', () => {
+  if (!global.rpProcessor) {
+    global.rpProcessor = jest.fn()
+  }
+  return { execute: global.rpProcessor }
+})
+
+describe('recurring-payments-job', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('exposes an execute command to the cli', async () => {
+    jest.isolateModules(() => {
+      require('../recurring-payments-job.js')
+      expect(commander.commands[0].name()).toBe('execute')
+      commander.commands[0]._actionHandler([])
+      expect(execute).toHaveBeenCalled()
+    })
+  })
+
+  it('uses a wildcard match to output help when command not found', async () => {
+    jest.isolateModules(() => {
+      require('../recurring-payments-job.js')
+      expect(commander.commands[1].name()).toBe('*')
+      commander.commands[1]._actionHandler(['testxmlfile.xml'])
+      expect(commander.help).toHaveBeenCalled()
+    })
+  })
+
+  it('outputs help if no arguments are present', async () => {
+    jest.isolateModules(() => {
+      commander.args = []
+      require('../recurring-payments-job.js')
+      expect(commander.outputHelp).toHaveBeenCalled()
+    })
+  })
 })
