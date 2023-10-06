4eb09477377bc96e6b7234140e157ae42a2ea7dd
irisfaraway <iris.faraway@gmail.com>
2023-10-06 15:21:06 +0100

    Remove check on Welsh language feature flag

--- packages/gafl-webapp-service/src/processors/__tests__/page-language-helper.spec.js
+++ packages/gafl-webapp-service/src/processors/__tests__/page-language-helper.spec.js
@@ -2,24 +2,14 @@ import { welshEnabledAndApplied } from '../page-language-helper.js'
 
 describe('welshEnabledAndApplied', () => {
   describe('welshEnabledAndApplied', () => {
-    it('returns false when SHOW_WELSH_CONTENT is not true', async () => {
-      process.env.SHOW_WELSH_CONTENT = false
-      const request = { query: { lang: 'cy' } }
-
-      const result = welshEnabledAndApplied(request)
-      expect(result).toEqual(false)
-    })
-
-    it('returns false when SHOW_WELSH_CONTENT is true but the lang is not set to cy', async () => {
-      process.env.SHOW_WELSH_CONTENT = true
+    it('returns false when the lang is not set to cy', async () => {
       const request = { query: { lang: 'en' } }
 
       const result = welshEnabledAndApplied(request)
       expect(result).toEqual(false)
     })
 
-    it('returns true when SHOW_WELSH_CONTENT is true and the lang is set to cy', async () => {
-      process.env.SHOW_WELSH_CONTENT = true
+    it('returns true when the lang is set to cy', async () => {
       const request = { query: { lang: 'cy' } }
 
       const result = welshEnabledAndApplied(request)
--- packages/gafl-webapp-service/src/processors/page-language-helper.js
+++ packages/gafl-webapp-service/src/processors/page-language-helper.js
@@ -1,4 +1,3 @@
 export const welshEnabledAndApplied = request => {
-  const showWelshContent = process.env.SHOW_WELSH_CONTENT?.toLowerCase() === 'true'
-  return showWelshContent && request.query.lang === 'cy'
+  return request.query.lang === 'cy'
 }
