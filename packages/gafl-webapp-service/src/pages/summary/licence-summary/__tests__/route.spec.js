3b9865a26614124ea9e29503752858b73d61dfa9
ScottDormand96 <sdormand@hotmail.com>
2023-11-16 13:00:44 +0000

    remove it.only from test

--- packages/gafl-webapp-service/src/pages/summary/licence-summary/__tests__/route.spec.js
+++ packages/gafl-webapp-service/src/pages/summary/licence-summary/__tests__/route.spec.js
@@ -302,7 +302,7 @@ describe('licence-summary > route', () => {
       expect(transactionCacheSet).not.toHaveBeenCalled()
     })
 
-    it.only('addLanguageCodeToUri is called with request and NEW_TRANSACTION.uri', async () => {
+    it('addLanguageCodeToUri is called with request and NEW_TRANSACTION.uri', async () => {
       const mockRequest = getMockRequest()
       await getData(mockRequest)
       expect(addLanguageCodeToUri).toHaveBeenCalledWith(mockRequest, NEW_TRANSACTION.uri)
