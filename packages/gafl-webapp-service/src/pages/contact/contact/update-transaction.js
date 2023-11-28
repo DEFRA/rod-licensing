67e1dd4f423661497af03c829743df10aacd1013
ScottDormand96 <sdormand@hotmail.com>
2023-11-28 14:26:22 +0000

    add to use licensee instead paylaod if payload is undefined

--- packages/gafl-webapp-service/src/pages/contact/contact/update-transaction.js
+++ packages/gafl-webapp-service/src/pages/contact/contact/update-transaction.js
@@ -6,26 +6,29 @@ export default async request => {
   const { payload } = await request.cache().helpers.page.getCurrentPermission(CONTACT.page)
   const permission = await request.cache().helpers.transaction.getCurrentPermission()
 
+  console.log(permission)
+
   permission.licensee = {
     preferredMethodOfNewsletter: HOW_CONTACTED.none,
     ...permission.licensee,
-    ...(isPhysical(permission) ? getPhysicalReminders(payload) : getDigitalConfirmationsAndReminders(permission.licensee, payload))
+    ...(isPhysical(permission) ? getPhysicalReminders(permission.licensee, payload) : getDigitalConfirmationsAndReminders(permission.licensee, payload))
   }
 
   await request.cache().helpers.transaction.setCurrentPermission(permission)
 }
 
-const getPhysicalReminders = payload => {
+const getPhysicalReminders = (licensee, payload) => {
+  console.log('HIT')
   switch (payload['how-contacted']) {
     case 'email':
       return {
-        email: payload.email,
+        email: payload.email ? payload.email : licensee.email,
         preferredMethodOfReminder: HOW_CONTACTED.email
       }
     case 'text':
       return {
         preferredMethodOfReminder: HOW_CONTACTED.text,
-        mobilePhone: payload.text
+        mobilePhone: payload.text ? payload.text : licensee.mobilePhone
       }
     default:
       return {
@@ -41,7 +44,7 @@ const getDigitalConfirmationsAndReminders = (licensee, payload) => {
         postalFulfilment: false,
         preferredMethodOfConfirmation: HOW_CONTACTED.email,
         preferredMethodOfReminder: HOW_CONTACTED.email,
-        email: payload.email,
+        email: payload.email ? payload.email : licensee.email,
         mobilePhone: null
       }
     case 'text':
@@ -49,7 +52,7 @@ const getDigitalConfirmationsAndReminders = (licensee, payload) => {
         postalFulfilment: false,
         preferredMethodOfConfirmation: HOW_CONTACTED.text,
         preferredMethodOfReminder: HOW_CONTACTED.text,
-        mobilePhone: payload.text,
+        mobilePhone: payload.text ? payload.text : licensee.mobilePhone,
         email: licensee.preferredMethodOfNewsletter === HOW_CONTACTED.email ? licensee.email : null
       }
     default:
