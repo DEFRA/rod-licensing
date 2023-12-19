51355c5296ec5ee9dcacb180ddd7ef713552150a
Phil Benson <phil.benson@defra.gov.uk>
2023-12-19 16:31:00 +0000

    Add tests for terms and conditions get data, also add gafl matchers to package.json

--- packages/gafl-webapp-service/src/pages/terms-and-conditions/__tests__/route.spec.js
+++ packages/gafl-webapp-service/src/pages/terms-and-conditions/__tests__/route.spec.js
@@ -1,16 +1,74 @@
-import { getData, validator } from '../route'
-import pageRoute from '../../../routes/page-route.js'
-import { nextPage } from '../../../routes/next-page.js'
+import { getData } from '../route'
+import { CONTACT_SUMMARY, LICENCE_SUMMARY } from '../../../uri.js'
+import { LICENCE_TYPE } from '../../../processors/mapping-constants.js'
 
-jest.mock('../../../routes/next-page.js', () => ({
-  nextPage: jest.fn()
+jest.mock('../../../routes/next-page.js', () => ({ nextPage: () => {} }))
+jest.mock('../../../routes/page-route.js', () => () => {})
+jest.mock('../../../uri.js', () => ({
+  TERMS_AND_CONDITIONS: { page: 'terms-and-conditions', uri: '/terms-and-conditions' },
+  CONTACT_SUMMARY: { page: 'contact-summary', uri: '/contact-summary' },
+  LICENCE_SUMMARY: { page: 'licence-summary', uri: '/licence-summary' }
 }))
-jest.mock('../../../routes/page-route.js')
+jest.mock('../../../processors/mapping-constants.js', () => ({
+  LICENCE_TYPE: {
+    'salmon-and-sea-trout': 'salmon_and_sea_trout'
+  }
+}))
 
-describe('terms-and-conditions > route', () => {
-  describe('default', () => {
-    it('should call the pageRoute with terms-and-conditions, /buy/conditions, validator, nextPage and getData', async () => {
-      expect(pageRoute).toBeCalledWith('terms-and-conditions', '/buy/conditions', validator, nextPage, getData)
+describe('terms-and-conditions get data', () => {
+  const getMockRequest = ({
+    licenceSummary = true,
+    contactSummary = true,
+    licenceType = 'old-wellies-and-shopping-trollies',
+    cost = 1.5
+  } = {}) => ({
+    cache: () => ({
+      helpers: {
+        status: {
+          getCurrentPermission: async () => ({
+            [LICENCE_SUMMARY.page]: licenceSummary,
+            [CONTACT_SUMMARY.page]: contactSummary
+          })
+        },
+        transaction: {
+          getCurrentPermission: async () => ({
+            licenceType,
+            permit: {
+              cost
+            }
+          })
+        }
+      }
     })
+  })
+  it('salmon and sea trout flag is true when licence is a salmon and sea trout one', async () => {
+    const request = getMockRequest({ licenceType: LICENCE_TYPE['salmon-and-sea-trout'] })
+    const { isSalmonAndSeaTrout } = await getData(request)
+    expect(isSalmonAndSeaTrout).toBeTruthy()
+  })
+
+  it("salmon and sea trout flag is false when licence is't a salmon and sea trout one", async () => {
+    const { isSalmonAndSeaTrout } = await getData(getMockRequest())
+    expect(isSalmonAndSeaTrout).toBeFalsy()
+  })
+
+  it('payment required flag is true when licence has a cost', async () => {
+    const { paymentRequired } = await getData(getMockRequest())
+    expect(paymentRequired).toBeTruthy()
+  })
+
+  it('payment required flag is false when licence is free', async () => {
+    const { paymentRequired } = await getData(getMockRequest({ cost: 0 }))
+    expect(paymentRequired).toBeFalsy()
+  })
+
+  it("throws a GetDataRedirect to licence summary when status licence summary isn't present", () => {
+    const request = getMockRequest({ licenceSummary: false })
+    expect(() => getData(request)).rejects.toThrowRedirectTo(LICENCE_SUMMARY.uri)
+  })
+
+  it("throws a GetDataRedirect to contact summary when contact summary isn't present", () => {
+    const request = getMockRequest({ contactSummary: false })
+    expect(() => getData(request)).rejects.toThrowRedirectTo(CONTACT_SUMMARY.uri)
   })
 })
