d2b52d88d50dc26d5912f1bf0c68e8fb6a13f211
Phil Benson <phil.benson@defra.gov.uk>
2023-12-20 10:58:52 +0000

    Fix code smells (#1843)
    * Fix code smells
    
    https://eaflood.atlassian.net/browse/IWTF-3893
    
    - test coverage in dynamics lib, sales api, POCL and GAFL
    - simplify page handler in GAFL
    - code style issues

---
+++ packages/gafl-webapp-service/src/pages/contact/digital-licence/licence-fulfilment/__tests__/result-function.spec.js
@@ -0,0 +1,34 @@
+import resultFunction from '../result-function.js'
+import constants from '../../../../../constants.js'
+
+jest.mock('../../../../../constants', () => ({
+  CONTACT_SUMMARY_SEEN: 'contact-summary-seen',
+  CommonResults: {
+    SUMMARY: 'summary-common-result',
+    OK: 'ok-common-result'
+  }
+}))
+
+describe('licence summary result function', () => {
+  const getMockRequest = (fromSummary = constants.CONTACT_SUMMARY_SEEN) => ({
+    cache: () => ({
+      helpers: {
+        status: {
+          getCurrentPermission: async () => ({
+            fromSummary
+          })
+        }
+      }
+    })
+  })
+
+  it('returns summary result if contact summary is requested', async () => {
+    const result = await resultFunction(getMockRequest())
+    expect(result).toBe(constants.CommonResults.SUMMARY)
+  })
+
+  it("returns ok result if contact summary isn't requested", async () => {
+    const result = await resultFunction(getMockRequest('not-contact-summary'))
+    expect(result).toBe(constants.CommonResults.OK)
+  })
+})
