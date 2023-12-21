7aad1ae33bb8d77afd26dae28e46cd37893eaa29
ScottDormand96 <sdormand@hotmail.com>
2023-12-21 09:32:53 +0000

    welsh translations, added missing bulletpoint

--- packages/gafl-webapp-service/src/processors/recurring-pay-reminder-display.js
+++ packages/gafl-webapp-service/src/processors/recurring-pay-reminder-display.js
@@ -2,9 +2,9 @@ import { HOW_CONTACTED } from '../processors/mapping-constants.js'
 
 export const recurringPayReminderDisplay = (permission, mssgs) => {
   if (permission.licensee.preferredMethodOfReminder === HOW_CONTACTED.email) {
-    return mssgs.recurring_payment_set_up_bulletpoint_4_email
+    return mssgs.recurring_payment_set_up_bulletpoint_5_email
   } else if (permission.licensee.preferredMethodOfReminder === HOW_CONTACTED.letter) {
-    return mssgs.recurring_payment_set_up_bulletpoint_4_letter
+    return mssgs.recurring_payment_set_up_bulletpoint_5_letter
   }
-  return mssgs.recurring_payment_set_up_bulletpoint_4_text
+  return mssgs.recurring_payment_set_up_bulletpoint_5_text
 }
