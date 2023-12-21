53d0aa936d1bdd48059df24f39be3acbed7be405
Scott Dormand <61588381+ScottDormand96@users.noreply.github.com>
2023-12-21 10:58:15 +0000

    Recurring payment agreement (#1844)
    * Recurring payment agreement
    
    https://eaflood.atlassian.net/browse/IWTF-3827
    
    recurring payment agreement page welsh translation
    
    * welsh translations, added missing bulletpoint

--- packages/gafl-webapp-service/src/processors/__tests__/recurring-pay-reminder-display.spec.js
+++ packages/gafl-webapp-service/src/processors/__tests__/recurring-pay-reminder-display.spec.js
@@ -1,9 +1,9 @@
 import { recurringPayReminderDisplay } from '../recurring-pay-reminder-display.js'
 
 const getCatalog = () => ({
-  recurring_payment_set_up_bulletpoint_4_email: 'we will send you an email showing the cost before the next payment is taken',
-  recurring_payment_set_up_bulletpoint_4_letter: 'we will send you a letter showing the cost before the next payment is taken',
-  recurring_payment_set_up_bulletpoint_4_text: 'we will send you a text showing the cost before the next payment is taken'
+  recurring_payment_set_up_bulletpoint_5_email: 'we will send you an email showing the cost before the next payment is taken',
+  recurring_payment_set_up_bulletpoint_5_letter: 'we will send you a letter showing the cost before the next payment is taken',
+  recurring_payment_set_up_bulletpoint_5_text: 'we will send you a text message showing the cost before the next payment is taken'
 })
 
 const getPermission = reminder => ({
@@ -16,7 +16,7 @@ describe('recurringPayReminderDisplay', () => {
   it.each([
     ['Email', 'we will send you an email showing the cost before the next payment is taken'],
     ['Letter', 'we will send you a letter showing the cost before the next payment is taken'],
-    ['Text', 'we will send you a text showing the cost before the next payment is taken']
+    ['Text', 'we will send you a text message showing the cost before the next payment is taken']
   ])('when reminder is %s, recurringPayReminderDisplay will return "%s"', (reminder, expected) => {
     const permission = getPermission(reminder)
     const result = recurringPayReminderDisplay(permission, getCatalog())
