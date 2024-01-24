0193bf88aaac3be2329335fbc2a6f51671ecca29
ScottDormand96 <sdormand@hotmail.com>
2024-01-24 13:44:26 +0000

    add recurring payments job back

---
+++ packages/recurring-payments-job/src/recurring-payments-processor.js
@@ -0,0 +1,12 @@
+import { salesApi } from '@defra-fish/connectors-lib'
+
+export const processRecurringPayments = async () => {
+  if (process.env.RUN_RECURRING_PAYMENTS?.toLowerCase() === 'true') {
+    console.log('Recurring Payments job enabled')
+    const date = new Date().toISOString().split('T')[0]
+    const response = await salesApi.getDueRecurringPayments(date)
+    console.log('Recurring Payments found: ', response)
+  } else {
+    console.log('Recurring Payments job disabled')
+  }
+}
