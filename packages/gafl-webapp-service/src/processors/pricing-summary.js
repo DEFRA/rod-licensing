6b519ac9cfbcc5838226197dd5ffd4cb7edbbe1f
ScottDormand96 <sdormand@hotmail.com>
2024-03-11 16:28:12 +0000

    display on 12th

--- packages/gafl-webapp-service/src/processors/pricing-summary.js
+++ packages/gafl-webapp-service/src/processors/pricing-summary.js
@@ -73,8 +73,8 @@ export const shouldDisplayPriceChangePaymentWarningMessage = concessions => {
   if (concessions.includes('Junior')) {
     return false
   }
-  const startRange = moment('2024-03-10T23:59:00Z')
-  const endRange = moment('2024-03-12T00:01:00Z')
+  const startRange = moment('2024-03-11T23:59:00Z')
+  const endRange = moment('2024-03-13T00:01:00Z')
 
   return now.isBetween(startRange, endRange, null, '[]')
 }
