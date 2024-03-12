78dbf58956d2d3a500090c12b097cc0949257921
irisfaraway <iris.faraway@gmail.com>
2024-03-12 17:11:48 +0000

    Build request content

--- packages/gafl-webapp-service/src/services/call-recording/__tests__/call-recording-service.spec.js
+++ packages/gafl-webapp-service/src/services/call-recording/__tests__/call-recording-service.spec.js
@@ -7,31 +7,50 @@ jest.mock('debug', () => jest.fn(() => jest.fn()))
 const { value: debug } = db.mock.results[db.mock.calls.findIndex(c => c[0] === 'webapp:call-recording-service')]
 
 describe('call-recording-service', () => {
+  beforeAll(() => {
+    process.env.TELESALES_ENDPOINT = 'endpoint'
+    process.env.TELESALES_AUTH_USERNAME = 'username'
+    process.env.TELESALES_AUTH_PASSWORD = 'password'
+  })
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
   describe('pauseRecording', () => {
     it('logs that it is requesting to pause the recording', async () => {
-      const agent = 'foo'
-      await pauseRecording(agent)
-      expect(debug).toHaveBeenCalledWith('Sending pause recording request to Storm for agent: %s', agent)
+      const agentEmail = 'agent@example.com'
+      await pauseRecording(agentEmail)
+      expect(debug).toHaveBeenCalledWith('Sending pause recording request to Storm for agentEmail: %s', agentEmail)
     })
 
     it('sends a soap request', async () => {
-      await pauseRecording('agent')
-      expect(soapRequest).toHaveBeenCalledWith({ url: 'foo', headers: 'bar', xml: 'pause' })
+      await pauseRecording('agent@example.com')
+      const expectedArgs = { url: 'endpoint', headers: { 'Content-Type': 'text/xml;charset=UTF-8' } }
+      expect(soapRequest).toHaveBeenCalledWith(expect.objectContaining(expectedArgs))
+    })
+
+    it('sends a soap request with the correct xml', async () => {
+      await pauseRecording('agent@example.com')
+      expect(soapRequest.mock.calls[0][0].xml).toMatchSnapshot()
     })
   })
-})
 
-describe('call-recording-service', () => {
   describe('resumeRecording', () => {
     it('logs that it is requesting to resume the recording', async () => {
-      const agent = 'foo'
-      await resumeRecording(agent)
-      expect(debug).toHaveBeenCalledWith('Sending resume recording request to Storm for agent: %s', agent)
+      const agentEmail = 'agent@example.com'
+      await resumeRecording(agentEmail)
+      expect(debug).toHaveBeenCalledWith('Sending resume recording request to Storm for agentEmail: %s', agentEmail)
     })
 
-    it('sends a soap request', async () => {
-      await resumeRecording('agent')
-      expect(soapRequest).toHaveBeenCalledWith({ url: 'foo', headers: 'bar', xml: 'resume' })
+    it('sends a soap request with the correct headers and endpoint', async () => {
+      const expectedArgs = { url: 'endpoint', headers: { 'Content-Type': 'text/xml;charset=UTF-8' } }
+      await resumeRecording('agent@example.com')
+      expect(soapRequest).toHaveBeenCalledWith(expect.objectContaining(expectedArgs))
+    })
+
+    it('sends a soap request with the correct xml', async () => {
+      await resumeRecording('agent@example.com')
+      expect(soapRequest.mock.calls[0][0].xml).toMatchSnapshot()
     })
   })
 })
--- packages/gafl-webapp-service/src/services/call-recording/call-recording-service.js
+++ packages/gafl-webapp-service/src/services/call-recording/call-recording-service.js
@@ -1,13 +1,70 @@
 import soapRequest from 'easy-soap-request'
+import { XMLBuilder } from 'fast-xml-parser'
 import db from 'debug'
 const debug = db('webapp:call-recording-service')
 
-export const pauseRecording = async agent => {
-  debug('Sending pause recording request to Storm for agent: %s', agent)
-  await soapRequest({ url: 'foo', headers: 'bar', xml: 'pause' })
+export const pauseRecording = async agentEmail => {
+  debug('Sending pause recording request to Storm for agentEmail: %s', agentEmail)
+  sendRequestToTelesales('pause', agentEmail)
 }
 
-export const resumeRecording = async agent => {
-  debug('Sending resume recording request to Storm for agent: %s', agent)
-  await soapRequest({ url: 'foo', headers: 'bar', xml: 'resume' })
+export const resumeRecording = async agentEmail => {
+  debug('Sending resume recording request to Storm for agentEmail: %s', agentEmail)
+  sendRequestToTelesales('resume', agentEmail)
+}
+
+const sendRequestToTelesales = async (action, agentEmail) => {
+  const url = process.env.TELESALES_ENDPOINT
+  const headers = { 'Content-Type': 'text/xml;charset=UTF-8' }
+  const xml = await buildXml(action, agentEmail)
+  await soapRequest({ url, headers, xml })
+}
+
+const buildXml = async (action, agentEmail) => {
+  const authUsername = process.env.TELESALES_AUTH_USERNAME
+  const authPassword = process.env.TELESALES_AUTH_PASSWORD
+
+  const builder = new XMLBuilder({
+    attributesGroupName: '@',
+    format: true,
+    ignoreAttributes: false,
+    suppressBooleanAttributes: false
+  })
+  const xml = builder.build({
+    'soap:Envelope': {
+      '@': {
+        'xmlns:soap': 'http://www.w3.org/2003/05/soap-envelope',
+        'xmlns:cal': 'http://redcentrex.redwoodtech.com/calls'
+      },
+      'soap:Header': {
+        'wsse:Security': {
+          '@': {
+            'soap:mustUnderstand': 'true',
+            'xmlns:wsse': 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd',
+            'xmlns:wsu': 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd'
+          },
+          'wsse:UsernameToken': {
+            '@': {
+              'wsu:Id': 'UsernameToken-1'
+            },
+            'wsse:Username': authUsername,
+            'wsse:Password': {
+              '@': {
+                Type: 'http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText'
+              },
+              '#text': authPassword
+            }
+          }
+        }
+      },
+      'soap:Body': {
+        'cal:RecordingRequest': {
+          'cal:UserID': agentEmail,
+          'cal:Record': action,
+          'cal:Muted': '0'
+        }
+      }
+    }
+  })
+  return xml
 }
