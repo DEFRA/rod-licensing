f89252573ce9e3aba63671b4fdbe053f842e52e2
Scott Dormand <61588381+ScottDormand96@users.noreply.github.com>
2024-04-23 16:35:09 +0100

    RP job local failure (#1946)
    * RP Job local failures
    
    https://eaflood.atlassian.net/browse/IWTF-3840
    
    Set rp job to run after sales api as is dependent
    
    * add env variable, update read me, edit to change to function so can test env variable
    
    * changes to rp job
    
    * working and tests passing
    
    * add timers

--- packages/recurring-payments-job/src/recurring-payments-job.js
+++ packages/recurring-payments-job/src/recurring-payments-job.js
@@ -1,9 +1,18 @@
 'use strict'
-import { Command } from 'commander'
+import recurringPaymentsJob from 'commander'
 import { processRecurringPayments } from './recurring-payments-processor.js'
 
-const recurringPaymentsJob = new Command()
+const delay = parseInt(process.env.RECURRING_PAYMENTS_LOCAL_DELAY || '0', 10)
+if (delay > 0) {
+  setTimeout(() => {
+    executeRecurringPaymentsJob()
+  }, delay * 1000)
+} else {
+  executeRecurringPaymentsJob()
+}
 
-recurringPaymentsJob.action(processRecurringPayments())
+function executeRecurringPaymentsJob () {
+  recurringPaymentsJob.action(processRecurringPayments())
+}
 
 export default recurringPaymentsJob
