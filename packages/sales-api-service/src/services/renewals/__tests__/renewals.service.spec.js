f666ae8b835c8b48334d06b8dd4498558655cbcd
irisfaraway <iris.faraway@gmail.com>
2024-05-16 17:08:43 +0100

    Add date data processing

--- packages/sales-api-service/src/services/renewals/__tests__/renewals.service.spec.js
+++ packages/sales-api-service/src/services/renewals/__tests__/renewals.service.spec.js
@@ -1,4 +1,5 @@
-import { prepareContactMethodData } from '../renewals.service.js'
+import moment from 'moment'
+import { prepareContactMethodData, prepareDateData } from '../renewals.service.js'
 
 describe('prepareContactMethodData', () => {
   it('should copy the relevant data', async () => {
@@ -21,5 +22,41 @@ describe('prepareContactMethodData', () => {
       preferredMethodOfReminder: 'Letter'
     }
     expect(prepareContactMethodData(existingPermission)).toEqual(expectedData)
+  })
+})
+
+describe('prepareDateData', () => {
+  describe('when the original permission has expired', () => {
+    it('should process the data correctly', async () => {
+      const endDate = moment().subtract(5, 'days')
+      const existingPermission = {
+        endDate
+      }
+      const expectedData = {
+        renewedHasExpired: true,
+        licenceToStart: 'after-payment',
+        licenceStartDate: moment().format('YYYY-MM-DD'),
+        licenceStartTime: 0,
+        renewedEndDate: endDate.toISOString()
+      }
+      expect(prepareDateData(existingPermission)).toEqual(expectedData)
+    })
+  })
+
+  describe('when the original permission has not expired', () => {
+    it('should process the data correctly', async () => {
+      const endDate = moment().add(5, 'days')
+      const existingPermission = {
+        endDate
+      }
+      const expectedData = {
+        renewedHasExpired: false,
+        licenceToStart: 'another-date',
+        licenceStartDate: endDate.format('YYYY-MM-DD'),
+        licenceStartTime: endDate.hours(),
+        renewedEndDate: endDate.toISOString()
+      }
+      expect(prepareDateData(existingPermission)).toEqual(expectedData)
+    })
   })
 })
--- packages/sales-api-service/src/services/renewals/renewals.service.js
+++ packages/sales-api-service/src/services/renewals/renewals.service.js
@@ -1,8 +1,57 @@
+import { SERVICE_LOCAL_TIME } from '@defra-fish/business-rules-lib'
+import moment from 'moment-timezone'
+
+// Replicated from GAFL - need to decide whether to move
+const cacheDateFormat = 'YYYY-MM-DD'
+const licenceToStart = {
+  AFTER_PAYMENT: 'after-payment',
+  ANOTHER_DATE: 'another-date'
+}
+
 export const prepareContactMethodData = existingPermission => {
-  const licenseeData = {}
-  licenseeData.preferredMethodOfNewsletter = existingPermission.licensee.preferredMethodOfNewsletter.label
-  licenseeData.preferredMethodOfConfirmation = existingPermission.licensee.preferredMethodOfConfirmation.label
-  licenseeData.preferredMethodOfReminder = existingPermission.licensee.preferredMethodOfReminder.label
+  const licenseeData = {
+    preferredMethodOfNewsletter: existingPermission.licensee.preferredMethodOfNewsletter.label,
+    preferredMethodOfConfirmation: existingPermission.licensee.preferredMethodOfConfirmation.label,
+    preferredMethodOfReminder: existingPermission.licensee.preferredMethodOfReminder.label
+  }
 
   return licenseeData
+}
+
+export const prepareDateData = existingPermission => {
+  const endDateMoment = moment.utc(existingPermission.endDate).tz(SERVICE_LOCAL_TIME)
+  const renewedHasExpired = !endDateMoment.isAfter(moment().tz(SERVICE_LOCAL_TIME))
+
+  const dateData = renewedHasExpired
+    ? dateDataIfExpired(licenceToStart)
+    : dateDataIfNotExpired(licenceToStart, renewedHasExpired, endDateMoment)
+  dateData.renewedEndDate = endDateMoment.toISOString()
+
+  return dateData
+}
+
+const getLicenceStartDate = (renewedHasExpired, licenceEndDate) => {
+  if (renewedHasExpired) {
+    return moment().tz(SERVICE_LOCAL_TIME)
+  }
+  return moment(licenceEndDate).add(1, 'minute').seconds(0).tz(SERVICE_LOCAL_TIME)
+}
+
+const dateDataIfExpired = licenceToStart => {
+  return {
+    renewedHasExpired: true,
+    licenceToStart: licenceToStart.AFTER_PAYMENT,
+    licenceStartDate: moment().tz(SERVICE_LOCAL_TIME).format(cacheDateFormat),
+    licenceStartTime: 0
+  }
+}
+
+const dateDataIfNotExpired = (licenceToStart, renewedHasExpired, endDateMoment) => {
+  const licenceStartDate = getLicenceStartDate(renewedHasExpired, endDateMoment)
+  return {
+    renewedHasExpired: false,
+    licenceToStart: licenceToStart.ANOTHER_DATE,
+    licenceStartDate: licenceStartDate.format(cacheDateFormat),
+    licenceStartTime: licenceStartDate.hours()
+  }
 }
