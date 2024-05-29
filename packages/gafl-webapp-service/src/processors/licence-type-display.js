c31cb52069a885019d2ca5bf664973c90f350252
lailien3 <138867360+lailien3@users.noreply.github.com>
2024-05-29 11:48:27 +0100

    Update licence-type-display.js

--- packages/gafl-webapp-service/src/processors/licence-type-display.js
+++ packages/gafl-webapp-service/src/processors/licence-type-display.js
@@ -27,11 +27,13 @@ export const licenceTypeDisplay = (permission, mssgs) => {
 
 export const licenceTypeAndLengthDisplay = (permission, mssgs) => {
   const licenceTypeMessage =
-    permission.licenceLength === '12M' ? mssgs.licence_type_12m :
-    permission.licenceLength === '8D' ? mssgs.licence_type_8d :
-    mssgs.licence_type_1d;
-  return `${licenceTypeDisplay(permission, mssgs)}, ${licenceTypeMessage}`;
-};
+    permission.licenceLength === '12M'
+      ? mssgs.licence_type_12m
+      : permission.licenceLength === '8D'
+        ? mssgs.licence_type_8d
+        : mssgs.licence_type_1d
+  return `${licenceTypeDisplay(permission, mssgs)}, ${licenceTypeMessage}`
+}
 
 export const isPhysical = permission => permission?.permit?.isForFulfilment
 
