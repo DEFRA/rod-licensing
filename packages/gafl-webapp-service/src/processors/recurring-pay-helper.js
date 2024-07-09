2b3de0b9b786beada68eb5cf15c7bb3e0a56a27e
lailien3 <138867360+lailien3@users.noreply.github.com>
2024-07-09 10:34:52 +0100

    Replaced magic number with constant (SonarCloud suggesstion)

--- packages/gafl-webapp-service/src/processors/recurring-pay-helper.js
+++ packages/gafl-webapp-service/src/processors/recurring-pay-helper.js
@@ -1,5 +1,7 @@
 import { HOW_CONTACTED } from './mapping-constants.js'
 
+const min_age_for_recurring_payment = 18;
+
 export const recurringPayReminderDisplay = (permission, mssgs) => {
   if (permission.licensee.preferredMethodOfReminder === HOW_CONTACTED.email) {
     return mssgs.recurring_payment_set_up_bulletpoint_5_email
@@ -13,5 +15,5 @@ export const validForRecurringPayment = permission =>
   process.env.SHOW_RECURRING_PAYMENTS?.toLowerCase() === 'true' &&
   permission.licenceLength === '12M' &&
   permission.isLicenceForYou &&
-  permission.licensee.age > 17 &&
+  permission.licensee.age >= min_age_for_recurring_payment &&
   process.env.CHANNEL?.toLowerCase() !== 'telesales'
