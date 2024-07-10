b76d517c9d01550642cdd6ea3e0feac549a9d479
lailien3 <138867360+lailien3@users.noreply.github.com>
2024-07-10 09:10:02 +0100

    RP job sends payment requests to GOV.UK Pay

--- packages/recurring-payments-job/src/recurring-payments-processor.js
+++ packages/recurring-payments-job/src/recurring-payments-processor.js
@@ -1,6 +1,6 @@
 import moment from 'moment-timezone'
-import { SERVICE_LOCAL_TIME } from '@defra-fish/business-rules-lib'
-import { salesApi } from '@defra-fish/connectors-lib'
+import { PAYMENT_TYPE, TRANSACTION_SOURCE } from '@defra-fish/business-rules-lib'
+import { salesApi, govUkPayApi } from '@defra-fish/connectors-lib'
 
 export const processRecurringPayments = async () => {
   if (process.env.RUN_RECURRING_PAYMENTS?.toLowerCase() === 'true') {
@@ -16,13 +16,16 @@ export const processRecurringPayments = async () => {
 
 const processRecurringPayment = async record => {
   const referenceNumber = record.expanded.activePermission.entity.referenceNumber
-  const transactionData = await processPermissionData(referenceNumber)
+  const transactionData = await processPermissionData(referenceNumber) // prepares transaction data ?
   console.log('Creating new transaction based on', referenceNumber)
   try {
-    const response = await salesApi.createTransaction(transactionData)
-    console.log('New transaction created:', response)
+    const transactionResponse = await salesApi.createTransaction(transactionData) // creates transaction ?
+    console.log('New transaction created:', transactionResponse)
+    const paymentResponse = await sendPaymentRequest(transactionResponse) // sends payment request ?
+    console.log('Payment request sent:', paymentResponse)
+    await processPaymentResults({ id: transactionResponse.id, paymentStatus: paymentResponse }) // processes payment results ?
   } catch (e) {
-    console.log('Error creating transaction', JSON.stringify(transactionData))
+    console.log('Error creating transaction or sending payment request', JSON.stringify(transactionData))
     throw e
   }
 }
@@ -40,7 +43,8 @@ const processPermissionData = async referenceNumber => {
         issueDate: null,
         licensee: licenseeWithoutCountryCode,
         permitId: data.permitId,
-        startDate: prepareStartDate(data)
+        startDate: prepareStartDate(data),
+        amount: data.amount
       }
     ]
   }
@@ -48,8 +52,58 @@ const processPermissionData = async referenceNumber => {
 
 const prepareStartDate = permission => {
   return moment
-    .tz(permission.licenceStartDate, 'YYYY-MM-DD', SERVICE_LOCAL_TIME)
+    .tz(permission.licenceStartDate, 'YYYY-MM-DD')
     .add(permission.licenceStartTime ?? 0, 'hours')
     .utc()
     .toISOString()
+}
+
+const sendPaymentRequest = async transaction => {
+  const paymentRequestData = {
+    amount: transaction.permissions[0].amount, // pulls amount from the transaction data
+    reference: transaction.id,
+    description: 'Renewal payment',
+    return_url: `${process.env.RETURN_URL}/payment-status`
+  }
+
+  const response = await govUkPayApi.createPayment(paymentRequestData) // sends payment request to govukpay API >
+  const paymentResponse = await response.json()
+  return paymentResponse
+}
+
+const processPaymentResults = async transaction => {
+  if (transaction.paymentStatus.state?.status === 'error') {
+    await salesApi.updatePaymentJournal(transaction.id, { paymentStatus: 'Failed' })
+  }
+
+  if (transaction.paymentStatus.state?.status === 'success') {
+    await salesApi.finaliseTransaction(transaction.id, {
+      payment: {
+        amount: transaction.paymentStatus.amount,
+        timestamp: transaction.paymentStatus.transactionTimestamp,
+        source: TRANSACTION_SOURCE.govPay,
+        method: PAYMENT_TYPE.debit
+      }
+    })
+    await salesApi.updatePaymentJournal(transaction.id, { paymentStatus: 'Completed' })
+  } else {
+    if (transaction.paymentStatus.state?.code === 'P0030') {
+      await salesApi.updatePaymentJournal(transaction.id, { paymentStatus: 'Expired' })
+    }
+
+    if (transaction.paymentStatus.state?.code === 'P0020') {
+      await salesApi.updatePaymentJournal(transaction.id, { paymentStatus: 'Cancelled' })
+    }
+
+    if (transaction.paymentStatus.state?.code === 'P0010') {
+      await salesApi.updatePaymentJournal(transaction.id, { paymentStatus: 'Failed' })
+    }
+
+    if (
+      transaction.paymentStatus.code === 'P0040' &&
+      moment().diff(moment(transaction.paymentTimestamp), 'hours') >= 3
+    ) {
+      await salesApi.updatePaymentJournal(transaction.id, { paymentStatus: 'Expired' })
+    }
+  }
 }
