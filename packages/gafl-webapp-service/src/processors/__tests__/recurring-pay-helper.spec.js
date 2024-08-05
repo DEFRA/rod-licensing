ecf9e810aa35406b5539fa425cab6e716e98241b
lailien3 <138867360+lailien3@users.noreply.github.com>
2024-08-05 15:02:09 +0100

    Hide RCP option for Junior licence purchases (#1989)
    * Hide RCP option for Junior licence purchases
    https://eaflood.atlassian.net/browse/IWTF-4066# Please enter the commit message for your changes. Lines starting
    
    * Created tests
    
    * Fixed 'age' attribute and fixed comma position
    
    * Random space from test removed
    
    * Changed age condition
    
    * Replaced magic number with constant (SonarCloud suggesstion)
    
    * Removed unnecessary test
    
    * Removed unnecessary test
    
    * Used template literals & removed hardcoded age
    
    * Removed hardcoded age, and ensured respective tests passed
    
    * Implemented template literal
    
    * Corrected age calculation, corrected describve statement's grammar, subbed in birthDate > age
    
    * Replaces age field with birthDate field, and amended tests
    
    * Prettier fixes

--- packages/gafl-webapp-service/src/processors/__tests__/recurring-pay-helper.spec.js
+++ packages/gafl-webapp-service/src/processors/__tests__/recurring-pay-helper.spec.js
@@ -6,9 +6,10 @@ const getCatalog = () => ({
   recurring_payment_set_up_bulletpoint_5_text: 'we will send you a text message showing the cost before the next payment is taken'
 })
 
-const getPermission = ({ reminder, licenceFor, length }) => ({
+const getPermission = ({ reminder, licenceFor, length, birthDate }) => ({
   licensee: {
-    preferredMethodOfReminder: reminder
+    preferredMethodOfReminder: reminder,
+    birthDate
   },
   isLicenceForYou: licenceFor,
   licenceLength: length
@@ -27,18 +28,20 @@ describe('recurringPayReminderDisplay', () => {
 })
 
 describe('validForRecurringPayment', () => {
-  it.each([
-    [true, '12M', true, true, 'not telesales'],
-    [false, '8D', true, true, 'not telesales'],
-    [false, '12M', false, true, 'not telesales'],
-    [false, '12M', true, false, 'not telesales'],
-    [false, '12M', true, true, 'telesales']
-  ])(
-    'should return %s as licence length is %s, licence for you is %s and SHOW_RECURRING_PAYMENTS is %s and journey is %s',
-    (expected, length, licenceFor, recurring, telesales) => {
+  test.each`
+    expected  | length   | licenceFor | recurring | telesales           | birthDate
+    ${true}   | ${'12M'} | ${true}    | ${true}   | ${'not telesales'}  | ${'1946-01-01'}
+    ${false}  | ${'8D'}  | ${true}    | ${true}   | ${'not telesales'}  | ${'1946-01-01'}
+    ${false}  | ${'12M'} | ${false}   | ${true}   | ${'not telesales'}  | ${'1946-01-01'}
+    ${false}  | ${'12M'} | ${true}    | ${false}  | ${'not telesales'}  | ${'1946-01-01'}
+    ${false}  | ${'12M'} | ${true}    | ${true}   | ${'telesales'}      | ${'1946-01-01'}
+    ${false}  | ${'12M'} | ${true}    | ${true}   | ${'not telesales'}  | ${'2010-01-01'}
+  `(
+    'should return %s as licence length is %s, licence for you is %s, SHOW_RECURRING_PAYMENTS is %s, journey is %s, and birthDate is %s',
+    ({ expected, length, licenceFor, recurring, telesales, birthDate }) => {
       process.env.CHANNEL = telesales
       process.env.SHOW_RECURRING_PAYMENTS = recurring
-      const permission = getPermission({ licenceFor, length })
+      const permission = getPermission({ licenceFor, length, birthDate })
       const result = validForRecurringPayment(permission)
       expect(result).toEqual(expected)
     }
