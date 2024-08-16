3b00dce30281896d3a30e70306ec634fb8b94531
Iris Faraway <iris.faraway@gmail.com>
2024-08-16 09:25:05 +0100

    Export date validations (#2021)
    https://eaflood.atlassian.net/browse/IWTF-3941

--- packages/business-rules-lib/src/index.js
+++ packages/business-rules-lib/src/index.js
@@ -1,4 +1,5 @@
 import * as contactValidation from './validators/contact.validators.js'
+import * as dateValidation from './validators/date.validators.js'
 import * as permissionValidation from './validators/permission.validators.js'
 export * from './util/ages.js'
 export * from './util/permissions.js'
@@ -6,5 +7,6 @@ export * from './constants.js'
 
 export const validation = {
   contact: contactValidation,
+  date: dateValidation,
   permission: permissionValidation
 }
