6e859718778383b7f846a10ddf8275c7a7fe83ae
lailien3 <138867360+lailien3@users.noreply.github.com>
2024-08-21 15:30:43 +0100

    Validate Recurring Payments Reques
    https://eaflood.atlassian.net/browse/IWTF-4057
    We need to validate the response for recurring payments. This schema for validating input is done in a similar way to renewals authentication.

---
+++ packages/sales-api-service/src/schema/recurring-payments.schema.js
@@ -0,0 +1,15 @@
+import Joi from 'joi'
+import { validation } from '@defra-fish/business-rules-lib'
+
+export const recurringPaymentsRequestParamsSchema = Joi.object({
+  date: validation.date.isoDate().description('The date for which recurring payments are being retrieved')
+}).label('recurring-payments-request-params')
+
+const recurringPaymentSchema = Joi.object({
+  id: Joi.string().required().description('The ID of the recurring payment'),
+  amount: Joi.number().required().description('The payment amount of the recurring payment'),
+  dueDate: validation.date.isoDate().description('The due date of the recurring payment'),
+  status: Joi.string().valid('pending', 'paid', 'failed').required().description('The status of the payment')
+}).label('recurring-payment')
+
+export const recurringPaymentsResponseSchema = Joi.array().items(recurringPaymentSchema).label('recurring-payments-response')
