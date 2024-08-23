b3d0ac3894b9031e590909dbc1d9155705d90c7d
lailien3 <138867360+lailien3@users.noreply.github.com>
2024-08-23 08:48:26 +0100

    Removed comments

--- packages/sales-api-service/src/schema/recurring-payments.schema.js
+++ packages/sales-api-service/src/schema/recurring-payments.schema.js
@@ -1,15 +1,10 @@
 import Joi from 'joi'
 import { validation } from '@defra-fish/business-rules-lib'
 
-export const recurringPaymentsRequestParamsSchema = Joi.object({
-  date: validation.date.isoDate().description('The date for which recurring payments are being retrieved')
+export const recurringPaymentsResponseSchema = Joi.object({
+  date: Joi.string()
+  .isoDate()
+  .required()
+  .description('The ISO8601 compatible date for which recurring payments are being retrieved')
+  .example(new Date().toISOString())
 }).label('recurring-payments-request-params')
-
-const recurringPaymentSchema = Joi.object({
-  id: Joi.string().required().description('The ID of the recurring payment'),
-  amount: Joi.number().required().description('The payment amount of the recurring payment'),
-  dueDate: validation.date.isoDate().description('The due date of the recurring payment'),
-  status: Joi.string().valid('pending', 'paid', 'failed').required().description('The status of the payment')
-}).label('recurring-payment')
-
-export const recurringPaymentsResponseSchema = Joi.array().items(recurringPaymentSchema).label('recurring-payments-response')
--- packages/sales-api-service/src/server/routes/recurring-payments.js
+++ packages/sales-api-service/src/server/routes/recurring-payments.js
@@ -1,6 +1,5 @@
 import { getRecurringPayments } from '../../services/recurring-payments.service.js'
-import { recurringPaymentsRequestParamsSchema, recurringPaymentsResponseSchema } from '../../schema/recurring-payments-schema.js'
-import Joi from 'joi'
+import { recurringPaymentsResponseSchema } from '../../schema/recurring-payments-schema.js'
 
 export default [
   {
@@ -11,18 +10,13 @@ export default [
         const { date } = request.params
         const result = await getRecurringPayments(date)
 
-        // const { error, value } = recurringPaymentsResponseSchema.validate(result)
-        // if (error) {
-        //   throw new Joi.ValidationError('Response validation failed', error.details, value)
-        // }
-
         return h.response(result)
       },
       description: 'Retrieve recurring payments due on provided date',
       notes: 'Returns the list of recurring payments that are due on the provided date',
       tags: ['api', 'recurring-payments'],
       validate: {
-        params: recurringPaymentsRequestParamsSchema
+        params: recurringPaymentsResponseSchema
       },
       plugins: {
         'hapi-swagger': {
@@ -30,7 +24,7 @@ export default [
             200: { description: 'List of due recurring payments', schema: recurringPaymentsResponseSchema },
             400: { description: 'Invalid date format' }
           },
-          order: 2
+          order: 1
         }
       }
     }
