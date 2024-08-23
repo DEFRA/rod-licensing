9f51a5654ebaf703c5295360055a9f6b0687c39b
lailien3 <138867360+lailien3@users.noreply.github.com>
2024-08-23 08:50:59 +0100

    Remove unused validator

--- packages/sales-api-service/src/schema/recurring-payments.schema.js
+++ packages/sales-api-service/src/schema/recurring-payments.schema.js
@@ -1,10 +1,9 @@
 import Joi from 'joi'
-import { validation } from '@defra-fish/business-rules-lib'
 
 export const recurringPaymentsResponseSchema = Joi.object({
   date: Joi.string()
-  .isoDate()
-  .required()
-  .description('The ISO8601 compatible date for which recurring payments are being retrieved')
-  .example(new Date().toISOString())
+    .isoDate()
+    .required()
+    .description('The ISO8601 compatible date for which recurring payments are being retrieved')
+    .example(new Date().toISOString())
 }).label('recurring-payments-request-params')
