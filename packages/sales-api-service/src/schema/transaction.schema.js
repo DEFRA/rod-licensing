37afcfbc9f3f0e59b5d6a3b3f722e084a7f12435
irisfaraway <iris.faraway@gmail.com>
2024-11-11 16:50:08 +0000

    Resolve linting issue

--- packages/sales-api-service/src/schema/transaction.schema.js
+++ packages/sales-api-service/src/schema/transaction.schema.js
@@ -7,6 +7,8 @@ import { MAX_PERMISSIONS_PER_TRANSACTION, POCL_TRANSACTION_SOURCES } from '@defr
 
 import { v4 as uuidv4 } from 'uuid'
 
+const AGREEMENT_ID_LENGTH = 26
+
 /**
  * Maximum number of items that can be created in a batch - limited by DynamoDB max batch size
  * @type {number}
@@ -34,7 +36,7 @@ const createTransactionRequestSchemaContent = {
   createdBy: Joi.string().optional(),
   journalId: Joi.string().optional(),
   transactionId: Joi.string().guid({ version: 'uuidv4' }),
-  agreementId: Joi.string().alphanum().length(26).optional()
+  agreementId: Joi.string().alphanum().length(AGREEMENT_ID_LENGTH).optional()
 }
 
 /**
