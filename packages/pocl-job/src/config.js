42f1d84a783f1e0de59dfee97c7fadb8e62aeacf
Scott Dormand <61588381+ScottDormand96@users.noreply.github.com>
2024-11-25 07:44:18 +0000

    Remove ftp functionality (#2064)
    * Remove ftp functionality
    
    https://eaflood.atlassian.net/browse/IWTF-4278
    
    Remove ftp functionality as package we use in fulfilment and pocl jobs (ssh2-sftp-client) has critical vulnerability
    
    * remove ssh2 sftp client
    
    * remove ssh2 sftp client and fix lint
    
    * remove reference
    
    * fix tests
    
    * fix tests and lint
    
    * update tests
    
    * refactor
    
    * refactor
    
    * update tests
    
    * refactor tests
    
    * s3spec
    
    * undefined token
    
    * rename file and undo removal of stores3
    
    * rename file and undo removal of stores3
    
    * add missing mock
    
    * Remove ssh2 mock
    
    * remove functionality from config
    
    * remove ftp reference
    
    ---------

--- packages/pocl-job/src/config.js
+++ packages/pocl-job/src/config.js
@@ -1,49 +1,5 @@
-import { AWS } from '@defra-fish/connectors-lib'
-import db from 'debug'
-const { secretsManager } = AWS()
-
-/**
- * Key exchange algorithms for public key authentication - in descending order of priority
- * @type {string[]}
- */
-export const SFTP_KEY_EXCHANGE_ALGORITHMS = [
-  'curve25519-sha256@libssh.org',
-  'curve25519-sha256',
-  'ecdh-sha2-nistp521',
-  'ecdh-sha2-nistp384',
-  'ecdh-sha2-nistp256',
-  'diffie-hellman-group-exchange-sha256',
-  'diffie-hellman-group14-sha256',
-  'diffie-hellman-group16-sha512',
-  'diffie-hellman-group18-sha512',
-  'diffie-hellman-group14-sha1',
-  'diffie-hellman-group-exchange-sha1',
-  'diffie-hellman-group1-sha1'
-]
-/**
- * Ciphers for SFTP support - in descending order of priority
- * @type {string[]}
- */
-export const SFTP_CIPHERS = [
-  // http://tools.ietf.org/html/rfc4344#section-4
-  'aes256-ctr',
-  'aes192-ctr',
-  'aes128-ctr',
-  'aes256-gcm',
-  'aes256-gcm@openssh.com',
-  'aes128-gcm',
-  'aes128-gcm@openssh.com',
-  'aes256-cbc',
-  'aes192-cbc',
-  'aes128-cbc',
-  'blowfish-cbc',
-  '3des-cbc',
-  'cast128-cbc'
-]
-
 class Config {
   _db
-  _ftp
   _s3
 
   async initialise () {
@@ -51,21 +7,6 @@ class Config {
       fileStagingTable: process.env.POCL_FILE_STAGING_TABLE,
       recordStagingTable: process.env.POCL_RECORD_STAGING_TABLE,
       stagingTtlDelta: Number.parseInt(process.env.POCL_STAGING_TTL || 60 * 60 * 168)
-    }
-
-    this.ftp = {
-      host: process.env.POCL_FTP_HOST,
-      port: process.env.POCL_FTP_PORT || '22',
-      path: process.env.POCL_FTP_PATH,
-      username: process.env.POCL_FTP_USERNAME,
-      privateKey: (await secretsManager.getSecretValue({ SecretId: process.env.POCL_FTP_KEY_SECRET_ID }).promise()).SecretString,
-      algorithms: { cipher: SFTP_CIPHERS, kex: SFTP_KEY_EXCHANGE_ALGORITHMS },
-      // Wait up to 60 seconds for the SSH handshake
-      readyTimeout: 60000,
-      // Retry 5 times over a minute
-      retries: 5,
-      retry_minTimeout: 12000,
-      debug: db('pocl:ftp')
     }
 
     this.s3 = {
@@ -83,18 +24,6 @@ class Config {
 
   set db (cfg) {
     this._db = cfg
-  }
-
-  /**
-   * FTP configuration settings
-   * @type {object}
-   */
-  get ftp () {
-    return this._ftp
-  }
-
-  set ftp (cfg) {
-    this._ftp = cfg
   }
 
   /**
