ff3e0baab1ad3a7e6a855426295b1150712e9c83
lailien3 <138867360+lailien3@users.noreply.github.com>
2024-12-02 08:40:44 +0000

    accidentally messed with wrong file

--- packages/sales-api-service/src/services/transactions/process-transaction-queue.js
+++ packages/sales-api-service/src/services/transactions/process-transaction-queue.js
@@ -107,10 +107,7 @@ export async function processQueue ({ id }) {
   await docClient.send(
     new PutCommand({
       TableName: TRANSACTION_STAGING_HISTORY_TABLE.TableName,
-      Item: {
-        ...transactionRecord,
-        expires: Math.floor(Date.now() / 1000) + TRANSACTION_STAGING_HISTORY_TABLE.Ttl
-      },
+      Item: Object.assign(transactionRecord, { expires: Math.floor(Date.now() / 1000) + TRANSACTION_STAGING_HISTORY_TABLE.Ttl }),
       ConditionExpression: 'attribute_not_exists(id)'
     })
   )
@@ -167,9 +164,8 @@ const createTransactionEntities = async transactionRecord => {
   transaction.channelId = transactionRecord.channelId
 
   transaction.bindToEntity(Transaction.definition.relationships.transactionCurrency, currency)
-  if (transactionRecord.transactionFile) {
+  transactionRecord.transactionFile &&
     transaction.bindToAlternateKey(Transaction.definition.relationships.poclFile, transactionRecord.transactionFile)
-  }
 
   const chargeJournal = await createTransactionJournal(transactionRecord, transaction, 'Charge', currency)
   const paymentJournal = await createTransactionJournal(transactionRecord, transaction, 'Payment', currency)
