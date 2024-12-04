1b195b2a1dc881cfae7aac3f5fbeb607a79327c9
Scott Dormand <scottdormand@Scotts-MacBook-Pro.local>
2024-12-04 11:23:59 +0000

    redundant code

--- packages/dynamics-lib/src/queries/__tests__/permission.queries.spec.js
+++ packages/dynamics-lib/src/queries/__tests__/permission.queries.spec.js
@@ -1,23 +1,6 @@
-import { permissionForLicensee, permissionForFullReferenceNumber, permissionForContacts } from '../permission.queries.js'
+import { permissionForFullReferenceNumber, permissionForContacts } from '../permission.queries.js'
 
 describe('Permission Queries', () => {
-  describe('permissionForLicensee', () => {
-    it('builds a filter to run a query for a permission with a given licensee date of birth and postcode', async () => {
-      const query = permissionForLicensee('ABC123', '2000-01-01', 'AB12 3CD')
-      expect(query.toRetrieveRequest()).toEqual({
-        collection: 'defra_permissions',
-        expand: expect.arrayContaining([
-          expect.objectContaining({ property: 'defra_ContactId' }),
-          expect.objectContaining({ property: 'defra_PermitId' }),
-          expect.objectContaining({ property: 'defra_defra_permission_defra_concessionproof_PermissionId' })
-        ]),
-        filter:
-          "endswith(defra_name, 'ABC123') and defra_ContactId/defra_postcode eq 'AB12 3CD' and defra_ContactId/birthdate eq 2000-01-01 and statecode eq 0",
-        select: expect.any(Array)
-      })
-    })
-  })
-
   describe('permissionForFullReferenceNumber', () => {
     it('builds a filter to run a query for a permission with a full reference number', async () => {
       const query = permissionForFullReferenceNumber('ABC123')
--- packages/dynamics-lib/src/queries/permission.queries.js
+++ packages/dynamics-lib/src/queries/permission.queries.js
@@ -3,29 +3,6 @@ import { Permission } from '../entities/permission.entity.js'
 import { escapeODataStringValue } from '../client/util.js'
 import { ConcessionProof } from '../entities/concession-proof.entity.js'
 
-/**
- * Builds a query to retrieve a permission and related entities for a given reference number and related contact information
- *
- * @param permissionReferenceNumber the reference number of the permission used to perform the lookup
- * @param licenseeBirthDate the birth date of the contact associated with the permission
- * @param licenseePostcode the postcode of the contact associated with the permission
- * @returns {PredefinedQuery}
- */
-export const permissionForLicensee = (permissionReferenceNumber, licenseeBirthDate, licenseePostcode) => {
-  const { licensee, permit, concessionProofs } = Permission.definition.relationships
-  let filter = `endswith(${Permission.definition.mappings.referenceNumber.field}, '${escapeODataStringValue(permissionReferenceNumber)}')`
-  filter += ` and ${licensee.property}/${licensee.entity.definition.mappings.postcode.field} eq '${escapeODataStringValue(
-    licenseePostcode
-  )}'`
-  filter += ` and ${licensee.property}/${licensee.entity.definition.mappings.birthDate.field} eq ${licenseeBirthDate}`
-  filter += ` and ${Permission.definition.defaultFilter}`
-  return new PredefinedQuery({
-    root: Permission,
-    filter: filter,
-    expand: [licensee, permit, concessionProofs]
-  })
-}
-
 export const permissionForFullReferenceNumber = permissionReferenceNumber => {
   const { licensee, permit, concessionProofs } = Permission.definition.relationships
   let filter = `${Permission.definition.mappings.referenceNumber.field} eq '${escapeODataStringValue(permissionReferenceNumber)}'`
