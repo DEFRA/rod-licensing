5b9b38fb43d36e383f7fa045253e14bca402de03
irisfaraway <iris.faraway@gmail.com>
2025-01-10 16:12:18 +0000

    Remove try block for unawaited call

--- packages/recurring-payments-job/src/services/__tests__/govuk-pay-service.spec.js
+++ packages/recurring-payments-job/src/services/__tests__/govuk-pay-service.spec.js
@@ -4,8 +4,6 @@ import { govUkPayApi } from '@defra-fish/connectors-lib'
 jest.mock('@defra-fish/connectors-lib')
 
 describe('govuk-pay-service', () => {
-  const preparedPayment = { id: '1234' }
-
   describe('sendPayment', () => {
     it('should send provided payload data to Gov.UK Pay', async () => {
       govUkPayApi.createPayment.mockResolvedValue({
@@ -20,31 +18,6 @@ describe('govuk-pay-service', () => {
       }
       await sendPayment(payload)
       expect(govUkPayApi.createPayment).toHaveBeenCalledWith(payload, true)
-    })
-
-    it('should throw an error when the GOV.UK Pay connector raises an error', async () => {
-      govUkPayApi.createPayment.mockImplementationOnce(() => {
-        throw new Error('Oops!')
-      })
-
-      try {
-        await sendPayment(preparedPayment)
-      } catch (e) {
-        expect(e.message).toBe('Oops!')
-      }
-    })
-
-    it('should log error message when the GOV.UK Pay API raises an error', async () => {
-      const consoleSpy = jest.spyOn(console, 'error').mockImplementation(() => {})
-      govUkPayApi.createPayment.mockImplementationOnce(() => {
-        throw new Error()
-      })
-
-      try {
-        await sendPayment(preparedPayment)
-      } catch (error) {
-        expect(consoleSpy).toHaveBeenCalledWith('Error creating payment', preparedPayment.id)
-      }
     })
   })
 })
--- packages/recurring-payments-job/src/services/govuk-pay-service.js
+++ packages/recurring-payments-job/src/services/govuk-pay-service.js
@@ -1,10 +1,5 @@
 import { govUkPayApi } from '@defra-fish/connectors-lib'
 
 export const sendPayment = async preparedPayment => {
-  try {
-    govUkPayApi.createPayment(preparedPayment, true)
-  } catch (e) {
-    console.error('Error creating payment', preparedPayment.id)
-    throw e
-  }
+  govUkPayApi.createPayment(preparedPayment, true)
 }
