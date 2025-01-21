6d97332f696f84a3f0a9448d06831a70743d3dca
Scott Dormand <scottdormand@Scotts-MacBook-Pro.local>
2025-01-21 07:56:37 +0000

    find payment

--- packages/recurring-payments-job/src/recurring-payments-processor.js
+++ packages/recurring-payments-job/src/recurring-payments-processor.js
@@ -104,9 +104,8 @@ const processRecurringPaymentStatus = async record => {
   const referenceNumber = record.expanded.activePermission.entity.referenceNumber
   const paymentId = getPaymentId(referenceNumber)
   console.log('Checking payment status for', paymentId)
-  getPaymentStatus(paymentId).then(status => {
-    console.log(`Payment status for ${paymentId}: ${status}`)
-  })
+  const status = await getPaymentStatus(paymentId)
+  console.log(`Payment status for ${paymentId}: ${status}`)
 }
 
 const getPaymentId = referenceNumber => {
