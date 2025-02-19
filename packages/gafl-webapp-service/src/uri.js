e0e7b2348fd4b8e6380025679bd4fd10ca4ba8d3
laila aleissa <138867360+lailien3@users.noreply.github.com>
2025-02-19 12:21:37 +0000

    create new uri and page

--- packages/gafl-webapp-service/src/uri.js
+++ packages/gafl-webapp-service/src/uri.js
@@ -48,6 +48,7 @@ export const IDENTIFY = { uri: '/buy/renew/identify', page: 'identify' }
 export const RENEWAL_INACTIVE = { uri: '/buy/renew/inactive', page: 'renewal-inactive' }
 export const AUTHENTICATE = { uri: '/buy/renew/authenticate' }
 export const RENEWAL_START_DATE = { uri: '/buy/renew/renewal-start-date', page: 'renewal-start-date' }
+export const RENEWAL_NO_MATCH = { uri: '/buy/renew/licence-not-found', page: 'licence-not-found' }
 
 export const CONTROLLER = { uri: '/buy' }
 export const NEW_TRANSACTION = { uri: '/buy/new' }
