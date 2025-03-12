d191d3d941fbb786f9ea8d05e6751313b4129abf
Philip Benson <philbens@gmail.com>
2025-03-12 12:22:40 +0000

    Amended batcher to use true private variables rather than convention

--- packages/connectors-lib/src/http-request-batcher.js
+++ packages/connectors-lib/src/http-request-batcher.js
@@ -4,67 +4,72 @@ import { StatusCodes } from 'http-status-codes'
 
 const debug = db('connectors:http-request-batcher')
 export default class HTTPRequestBatcher {
+  #batchSize
+  #delay
+  #requests = []
+  #responses = []
+
   constructor ({ batchSize = 50, delay = 1000 } = {}) {
-    this._batchSize = batchSize
-    this._delay = delay
-    this._requests = []
-    this._responses = []
+    this.#batchSize = batchSize
+    this.#delay = delay
   }
 
   get batchSize () {
-    return this._batchSize
+    return this.#batchSize
   }
 
   get requestQueue () {
-    return this._requests
+    return this.#requests
   }
 
   get responses () {
-    return this._responses
+    return this.#responses
   }
 
   get delay () {
-    return this._delay
+    return this.#delay
   }
 
   addRequest (url, options) {
     if (!url) {
       throw new Error('URL is required')
     }
-    this._requests.push({ url, options })
+    this.#requests.push({ url, options })
   }
 
   async _sendBatch (fetchRequests, sentRequests, requestQueue) {
     const batchResponses = await Promise.all(fetchRequests)
-    this._responses.push(...batchResponses)
+    this.#responses.push(...batchResponses)
     for (let x = 0; x < batchResponses.length; x++) {
       const response = batchResponses[x]
       if (response.status === StatusCodes.TOO_MANY_REQUESTS && sentRequests[x].attempts < 2) {
         requestQueue.push({ ...sentRequests[x], attempts: sentRequests[x].attempts + 1 })
-        this._batchSize = Math.max(this._batchSize - 1, 1)
-        debug(`429 response received for ${sentRequests[x].url}, reducing batch size to ${this._batchSize}`)
+        this.#batchSize = Math.max(this.#batchSize - 1, 1)
+        debug(`429 response received for ${sentRequests[x].url}, reducing batch size to ${this.#batchSize}`)
       }
     }
     fetchRequests.length = 0
     sentRequests.length = 0
     if (requestQueue.length) {
       // don't wait if this is the last batch
-      await new Promise(resolve => setTimeout(resolve, this._delay))
+      await new Promise(resolve => setTimeout(resolve, this.#delay))
     }
   }
 
   async fetch () {
     debug(
-      `Beginning batched fetch of ${this._requests.length} requests with initial batch size of ${this._batchSize} and delay between batches of ${this._delay}ms`
+      `Beginning batched fetch of ${this.#requests.length} requests with initial batch size of ${
+        this.#batchSize
+      } and delay between batches of ${this.#delay}ms`
     )
-    const requestQueue = [...this._requests]
+    const requestQueue = [...this.#requests]
     const sentRequests = []
     const fetchRequests = []
     while (requestQueue.length) {
       const request = requestQueue.shift()
       fetchRequests.push(fetch(request.url, request.options))
       sentRequests.push({ attempts: 1, ...request })
-      if (fetchRequests.length === this._batchSize) {
+      if (fetchRequests.length === this.#batchSize) {
         await this._sendBatch(fetchRequests, sentRequests, requestQueue)
       }
     }
