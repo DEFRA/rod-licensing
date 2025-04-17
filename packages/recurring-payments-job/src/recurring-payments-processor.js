32fadc530f381f2baba5a83cea7d76d5e8d64b70
laila aleissa <138867360+lailien3@users.noreply.github.com>
2025-04-17 16:45:54 +0100

    reverted uneeded code

--- packages/recurring-payments-job/src/recurring-payments-processor.js
+++ packages/recurring-payments-job/src/recurring-payments-processor.js
@@ -5,6 +5,7 @@ import { getPaymentStatus, sendPayment } from './services/govuk-pay-service.js'
 
 const PAYMENT_STATUS_DELAY = 60000
 const payments = []
+const PAYMENT_STATUS_SUCCESS = 'success'
 
 export const processRecurringPayments = async () => {
   if (process.env.RUN_RECURRING_PAYMENTS?.toLowerCase() === 'true') {
@@ -30,13 +31,13 @@ export const processRecurringPayments = async () => {
 const processRecurringPayment = async record => {
   const referenceNumber = record.expanded.activePermission.entity.referenceNumber
   const agreementId = record.entity.agreementId
-  const transaction = await createNewTransaction(referenceNumber)
+  const transaction = await createNewTransaction(referenceNumber, agreementId)
   await takeRecurringPayment(agreementId, transaction)
 }
 
-const createNewTransaction = async referenceNumber => {
-  const transactionData = await processPermissionData(referenceNumber)
-  console.log('Creating new transaction based on', referenceNumber)
+const createNewTransaction = async (referenceNumber, agreementId) => {
+  const transactionData = await processPermissionData(referenceNumber, agreementId)
+  console.log('Creating new transaction based on', referenceNumber, 'with agreementId', agreementId)
   try {
     const response = await salesApi.createTransaction(transactionData)
     console.log('New transaction created:', response)
@@ -61,12 +62,13 @@ const takeRecurringPayment = async (agreementId, transaction) => {
   }
 }
 
-const processPermissionData = async referenceNumber => {
-  console.log('Preparing data based on', referenceNumber)
+const processPermissionData = async (referenceNumber, agreementId) => {
+  console.log('Preparing data based on', referenceNumber, 'with agreementId', agreementId)
   const data = await salesApi.preparePermissionDataForRenewal(referenceNumber)
   const licenseeWithoutCountryCode = Object.assign((({ countryCode: _countryCode, ...l }) => l)(data.licensee))
   return {
     dataSource: 'Recurring Payment',
+    agreementId,
     permissions: [
       {
         isLicenceForYou: data.isLicenceForYou,
@@ -106,17 +108,16 @@ const preparePayment = (agreementId, transaction) => {
 const processRecurringPaymentStatus = async record => {
   const agreementId = record.entity.agreementId
   const paymentId = getPaymentId(agreementId)
-  try {
-    const {
-      state: { status }
-    } = await getPaymentStatus(paymentId)
-    console.log(`Payment status for ${paymentId}: ${JSON.stringify(status)}`)
-  } catch (error) {
-    console.error('Error fetching payment status for payment:', paymentId, 'Error:', error)
+  const {
+    state: { status }
+  } = await getPaymentStatus(paymentId)
+  console.log(`Payment status for ${paymentId}: ${status}`)
+  if (status === PAYMENT_STATUS_SUCCESS) {
+    const payment = payments.find(p => p.paymentId === paymentId)
+    await salesApi.processRPResult(payment.transaction.id, paymentId, payment.created_date)
   }
 }
 
 const getPaymentId = agreementId => {
-  const payment = payments.find(p => p.agreementId === agreementId)
-  return payment.paymentId
+  return payments.find(p => p.agreementId === agreementId).paymentId
 }
