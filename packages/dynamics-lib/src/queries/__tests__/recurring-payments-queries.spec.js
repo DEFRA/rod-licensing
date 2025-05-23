6566b46a0c2e5e6ee827483cba7394dc8b60cac7
Scott <scott.dormand@defra.gov.uk>
2025-05-23 08:16:34 +0100

    set to check next rp is null

--- packages/dynamics-lib/src/queries/__tests__/recurring-payments-queries.spec.js
+++ packages/dynamics-lib/src/queries/__tests__/recurring-payments-queries.spec.js
@@ -9,7 +9,8 @@ describe('Recurring Payment Queries', () => {
 
       expect(query.toRetrieveRequest()).toEqual({
         collection: 'defra_recurringpayments',
-        filter: "defra_nextduedate eq 'Wed Nov 08 2023 00:00:00 GMT+0000 (Greenwich Mean Time)' and defra_cancelleddate eq null",
+        filter:
+          "defra_nextduedate eq 'Wed Nov 08 2023 00:00:00 GMT+0000 (Greenwich Mean Time)' and defra_cancelleddate eq null and _defra_nextrecurringpayment_value eq null",
         select: [
           'defra_recurringpaymentid',
           'defra_name',
