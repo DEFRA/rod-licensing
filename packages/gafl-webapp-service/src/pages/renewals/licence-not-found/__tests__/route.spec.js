0c901a7b1e2d4ce0f127ef921e8df42ee07e0520
laila aleissa <138867360+lailien3@users.noreply.github.com>
2025-05-28 17:12:57 +0100

    Change tests to actually test page routing

--- packages/gafl-webapp-service/src/pages/renewals/licence-not-found/__tests__/route.spec.js
+++ packages/gafl-webapp-service/src/pages/renewals/licence-not-found/__tests__/route.spec.js
@@ -1,32 +1,22 @@
-import { IDENTIFY, AUTHENTICATE, RENEWAL_PUBLIC, LICENCE_NOT_FOUND } from '../../../../uri.js'
-import { start, stop, initialize, injectWithCookies } from '../../../../__mocks__/test-utils-system.js'
-import { dobHelper, ADULT_TODAY } from '../../../../__mocks__/test-utils-business-rules.js'
-import { salesApi } from '@defra-fish/connectors-lib'
+import pageRoute from '../../../../routes/page-route.js'
+import { LICENCE_NOT_FOUND } from '../../../../uri.js'
+import '../route.js'
 
-beforeAll(() => new Promise(resolve => start(resolve)))
-beforeAll(() => new Promise(resolve => initialize(resolve)))
-afterAll(d => stop(d))
-
-const VALID_RENEWAL_PUBLIC_URI = RENEWAL_PUBLIC.uri.replace('{referenceNumber?}', '')
-
-jest.mock('@defra-fish/connectors-lib')
+jest.mock('../../../../routes/page-route.js', () => jest.fn())
+jest.mock('../../../../uri.js', () => ({
+  RENEWAL_PUBLIC: { uri: Symbol('renewal public uri') },
+  LICENCE_NOT_FOUND: {
+    page: Symbol('licence-not-found page'),
+    uri: Symbol('licence-not-found uri')
+  }
+}))
 
 describe('LICENCE_NOT_FOUND route', () => {
-  it('redirects to licence not found on posting valid but not authenticated details', async () => {
-    salesApi.authenticate.mockImplementation(jest.fn(async () => new Promise(resolve => resolve(null))))
-    await injectWithCookies('GET', VALID_RENEWAL_PUBLIC_URI)
-    await injectWithCookies('GET', IDENTIFY.uri)
-    const data = await injectWithCookies(
-      'POST',
-      IDENTIFY.uri,
-      Object.assign({ postcode: 'BS9 1HJ', referenceNumber: 'AAAAAA' }, dobHelper(ADULT_TODAY))
-    )
-    expect(data.statusCode).toBe(302)
-    expect(data.headers.location).toHaveValidPathFor(AUTHENTICATE.uri)
-    const data2 = await injectWithCookies('GET', AUTHENTICATE.uri)
-    expect(data2.statusCode).toBe(302)
-    expect(data2.headers.location).toHaveValidPathFor(LICENCE_NOT_FOUND.uri)
-    const data3 = await injectWithCookies('GET', LICENCE_NOT_FOUND.uri)
-    expect(data3.statusCode).toBe(200)
+  it('passes LICENCE_NOT_FOUND.page as the first argument to pageRoute', () => {
+    expect(pageRoute.mock.calls[0][0]).toBe(LICENCE_NOT_FOUND.page)
+  })
+
+  it('passes LICENCE_NOT_FOUND.uri as the second argument to pageRoute', () => {
+    expect(pageRoute.mock.calls[0][1]).toBe(LICENCE_NOT_FOUND.uri)
   })
 })
