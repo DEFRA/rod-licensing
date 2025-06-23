8a59cbf1b99b45cfa4e721cda19afcb343c11ecc
Scott <scott.dormand@defra.gov.uk>
2025-06-23 10:19:01 +0100

    update s3 to local to v3

--- packages/pocl-job/src/transport/__tests__/s3-to-local.spec.js
+++ packages/pocl-job/src/transport/__tests__/s3-to-local.spec.js
@@ -2,7 +2,7 @@ import { s3ToLocal } from '../s3-to-local.js'
 import stream from 'stream'
 import { AWS } from '@defra-fish/connectors-lib'
 import fs from 'fs'
-const { s3 } = AWS.mock.results[0].value
+const { GetObjectCommand } = AWS.mock.results[0].value
 
 const MOCK_TMP = '/tmp/local/mock'
 jest.mock('stream')
@@ -16,36 +16,64 @@ jest.mock('../../config.js', () => ({
   }
 }))
 
-jest.mock('@defra-fish/connectors-lib', () => ({
-  AWS: jest.fn(() => ({
-    s3: {
-      getObject: jest.fn(() => ({
-        createReadStream: jest.fn(() => ({}))
-      }))
-    }
-  }))
-}))
+jest.mock('@defra-fish/connectors-lib', () => {
+  const { Readable } = require('stream')
 
-describe('s3-to-local', () => {
-  beforeEach(() => {
-    jest.clearAllMocks()
+  const mockS3Stream = new Readable({
+    read () {
+      this.push(null)
+    }
   })
 
-  it('retrieves a file from s3 for a given key', async () => {
-    jest.spyOn(fs, 'createWriteStream').mockReturnValueOnce({})
+  const mockSend = jest.fn().mockResolvedValue({
+    Body: mockS3Stream
+  })
+
+  const mockGetObjectCommand = jest.fn(function (input) {
+    this.input = input
+  })
+
+  return {
+    AWS: jest.fn(() => ({
+      s3: {
+        send: mockSend
+      },
+      GetObjectCommand: mockGetObjectCommand
+    }))
+  }
+})
+
+describe('s3ToLocal', () => {
+  beforeEach(() => {
+    jest.spyOn(fs, 'createWriteStream').mockReturnValue({})
     stream.pipeline.mockImplementation(
-      jest.fn((streams, callback) => {
+      jest.fn((...args) => {
+        const callback = args[args.length - 1]
         callback()
       })
     )
+  })
 
+  it('returns the expected local file path', async () => {
     const result = await s3ToLocal('/example/testS3Key.xml')
-    const { createReadStream } = s3.getObject.mock.results[0].value
-
     expect(result).toBe(`${MOCK_TMP}/example/testS3Key.xml`)
-    expect(createReadStream).toHaveBeenCalled()
+  })
+
+  it('calls stream.pipeline', async () => {
+    await s3ToLocal('/example/testS3Key.xml')
     expect(stream.pipeline).toHaveBeenCalled()
-    expect(s3.getObject).toHaveBeenCalledWith({
+  })
+
+  it('calls s3.send with an instance of GetObjectCommand', async () => {
+    await s3ToLocal('/example/testS3Key.xml')
+    const sendCallArg = AWS().s3.send.mock.calls[0][0]
+    expect(sendCallArg).toBeInstanceOf(GetObjectCommand)
+  })
+
+  it('calls GetObjectCommand with the correct input', async () => {
+    await s3ToLocal('/example/testS3Key.xml')
+    const sendCallArg = AWS().s3.send.mock.calls[0][0]
+    expect(sendCallArg.input).toEqual({
       Bucket: 'testbucket',
       Key: '/example/testS3Key.xml'
     })
--- packages/pocl-job/src/transport/s3-to-local.js
+++ packages/pocl-job/src/transport/s3-to-local.js
@@ -7,14 +7,19 @@ import config from '../config.js'
 import { getTempDir } from '../io/file.js'
 import { AWS } from '@defra-fish/connectors-lib'
 const pipeline = util.promisify(stream.pipeline)
-const { s3 } = AWS()
+const { s3, GetObjectCommand } = AWS()
 const debug = db('pocl:transport')
 
 export async function s3ToLocal (s3Key) {
   debug('Transferring %s to the local storage', s3Key)
   const localPath = Path.join(getTempDir(Path.dirname(s3Key)), Path.basename(s3Key))
   const localWriteStream = fs.createWriteStream(localPath)
-  const s3ReadStream = s3.getObject({ Bucket: config.s3.bucket, Key: s3Key }).createReadStream()
-  await pipeline([s3ReadStream, localWriteStream])
+  const command = new GetObjectCommand({
+    Bucket: config.s3.bucket,
+    Key: s3Key
+  })
+  const response = await s3.send(command)
+  const s3ReadStream = response.Body
+  await pipeline(s3ReadStream, localWriteStream)
   return localPath
 }
