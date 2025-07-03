b7306afe2e4b39ab8af28f5d90a8877ffbf51a7f
Philip Benson <philbens@gmail.com>
2025-07-03 11:00:44 +0100

    Add GovPay health check to connectors

--- packages/connectors-lib/src/govuk-pay-api.js
+++ packages/connectors-lib/src/govuk-pay-api.js
@@ -86,3 +86,13 @@ export const fetchPaymentEvents = async (paymentId, recurring = false) => {
     throw err
   }
 }
+
+export const isGovPayUp = async () => {
+  try {
+    return await fetch('https://publicapi.payments.service.gov.uk/healthcheck')
+  } catch (err) {
+    console.error('Error retrieving GovPay health status', err)
+    throw err
+  }
+  
+}
