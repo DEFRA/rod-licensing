f5b33c4158e235aa33d161fc01155629bd090505
Iris Faraway <iris.faraway@gmail.com>
2025-07-21 15:10:56 +0100

    Set name of RecurringPayment record (#2179)
    https://eaflood.atlassian.net/browse/IWTF-4701
    
    Currently the name field of RecurringPayments in the CRM is not set to anything useful. We want it to include the contact name and the year the next payment is due to make it easier to find records in the interface.

--- packages/sales-api-service/src/services/recurring-payments.service.js
+++ packages/sales-api-service/src/services/recurring-payments.service.js
@@ -65,7 +65,7 @@ export const processRecurringPayment = async (transactionRecord, contact) => {
   if (transactionRecord.payment?.recurring) {
     const recurringPayment = new RecurringPayment()
     hash.update(recurringPayment.uniqueContentId)
-    recurringPayment.name = transactionRecord.payment.recurring.name
+    recurringPayment.name = determineRecurringPaymentName(transactionRecord, contact)
     recurringPayment.nextDueDate = transactionRecord.payment.recurring.nextDueDate
     recurringPayment.cancelledDate = transactionRecord.payment.recurring.cancelledDate
     recurringPayment.cancelledReason = transactionRecord.payment.recurring.cancelledReason
@@ -147,4 +147,9 @@ export const cancelRecurringPayment = async id => {
   } else {
     console.log('No matches found for cancellation')
   }
+}
+
+const determineRecurringPaymentName = (transactionRecord, contact) => {
+  const [dueYear] = transactionRecord.payment.recurring.nextDueDate.split('-')
+  return [contact.firstName, contact.lastName, dueYear].join(' ')
 }
