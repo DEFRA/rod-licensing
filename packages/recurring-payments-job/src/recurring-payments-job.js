18625b7e039f64b293d3916dbda5e8a3ece6f078
Philip Benson <philbens@gmail.com>
2025-08-20 11:01:39 +0100

    Add constants for magic numbers

--- packages/recurring-payments-job/src/recurring-payments-job.js
+++ packages/recurring-payments-job/src/recurring-payments-job.js
@@ -3,6 +3,9 @@ import recurringPaymentsJob from 'commander'
 import { processRecurringPayments } from './recurring-payments-processor.js'
 import { airbrake } from '@defra-fish/connectors-lib'
 
+const SIGINT_CODE = 130
+const SIGTERM_CODE = 137
+
 airbrake.initialise()
 const delay = parseInt(process.env.RECURRING_PAYMENTS_LOCAL_DELAY || '0', 10)
 if (delay > 0) {
@@ -18,12 +21,12 @@ function executeRecurringPaymentsJob () {
   airbrake.flush()
 }
 
-const shutdown = (code) => {
+const shutdown = code => {
   airbrake.flush()
   process.exit(code)
 }
 
-process.on('SIGINT', () => shutdown(130))
-process.on('SIGTERM', () => shutdown(137))
+process.on('SIGINT', () => shutdown(SIGINT_CODE))
+process.on('SIGTERM', () => shutdown(SIGTERM_CODE))
 
 export default recurringPaymentsJob
