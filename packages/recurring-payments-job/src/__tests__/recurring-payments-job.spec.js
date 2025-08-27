74ef53fa13ce12da14ad84ad19fa64cde9a0dc89
Phil Benson <phil.benson@defra.gov.uk>
2025-08-27 12:37:43 +0100

    Setup Recurring Payments job to log to Errbit (#2198)
    * Setup Recurring Payments job to log to Errbit
    
    https://eaflood.atlassian.net/browse/IWTF-4689
    
    Modify Recurring Payments job so that it logs to Errbit, by using Airbrake
    
    * Modify RP entry script to initialise and flush Airbrake
    * Flush airbrake on SIGINT and SIGTERM signals

--- packages/recurring-payments-job/src/__tests__/recurring-payments-job.spec.js
+++ packages/recurring-payments-job/src/__tests__/recurring-payments-job.spec.js
@@ -1,34 +1,107 @@
 import commander from 'commander'
+import { airbrake } from '@defra-fish/connectors-lib'
 import { processRecurringPayments } from '../recurring-payments-processor.js'
 import fs from 'fs'
 
 jest.useFakeTimers()
 
-jest.mock('../recurring-payments-processor.js', () => {
-  if (!global.processRecurringPayments) {
-    global.processRecurringPayments = jest.fn()
+jest.mock('../recurring-payments-processor.js')
+jest.mock('@defra-fish/connectors-lib', () => ({
+  airbrake: {
+    initialise: jest.fn(),
+    flush: jest.fn()
   }
-  return { processRecurringPayments: global.processRecurringPayments }
-})
-
-jest.mock('commander', () => {
-  if (!global.commander) {
-    global.commander = jest.requireActual('commander')
-  }
-  return global.commander
-})
+}))
 
 jest.mock('fs')
-
 describe('recurring-payments-job', () => {
+  beforeAll(() => {
+    fs.readFileSync.mockReturnValue(JSON.stringify({ name: 'recurring-payments-test', version: '1.0.0' }))
+  })
+
   beforeEach(() => {
     jest.clearAllMocks()
     commander.args = ['test']
   })
 
+  it('initialises airbrake', () => {
+    jest.isolateModules(() => {
+      require('../recurring-payments-job.js')
+      expect(airbrake.initialise).toHaveBeenCalled()
+    })
+  })
+
+  it('flushes airbrake before script ends', () => {
+    jest.isolateModules(() => {
+      require('../recurring-payments-job.js')
+      expect(airbrake.flush).toHaveBeenCalled()
+    })
+  })
+
+  it("doesn't flush airbrake before processRecurringPayments has been called", () => {
+    jest.isolateModules(() => {
+      processRecurringPayments.mockImplementationOnce(() => {
+        expect(airbrake.flush).not.toHaveBeenCalled()
+      })
+      require('../recurring-payments-job.js')
+    })
+  })
+
+  it.each([
+    ['SIGINT', 130],
+    ['SIGTERM', 137]
+  ])('flushes airbrake on %s signal', (signal, code) => {
+    jest.isolateModules(() => {
+      // setup a delay so script doesn't call processRecurringPayments and exit naturally
+      process.env.RECURRING_PAYMENTS_LOCAL_DELAY = '1'
+      const signalCallbacks = {}
+      jest.spyOn(process, 'on')
+      jest.spyOn(process, 'exit')
+      process.on.mockImplementation((signalToken, callback) => {
+        signalCallbacks[signalToken] = callback
+      })
+      process.exit.mockImplementation(() => {
+        // so we don't crash out of the tests!
+      })
+
+      require('../recurring-payments-job.js')
+      signalCallbacks[signal]()
+
+      expect(airbrake.flush).toHaveBeenCalled()
+      process.on.mockRestore()
+      process.exit.mockRestore()
+    })
+  })
+
+  it.each([
+    ['SIGINT', 130],
+    ['SIGTERM', 137]
+  ])('calls process.exit on %s signal with %i code', (signal, code) => {
+    jest.isolateModules(() => {
+      // setup a delay so script doesn't call processRecurringPayments and exit naturally
+      process.env.RECURRING_PAYMENTS_LOCAL_DELAY = '1'
+      const signalCallbacks = {}
+      jest.spyOn(process, 'on')
+      jest.spyOn(process, 'exit')
+      process.on.mockImplementation((signalToken, callback) => {
+        signalCallbacks[signalToken] = callback
+      })
+      process.exit.mockImplementation(() => {
+        // so we don't crash out of the tests!
+      })
+
+      require('../recurring-payments-job.js')
+      signalCallbacks[signal]()
+
+      expect(process.exit).toHaveBeenCalledWith(code)
+      process.on.mockRestore()
+      process.exit.mockRestore()
+    })
+  })
+
   it('logs startup details including name and version', () => {
     const mockPkg = { name: 'recurring-payments-test', version: '1.2.3' }
-    fs.readFileSync.mockReturnValue(JSON.stringify(mockPkg))
+    fs.readFileSync.mockReturnValueOnce(JSON.stringify(mockPkg))
 
     jest.isolateModules(() => {
       const logSpy = jest.spyOn(console, 'log').mockImplementation(() => {})
@@ -39,11 +112,13 @@ describe('recurring-payments-job', () => {
         mockPkg.name,
         mockPkg.version
       )
+      logSpy.mockRestore()
     })
   })
 
   it('calls processRecurringPayments when no delay', () => {
     jest.isolateModules(() => {
+      process.env.RECURRING_PAYMENTS_LOCAL_DELAY = '0'
       require('../recurring-payments-job.js')
       expect(processRecurringPayments).toHaveBeenCalled()
     })
@@ -51,6 +126,7 @@ describe('recurring-payments-job', () => {
 
   it('doesnt call setTimeout when no correct delay', () => {
     jest.isolateModules(() => {
+      process.env.RECURRING_PAYMENTS_LOCAL_DELAY = 'invalid-delay'
       const setTimeoutSpy = jest.spyOn(global, 'setTimeout')
       require('../recurring-payments-job.js')
       expect(setTimeoutSpy).not.toHaveBeenCalled()
--- packages/recurring-payments-job/src/recurring-payments-job.js
+++ packages/recurring-payments-job/src/recurring-payments-job.js
@@ -1,13 +1,17 @@
 'use strict'
 import recurringPaymentsJob from 'commander'
 import { processRecurringPayments } from './recurring-payments-processor.js'
+import { airbrake } from '@defra-fish/connectors-lib'
 import path from 'path'
 import fs from 'fs'
 
+const SIGINT_CODE = 130
+const SIGTERM_CODE = 137
 const pkgPath = path.join(process.cwd(), 'package.json')
 const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf-8'))
 
 console.log('Recurring payments job starting at %s. name: %s. version: %s', new Date().toISOString(), pkg.name, pkg.version)
+airbrake.initialise()
 
 const delay = parseInt(process.env.RECURRING_PAYMENTS_LOCAL_DELAY || '0', 10)
 if (delay > 0) {
@@ -20,6 +24,15 @@ if (delay > 0) {
 
 function executeRecurringPaymentsJob () {
   recurringPaymentsJob.action(processRecurringPayments())
+  airbrake.flush()
 }
+
+const shutdown = code => {
+  airbrake.flush()
+  process.exit(code)
+}
+
+process.on('SIGINT', () => shutdown(SIGINT_CODE))
+process.on('SIGTERM', () => shutdown(SIGTERM_CODE))
 
 export default recurringPaymentsJob
