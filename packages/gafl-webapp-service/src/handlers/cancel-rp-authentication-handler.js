1131ce8c36d08c2c2d1f34d0154c2e6ae2983922
Philip Benson <philbens@gmail.com>
2025-09-03 09:41:52 +0100

    fix code smell

--- packages/gafl-webapp-service/src/handlers/cancel-rp-authentication-handler.js
+++ packages/gafl-webapp-service/src/handlers/cancel-rp-authentication-handler.js
@@ -1,6 +1,6 @@
 import { CONTROLLER } from '../uri.js'
 
-export default async (request, h) => {
+export default async (_request, h) => {
   console.log('cancel rp authenticate handler', h.redirectWithLanguageCode)
   return h.redirectWithLanguageCode(CONTROLLER.uri)
 }
