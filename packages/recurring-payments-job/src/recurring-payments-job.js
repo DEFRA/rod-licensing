550e7f84a9f1cee2886d9422a83292c84d02e541
Philip Benson <philbens@gmail.com>
2025-09-24 15:41:13 +0100

    Debug notifier

--- packages/recurring-payments-job/src/recurring-payments-job.js
+++ packages/recurring-payments-job/src/recurring-payments-job.js
@@ -3,10 +3,34 @@ import recurringPaymentsJob from 'commander'
 import { execute } from './recurring-payments-processor.js'
 import path from 'path'
 import fs from 'fs'
+import { Notifier } from '@airbrake/node'
+import { formatWithOptions, inspect } from 'util'
 const pkgPath = path.join(process.cwd(), 'package.json')
 const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf-8'))
 
 console.log('Recurring payments job starting at %s. name: %s. version: %s', new Date().toISOString(), pkg.name, pkg.version)
+
+const INSPECT_OPTS = { depth: null, maxStringLength: null, maxArrayLength: null, breakLength: null, compact: true, showHidden: true }
+try {
+  console.log('creating notifier', process.env.AIRBRAKE_PROJECT_KEY, process.env.AIRBRAKE_HOST, process.env.NODE_ENV)
+  const ab = new Notifier({
+    projectId: 2,
+    projectKey: process.env.AIRBRAKE_PROJECT_KEY,
+    host: process.env.AIRBRAKE_HOST,
+    environment: process.env.NODE_ENV,
+    performanceStats: false
+  })
+  console.log('notifying')
+  ab.notify({
+    error: new Error(formatWithOptions(INSPECT_OPTS, 'not really an error')),
+    params: { consoleInvocationDetails: { method: 'error', arguments: { 1: inspect('test log message', INSPECT_OPTS) } } },
+    environment: { name: process.env.name },
+    context: { }
+  })
+  console.log('notified')
+} catch (e) {
+  console.log('error notifying: ', e)
+}
 
 const delay = parseInt(process.env.RECURRING_PAYMENTS_LOCAL_DELAY || '0', 10)
 if (delay > 0) {
