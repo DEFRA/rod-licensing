53b38871c2c848300a7050c774964756359c39b7
laila aleissa <138867360+lailien3@users.noreply.github.com>
2025-10-28 09:34:00 +0000

    create route tests

--- packages/gafl-webapp-service/src/pages/recurring-payments/cancel/licence-not-found/route.js
+++ packages/gafl-webapp-service/src/pages/recurring-payments/cancel/licence-not-found/route.js
@@ -1,4 +1,4 @@
-import { LICENCE_NOT_FOUND_RP } from '../../../uri.js'
-import pageRoute from '../../../routes/page-route.js'
+import { LICENCE_NOT_FOUND_RP } from '../../../../uri.js'
+import pageRoute from '../../../../routes/page-route.js'
 
 export default pageRoute(LICENCE_NOT_FOUND_RP.page, LICENCE_NOT_FOUND_RP.uri, null)
