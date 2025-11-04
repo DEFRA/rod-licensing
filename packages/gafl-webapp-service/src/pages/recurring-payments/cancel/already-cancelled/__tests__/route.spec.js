e2cae53b9a8c4d0a12c382990c427935d454623f
laila aleissa <138867360+lailien3@users.noreply.github.com>
2025-11-04 16:07:40 +0000

    create skelaton for page with journey deifned

--- packages/gafl-webapp-service/src/pages/recurring-payments/cancel/already-cancelled/__tests__/route.spec.js
+++ packages/gafl-webapp-service/src/pages/recurring-payments/cancel/already-cancelled/__tests__/route.spec.js
@@ -1,80 +1,21 @@
 import pageRoute from '../../../../../routes/page-route.js'
 import { RP_ALREADY_CANCELLED } from '../../../../../uri.js'
+import '../route.js'
 
-require('../route.js')
-// eslint-disable-next-line no-unused-vars
-const [[_v, _p, validator, completion, getData]] = pageRoute.mock.calls
-
-jest.mock('../../../../../routes/page-route.js')
+jest.mock('../../../../../routes/page-route.js', () => jest.fn())
 jest.mock('../../../../../uri.js', () => ({
-  ...jest.requireActual('../../../../../uri.js'),
-  RP_ALREADY_CANCELLED: { page: Symbol('cancel-rp-complete-page'), uri: Symbol('cancel-rp-complete-uri') }
+  RP_ALREADY_CANCELLED: {
+    page: Symbol('rp-already-cancelled page'),
+    uri: Symbol('rp-already-cancelled uri')
+  }
 }))
-jest.mock('../../../../../processors/uri-helper.js')
 
-describe('pageRoute receives expected arguments', () => {
-  it('passes RP_ALREADY_CANCELLED.page as the view name', () => {
-    jest.isolateModules(() => {
-      require('../route.js')
-      expect(pageRoute).toHaveBeenCalledWith(
-        RP_ALREADY_CANCELLED.page,
-        expect.anything(),
-        expect.anything(),
-        expect.anything(),
-        expect.anything()
-      )
-    })
+describe('RP_ALREADY_CANCELLED route', () => {
+  it('passes RP_ALREADY_CANCELLED.page as the first argument to pageRoute', () => {
+    expect(pageRoute.mock.calls[0][0]).toBe(RP_ALREADY_CANCELLED.page)
   })
 
-  it('passes RP_ALREADY_CANCELLED.uri as the path', () => {
-    jest.isolateModules(() => {
-      require('../route.js')
-      expect(pageRoute).toHaveBeenCalledWith(
-        expect.anything(),
-        RP_ALREADY_CANCELLED.uri,
-        expect.anything(),
-        expect.anything(),
-        expect.anything()
-      )
-    })
-  })
-
-  it('passes a function as the validator', () => {
-    jest.isolateModules(() => {
-      require('../route.js')
-      expect(pageRoute).toHaveBeenCalledWith(
-        expect.anything(),
-        expect.anything(),
-        expect.any(Function),
-        expect.anything(),
-        expect.anything()
-      )
-    })
-  })
-
-  it('passes a function to generate redirect location on completion', () => {
-    jest.isolateModules(() => {
-      require('../route.js')
-      expect(pageRoute).toHaveBeenCalledWith(
-        expect.anything(),
-        expect.anything(),
-        expect.anything(),
-        expect.any(Function),
-        expect.anything()
-      )
-    })
-  })
-
-  it('passes a function to get the page data', () => {
-    jest.isolateModules(() => {
-      require('../route.js')
-      expect(pageRoute).toHaveBeenCalledWith(
-        expect.anything(),
-        expect.anything(),
-        expect.anything(),
-        expect.anything(),
-        expect.any(Function)
-      )
-    })
+  it('passes RP_ALREADY_CANCELLED.uri as the second argument to pageRoute', () => {
+    expect(pageRoute.mock.calls[0][1]).toBe(RP_ALREADY_CANCELLED.uri)
   })
 })
--- packages/gafl-webapp-service/src/pages/recurring-payments/cancel/already-cancelled/route.js
+++ packages/gafl-webapp-service/src/pages/recurring-payments/cancel/already-cancelled/route.js
@@ -1,10 +1,4 @@
-import pageRoute from '../../../../routes/page-route.js'
 import { RP_ALREADY_CANCELLED } from '../../../../uri.js'
+import pageRoute from '../../../../routes/page-route.js'
 
-export default pageRoute(
-  RP_ALREADY_CANCELLED.page,
-  RP_ALREADY_CANCELLED.uri,
-  () => {},
-  () => {},
-  () => {}
-)
+export default pageRoute(RP_ALREADY_CANCELLED.page, RP_ALREADY_CANCELLED.uri, null)
