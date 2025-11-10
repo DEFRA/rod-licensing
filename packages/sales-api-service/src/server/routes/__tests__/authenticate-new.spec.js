54c79ac8709cdd0cc1fc1308223985ffe060a824
laila aleissa <138867360+lailien3@users.noreply.github.com>
2025-11-10 08:15:40 +0000

    RP journey cancellation page 1 - Identify
    https://eaflood.atlassian.net/browse/IWTF-4334
    
    This page will be used by the angler to identify themselves and their rp

--- packages/sales-api-service/src/server/routes/__tests__/authenticate-new.spec.js
+++ packages/sales-api-service/src/server/routes/__tests__/authenticate-new.spec.js
@@ -1,31 +1,84 @@
 import { contactForLicenseeNoReference, executeQuery } from '@defra-fish/dynamics-lib'
-import db from 'debug'
-jest.mock('@defra-fish/dynamics-lib')
-jest.mock('debug')
 
 describe('executeWithErrorLog', () => {
-  it('throws error', async () => {
-    const debug = jest.fn()
-    db.mockReturnValueOnce(debug)
-    executeQuery.mockImplementationOnce(() => {
-      throw new Error()
+  const arrangeExecuteWithErrorLog = async () => {
+    jest.resetModules()
+
+    const logSpy = jest.fn()
+    jest.doMock('debug', () => jest.fn(() => logSpy))
+
+    jest.doMock('@defra-fish/dynamics-lib', () => {
+      const actual = jest.requireActual('@defra-fish/dynamics-lib')
+      return {
+        ...actual,
+        executeQuery: jest.fn().mockImplementationOnce(() => Promise.reject(new Error('oopsie')))
+      }
     })
-    contactForLicenseeNoReference.mockReturnValueOnce({ filter: 'query filter test' })
-    const authenticate = require('../authenticate.js').default
+
+    const { errorLogTest } = await import('../authenticate.js')
+    return { logSpy, executeWithErrorLog: errorLogTest.executeWithErrorLog }
+  }
+
+  it('rejects when executeQuery fails', async () => {
+    const { executeWithErrorLog } = await arrangeExecuteWithErrorLog()
+    await expect(executeWithErrorLog({ filter: 'query filter test' })).rejects.toThrow()
+  })
+
+  it('logs the filter on failure', async () => {
+    const { executeWithErrorLog, logSpy } = await arrangeExecuteWithErrorLog()
+    await executeWithErrorLog({ filter: 'query filter test' }).catch(() => {})
+    expect(logSpy).toHaveBeenCalledWith('Error executing query with filter query filter test')
+  })
+})
+
+describe('executeWithErrorLog via handler', () => {
+  const arrangeHandler = async () => {
+    jest.resetModules()
+
+    const logSpy = jest.fn()
+    jest.doMock('debug', () => jest.fn(() => logSpy))
+
+    jest.doMock('@defra-fish/dynamics-lib', () => {
+      const actual = jest.requireActual('@defra-fish/dynamics-lib')
+      return {
+        ...actual,
+        executeQuery: jest.fn().mockImplementationOnce(() => Promise.reject(new Error('oopsie'))),
+        contactForLicenseeNoReference: jest.fn(() => ({ filter: 'query filter test' })),
+        permissionForContacts: jest.fn(() => [])
+      }
+    })
+
+    const authenticate = (await import('../authenticate.js')).default
     const [
       {
         options: { handler }
       }
     ] = authenticate
-    const mockRequest = {
-      query: { licenseeBirthDate: '', licenseePostcode: '' },
-      params: { referenceNumber: '' }
+
+    return { logSpy, handler }
+  }
+
+  it('bubbles the error from executeQuery', async () => {
+    const { handler } = await arrangeHandler()
+    const request = {
+      query: { licenseeBirthDate: '2000-01-01', licenseePostcode: 'AB12 3CD' },
+      params: { referenceNumber: 'CD379B' }
     }
+    const h = { response: () => ({ code: () => {} }) }
 
-    try {
-      await handler(mockRequest)
-    } catch {}
+    await expect(handler(request, h)).rejects.toThrow('oopsie')
+  })
 
-    expect(debug).toHaveBeenCalledWith('Error executing query with filter query filter test')
+  it('logs the filter when executeQuery fails', async () => {
+    const { handler, logSpy } = await arrangeHandler()
+    const request = {
+      query: { licenseeBirthDate: '2000-01-01', licenseePostcode: 'AB12 3CD' },
+      params: { referenceNumber: 'CD379B' }
+    }
+    const h = { response: () => ({ code: () => {} }) }
+
+    await handler(request, h).catch(() => {})
+
+    expect(logSpy).toHaveBeenCalledWith('Error executing query with filter query filter test')
   })
 })
