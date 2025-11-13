2b8318c2dd2178dfddd4d609c41c86a32c285fa5
irisfaraway <iris.faraway@gmail.com>
2025-11-13 16:01:22 +0000

    Try authing in the temp script

--- scripts/github/temp.sh
+++ scripts/github/temp.sh
@@ -9,3 +9,9 @@ trap 'exit 1' INT
 echo 'Greetings from this temporary script!'
 echo ${ACTIONS_ID_TOKEN_REQUEST_URL}
 echo 'I hope that worked okay!'
+
+curl -H "Authorization: bearer ${ACTIONS_ID_TOKEN_REQUEST_TOKEN}" "${ACTIONS_ID_TOKEN_REQUEST_URL}&audience=api://AzureADTokenExchange" >> $NPM_AUTH_TOKEN
+
+echo "Setting up npm"
+echo "//registry.npmjs.org/:_authToken=\${NPM_AUTH_TOKEN}" >> $HOME/.npmrc 2> /dev/null
+npm whoami
