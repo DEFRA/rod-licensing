1af403198dd934650b4985c54fdc2ca5b6fd1300
laila aleissa <138867360+lailien3@users.noreply.github.com>
2025-11-29 17:22:14 +0000

    Unhappy cancellation journey page 2 - already cancelled
    https://eaflood.atlassian.net/browse/IWTF-4807
    
    This page will tell an angler that the RCP agreement is already cancelled for that particular licence

---
+++ packages/gafl-webapp-service/src/pages/recurring-payments/cancel/already-cancelled/__tests__/route.spec.js
@@ -0,0 +1,21 @@
+import pageRoute from '../../../../../routes/page-route.js'
+import { CANCEL_RP_ALREADY_CANCELLED } from '../../../../../uri.js'
+import '../route.js'
+
+jest.mock('../../../../../routes/page-route.js', () => jest.fn())
+jest.mock('../../../../../uri.js', () => ({
+  CANCEL_RP_ALREADY_CANCELLED: {
+    page: Symbol('rp-already-cancelled page'),
+    uri: Symbol('rp-already-cancelled uri')
+  }
+}))
+
+describe('CANCEL_RP_ALREADY_CANCELLED route', () => {
+  it('passes CANCEL_RP_ALREADY_CANCELLED.page as the first argument to pageRoute', () => {
+    expect(pageRoute.mock.calls[0][0]).toBe(CANCEL_RP_ALREADY_CANCELLED.page)
+  })
+
+  it('passes CANCEL_RP_ALREADY_CANCELLED.uri as the second argument to pageRoute', () => {
+    expect(pageRoute.mock.calls[0][1]).toBe(CANCEL_RP_ALREADY_CANCELLED.uri)
+  })
+})
