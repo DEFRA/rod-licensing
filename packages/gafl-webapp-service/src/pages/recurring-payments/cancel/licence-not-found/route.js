cfefe4c021974a80bdf97fbab1a469d069fa9d6b
laila aleissa <138867360+lailien3@users.noreply.github.com>
2025-12-24 13:34:41 +0000

    top backlink button

--- packages/gafl-webapp-service/src/pages/recurring-payments/cancel/licence-not-found/route.js
+++ packages/gafl-webapp-service/src/pages/recurring-payments/cancel/licence-not-found/route.js
@@ -1,4 +1,19 @@
-import { CANCEL_RP_LICENCE_NOT_FOUND } from '../../../../uri.js'
+import { CANCEL_RP_LICENCE_NOT_FOUND, CANCEL_RP_IDENTIFY } from '../../../../uri.js'
 import pageRoute from '../../../../routes/page-route.js'
 
-export default pageRoute(CANCEL_RP_LICENCE_NOT_FOUND.page, CANCEL_RP_LICENCE_NOT_FOUND.uri)
+const getData = async request => {
+  const {
+    payload: { referenceNumber }
+  } = await request.cache().helpers.page.getCurrentPermission(CANCEL_RP_IDENTIFY.page)
+  return {
+    referenceNumber
+  }
+}
+
+export default pageRoute(
+  CANCEL_RP_LICENCE_NOT_FOUND.page,
+  CANCEL_RP_LICENCE_NOT_FOUND.uri,
+  () => {},
+  () => {},
+  getData
+)
