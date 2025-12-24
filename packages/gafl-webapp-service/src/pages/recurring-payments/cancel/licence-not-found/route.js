6cb801cc56e8de2726395eb0f0a2ebcdae364798
laila aleissa <138867360+lailien3@users.noreply.github.com>
2025-12-24 13:51:31 +0000

    update page handler to use journey definition to determine backlink

--- packages/gafl-webapp-service/src/pages/recurring-payments/cancel/licence-not-found/route.js
+++ packages/gafl-webapp-service/src/pages/recurring-payments/cancel/licence-not-found/route.js
@@ -1,19 +1,4 @@
-import { CANCEL_RP_LICENCE_NOT_FOUND, CANCEL_RP_IDENTIFY } from '../../../../uri.js'
+import { CANCEL_RP_LICENCE_NOT_FOUND } from '../../../../uri.js'
 import pageRoute from '../../../../routes/page-route.js'
 
-const getData = async request => {
-  const {
-    payload: { referenceNumber }
-  } = await request.cache().helpers.page.getCurrentPermission(CANCEL_RP_IDENTIFY.page)
-  return {
-    referenceNumber
-  }
-}
-
-export default pageRoute(
-  CANCEL_RP_LICENCE_NOT_FOUND.page,
-  CANCEL_RP_LICENCE_NOT_FOUND.uri,
-  () => {},
-  () => {},
-  getData
-)
+export default pageRoute(CANCEL_RP_LICENCE_NOT_FOUND.page, CANCEL_RP_LICENCE_NOT_FOUND.uri)
