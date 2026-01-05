5145806adfc4135b66be450c03baf5bc8046d682
Philip Benson <philbens@gmail.com>
2026-01-05 17:14:04 +0000

    add validaton for journey goal page

--- packages/gafl-webapp-service/src/pages/journey-goal/route.js
+++ packages/gafl-webapp-service/src/pages/journey-goal/route.js
@@ -2,9 +2,14 @@ import pageRoute from '../../routes/page-route.js'
 import { JOURNEY_GOAL } from '../../uri.js'
 import { nextPage } from '../../routes/next-page.js'
 import { journeyGoalResults } from './result-function.js'
+import Joi from 'joi'
 
 const getData = async () => ({
   journeyGoals: journeyGoalResults
 })
 
-export default pageRoute(JOURNEY_GOAL.page, JOURNEY_GOAL.uri, () => {}, nextPage, getData)
+export const validator = Joi.object({
+  'journey-goal': Joi.string().valid(Object.values(journeyGoalResults)).required()
+}).options({ abortEarly: false, allowUnknown: true })
+
+export default pageRoute(JOURNEY_GOAL.page, JOURNEY_GOAL.uri, validator, nextPage, getData)
