dac6eabd4f53742c8ca5bbf3e86122cedd36a5d4
Gabor Koos <gabor.koos@gmail.com>
2026-01-08 13:51:22 +0000

    Missing file added to version control

---
+++ packages/gafl-webapp-service/src/utils/validators.js
@@ -0,0 +1,22 @@
+// Run multiple validators and merge errors
+export const runValidators = (validators, payload) => {
+  const errors = validators
+    .map(validator => {
+      try {
+        validator(payload)
+        return null
+      } catch (err) {
+        return err
+      }
+    })
+    .filter(Boolean)
+
+  if (errors.length === 0) return
+
+  if (errors.length === 1) throw errors[0]
+
+  const mergedDetails = errors.flatMap(e => Array.isArray(e.details) ? e.details : [])
+  const error = new Error('expected error')
+  error.details = mergedDetails
+  throw error
+}
