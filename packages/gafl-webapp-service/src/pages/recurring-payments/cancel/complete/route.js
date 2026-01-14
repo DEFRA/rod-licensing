3bf43fdc894a2cd5abf639f2ae9b30019a5391e1
Gabor Koos <gabor.koos@gmail.com>
2026-01-14 12:15:22 +0000

    Cancellation completed page text added

--- packages/gafl-webapp-service/src/pages/recurring-payments/cancel/complete/route.js
+++ packages/gafl-webapp-service/src/pages/recurring-payments/cancel/complete/route.js
@@ -1,10 +1,20 @@
 import pageRoute from '../../../../routes/page-route.js'
 import { CANCEL_RP_COMPLETE } from '../../../../uri.js'
+import { cacheDateFormat, dateDisplayFormat } from '../../../../processors/date-and-time-display.js'
+import moment from 'moment-timezone'
+
+const getData = async request => {
+  const { permission } = await request.cache().helpers.transaction.getCurrentPermission()
+
+  return {
+    licenceExpiry: moment(permission.endDate, cacheDateFormat, request.locale).format(dateDisplayFormat)
+  }
+}
 
 export default pageRoute(
   CANCEL_RP_COMPLETE.page,
   CANCEL_RP_COMPLETE.uri,
   () => {},
   () => {},
-  () => {}
+  getData
 )
