f6e0824b71ce1e5b8850105d8a53337080fc1cf0
Philip Benson <philbens@gmail.com>
2026-03-03 12:32:27 +0000

    Added demo of bug

---
+++ packages/gafl-webapp-service/src/handlers/__tests__/agreed-handler-error-investigation.spec.js
@@ -0,0 +1,46 @@
+import agreedHandler from "../agreed-handler"
+import { COMPLETION_STATUS } from '../../constants.js'
+
+jest.mock('@defra-fish/connectors-lib')
+jest.mock('../../processors/refdata-helper.js', () => ({
+  countries: {
+    getAll: async () => []
+  }
+}))
+
+describe('agreed handler error investigation', () => {
+  it('error thrown', async () => {
+    const request = {
+      cache: () => ({
+        helpers: {
+          status: {
+            get: async () => ({
+              [COMPLETION_STATUS.agreed]: true,
+              permissions: [
+                {
+                    currentPage: "choose-payment",
+                    "licence-for": true,
+                    "choose-payment": true
+                }
+              ],
+              currentPermissionIdx: 0,
+            })
+          },
+          transaction: {
+            get: async () => ({
+              payment: {},
+              permissions: [
+                  {
+                      licensee: {},
+                      isLicenceForYou: true
+                  }
+              ],
+              id: "8e095950-4f7e-4279-b330-27b887ebaf8f"
+            })
+          }
+        }
+      })
+    }
+    await agreedHandler(request, { redirectWithLanguageCode: () => {}, redirect: () => {} })
+  })
+})
