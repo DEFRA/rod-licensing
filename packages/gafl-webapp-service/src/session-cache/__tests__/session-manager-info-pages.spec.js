e2ed96117d403ae056a5cac9fd0b34b95734e94d
laila aleissa <138867360+lailien3@users.noreply.github.com>
2026-03-06 15:55:18 +0000

    Allowing guidance links to be accessed out of sessions (#2319)
    * Allowing guidance links to be accessed out of sessions
    
    https://eaflood.atlassian.net/browse/IWTF-4854
    
    The guidance links in the GAFL footer all require the user to already be in a session. If a user goes directly to any of the links, e.g. from an email, they are redirected to ../buy/licence-for when they would expect to land on the intended guidance page. This ticket rectifies this, and allows users to access these links without being in a session.
    
    * better naming

---
+++ packages/gafl-webapp-service/src/session-cache/__tests__/session-manager-info-pages.spec.js
@@ -0,0 +1,18 @@
+import { start, stop, initialize, injectWithoutSessionCookie } from '../../__mocks__/test-utils-system.js'
+import { COOKIES, PRIVACY_POLICY, REFUND_POLICY, ACCESSIBILITY_STATEMENT } from '../../uri.js'
+
+describe('Session Exempt Informational Pages', () => {
+  beforeAll(() => new Promise(resolve => start(resolve)))
+  beforeAll(() => new Promise(resolve => initialize(resolve)))
+  afterAll(d => stop(d))
+
+  it.each([
+    ['cookies', COOKIES.uri],
+    ['privacy policy', PRIVACY_POLICY.uri],
+    ['refund policy', REFUND_POLICY.uri],
+    ['accessibility statement', ACCESSIBILITY_STATEMENT.uri]
+  ])('should allow direct access to %s guidance page without session', async (_desc, uri) => {
+    const response = await injectWithoutSessionCookie('GET', uri)
+    expect(response.statusCode).toBe(200)
+  })
+})
