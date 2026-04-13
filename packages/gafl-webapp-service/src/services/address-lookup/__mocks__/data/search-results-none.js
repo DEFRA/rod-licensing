86a03b0a6c41d56519773cac2eb30a1b7f4a08ee
laila aleissa <138867360+lailien3@users.noreply.github.com>
2026-04-13 15:05:52 +0100

    Revert "GAFL: DEV - Implement OS API Postcode Lookup & Filter by House Name/Number (No Pagination) (#2317)"
    This reverts commit c09669a76bc9acee79257dda9b1913ddadb842fd.

--- packages/gafl-webapp-service/src/services/address-lookup/__mocks__/data/search-results-none.js
+++ packages/gafl-webapp-service/src/services/address-lookup/__mocks__/data/search-results-none.js
@@ -1,16 +1,11 @@
 export default {
   header: {
-    uri: 'https://api.os.uk/search/places/v1/postcode?postcode=BS9%201HJ',
-    query: 'postcode=BS9 1HJ',
-    offset: 0,
-    totalresults: 0,
-    format: 'JSON',
+    totalMatches: 0,
+    endMatch: 0,
+    query: 'postcode=BS9 1hJ',
+    language: 'EN',
     dataset: 'DPA',
-    lr: 'EN,CY',
-    maxresults: 100,
-    epoch: '125',
-    lastupdate: '2026-02-26',
-    output_srs: 'EPSG:27700'
+    epoch: '74'
   },
   results: []
 }
