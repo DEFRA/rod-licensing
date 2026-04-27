8c38ed040c523b482752f4226ecc1ec415e0d3b9
laila aleissa <138867360+lailien3@users.noreply.github.com>
2026-04-27 16:36:20 +0100

    Update dynamics client config + token refresh (#2379)
    * Upgrade dynamics-web-api package
    
    https://eaflood.atlassian.net/browse/IWTF-5049
    
    * update lock files
    
    * Update dynamics client config + token refresh
    
    https://eaflood.atlassian.net/browse/IWTF-5050
    
    Currently, our config uses a callback in onTokenRefresh. In v2, callbacks will be removed, so we need to return the token instead.
    Config fields like webApuUrl and webApiVersion need updating to the v2 format
    
    * change env to use root domain

--- packages/dynamics-lib/src/client/__tests__/dynamics-client.spec.js
+++ packages/dynamics-lib/src/client/__tests__/dynamics-client.spec.js
@@ -3,7 +3,7 @@ import SimpleOAuth2 from 'simple-oauth2'
 
 describe('dynamics-client', () => {
   it('is configured via environment variables', async () => {
-    process.env.DYNAMICS_API_PATH = 'https://test-server/api/data/v9.1/'
+    process.env.DYNAMICS_API_HOST = 'https://test-server'
     process.env.DYNAMICS_API_VERSION = '9.1'
     process.env.DYNAMICS_API_TIMEOUT = 60000
     process.env.OAUTH_AUTHORITY_HOST_URL = 'https://test-authority/'
@@ -14,7 +14,7 @@ describe('dynamics-client', () => {
     const dynamicsApiConfig = config()
 
     expect(dynamicsApiConfig).toMatchObject({
-      serverUrl: 'https://test-server',
+      serverUrl: process.env.DYNAMICS_API_HOST,
       dataApi: { version: process.env.DYNAMICS_API_VERSION },
       timeout: `${process.env.DYNAMICS_API_TIMEOUT}`,
       onTokenRefresh: expect.any(Function)
