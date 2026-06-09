930e1ab4416138e1f4c05962972592d5de2f5a8e
laila aleissa <138867360+lailien3@users.noreply.github.com>
2026-06-09 14:23:15 +0100

    add concession

--- packages/gafl-webapp-service/src/processors/recurring-payments-write-cache.js
+++ packages/gafl-webapp-service/src/processors/recurring-payments-write-cache.js
@@ -11,7 +11,7 @@ export const setupCancelRecurringPaymentCacheFromAuthResult = async (request, au
         lastName: licensee.lastName,
         preferredMethodOfConfirmation: licensee.preferredMethodOfConfirmation?.label
       },
-      permit: { description: permit.description, permitSubtype: permit.permitSubtype, numberOfRods: permit.numberOfRods }
+      permit: { description: permit.description, permitSubtype: permit.permitSubtype, numberOfRods: permit.numberOfRods, concessions: permit.concessions }
     },
     recurringPayment: {
       id: recurringPayment.id,
