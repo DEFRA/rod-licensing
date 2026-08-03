03e2a96632c5cdb46ed4c29b7cdc98b0516bf4d0
laila aleissa <138867360+lailien3@users.noreply.github.com>
2026-08-03 23:52:07 +0100

    5125 testing

--- packages/gafl-webapp-service/src/processors/__tests__/recurring-payments-write-cache.spec.js
+++ packages/gafl-webapp-service/src/processors/__tests__/recurring-payments-write-cache.spec.js
@@ -10,7 +10,11 @@ describe('setUpCancelRecurringPaymentCacheFromAuthenticationResult', () => {
         lastName: 'Pysgotwr',
         preferredMethodOfConfirmation: { id: 910400000, label: 'Email', description: 'Email' }
       },
-      permit: { description: 'Coarse 6 month 15 Rod Licence (Half)' },
+      permit: {
+        description: 'Coarse 12 month 3 Rod Licence (Full)',
+        permitSubtype: { label: 'Trout and coarse' },
+        numberOfRods: 3
+      },
       recurringPayment: { lastDigitsCardNumbers: '5678' }
     }
 
@@ -48,7 +52,14 @@ describe('setUpCancelRecurringPaymentCacheFromAuthenticationResult', () => {
     it.each([
       ['referenceNumber', '23270624-2WC3FSD-ABNCY4'],
       ['endDate', '2024-12-31'],
-      ['permit', { description: 'Coarse 12 month 2 Rod Licence (Full)' }]
+      [
+        'permit',
+        {
+          description: 'Coarse 12 month 3 Rod Licence (Full)',
+          permitSubtype: { label: 'Trout and coarse' },
+          numberOfRods: 3
+        }
+      ]
     ])("Adds permission %s, value '%s', to transaction cache", async (fieldName, fieldValue) => {
       const setCurrentPermission = jest.fn()
       const mockRequest = getSampleRequest(setCurrentPermission)
