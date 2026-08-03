2836f663bba9eb58eabf8a61533acefa18987238
laila aleissa <138867360+lailien3@users.noreply.github.com>
2026-08-03 13:27:42 +0100

    Update packages/business-rules-lib/src/util/ages.js
    Co-authored-by: Iris Faraway <iris.faraway@gmail.com>

--- packages/business-rules-lib/src/util/ages.js
+++ packages/business-rules-lib/src/util/ages.js
@@ -2,7 +2,7 @@
 export const MINOR_MAX_AGE = 12
 /** The maximum age at which an angler is entitled to a junior concession */
 export const JUNIOR_MAX_AGE = 16
-/** The minimum age at which a recurring card payment can we offered */
+/** The minimum age at which a recurring card payment can be offered */
 export const ADULT_MIN_AGE = 18
 /** The minimum age at which an angler becomes entitled to a senior concession */
 export const SENIOR_MIN_AGE = 66
